654ef41a0af75c9b11031126e5af72e1a89d22b5
Melisa Bok <melisabok@gmail.com>
2016-09-09 16:18:57 -0400

    change version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='OpenReviewPy',
-      version='0.5.18',
+      version='0.5.19',
       description='OpenReview client library',
       url='https://github.com/iesl/OpenReviewPy',
       author='Michael Spector',
