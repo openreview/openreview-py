c680148fc300a2a3a460f71e8b0f3119ae466b90
mspector <michael.l.spector@gmail.com>
2016-10-12 16:12:21 -0400

    Fix/remove printline 5.26 (#18)
    * removed printline
    
    * updated to 0.5.26

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='OpenReviewPy',
-      version='0.5.25',
+      version='0.5.26',
       description='OpenReview client library',
       url='https://github.com/iesl/OpenReviewPy',
       author='Michael Spector',
