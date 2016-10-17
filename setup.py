7da6eed80fe0215b26c4fee3a461fc977420cbb9
mspector <michael.l.spector@gmail.com>
2016-10-17 19:07:20 -0400

    updated to 0.5.27 (#20)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='OpenReviewPy',
-      version='0.5.26',
+      version='0.5.27',
       description='OpenReview client library',
       url='https://github.com/iesl/OpenReviewPy',
       author='Michael Spector',
