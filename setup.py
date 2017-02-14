6e1521551a5ee886a3aa9bd56b044da84c203cac
mspector <michael.l.spector@gmail.com>
2017-02-14 17:06:06 -0500

    add multiReply and taskCompletionCount to invitations (#30)
    * add multiReply and taskCompletionCount to invitations
    
    * remove comma

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='OpenReviewPy',
-      version='0.5.31',
+      version='0.5.32',
       description='OpenReview client library',
       url='https://github.com/iesl/OpenReviewPy',
       author='Michael Spector',
