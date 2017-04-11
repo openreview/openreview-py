b4da242047ec352ab6115195c2de2b2fa1982e7e
Michael Spector <michaelspector@gmail.com>
2017-04-11 14:12:05 -0400

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='openreview-py',
-      version='0.5.4.5',
+      version='0.5.4.6',
       description='OpenReview client library',
       url='https://github.com/iesl/openreview-py',
       author='Michael Spector, Melisa Bok',
