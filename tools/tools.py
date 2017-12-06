c1c9168e98595ab84eaf488f3417e9b76a570ca8
Melisa Bok <melisabok@gmail.com>
2017-12-06 17:13:05 -0500

    check if profile exists

--- tools/tools.py
+++ tools/tools.py
@@ -3,34 +3,50 @@ from openreview import *
 
 super_user_id = 'OpenReview.net'
 
+def get_profile(client, value):
+    profile = None
+    try:
+        profile = client.get_profile(value)
+    except openreview.OpenReviewException as e:
+        # throw an error if it is something other than "not found"
+        if e[0][0] != 'Profile not found':
+            print "OpenReviewException: {0}".format(e)
+            return e
+    return profile
+
 def create_profile(client, email, first, last, middle = None):
 
-	response = client.get_tildeusername(first, last, middle)
-	tilde_id = response['username']
+	profile = get_profile(client, email)
 
-	if tilde_id.endswith(last + '1'):
+	if not profile:
+		response = client.get_tildeusername(first, last, middle)
+		tilde_id = response['username']
 
-		tilde_group = Group(id = tilde_id, signatures = [super_user_id], signatories = [tilde_id], readers = [tilde_id], writers = [super_user_id], members = [email])
-		email_group = Group(id = email, signatures = [super_user_id], signatories = [email], readers = [email], writers = [super_user_id], members = [tilde_id])
-		profile_content = {
-	        'emails': [email],
-	        'preferred_email': email,
-	        'names': [
-	            {
-	                'first': first,
-	                'middle': middle,
-	                'last': last,
-	                'username': tilde_id
-	            }
-	        ]
-	    }
-		client.post_group(tilde_group)
-		client.post_group(email_group)
-		profile = client.post_profile(tilde_id, profile_content)
+		if tilde_id.endswith(last + '1'):
 
-		return profile
+			tilde_group = Group(id = tilde_id, signatures = [super_user_id], signatories = [tilde_id], readers = [tilde_id], writers = [super_user_id], members = [email])
+			email_group = Group(id = email, signatures = [super_user_id], signatories = [email], readers = [email], writers = [super_user_id], members = [tilde_id])
+			profile_content = {
+		        'emails': [email],
+		        'preferred_email': email,
+		        'names': [
+		            {
+		                'first': first,
+		                'middle': middle,
+		                'last': last,
+		                'username': tilde_id
+		            }
+		        ]
+		    }
+			client.post_group(tilde_group)
+			client.post_group(email_group)
+			profile = client.post_profile(tilde_id, profile_content)
 
+			return profile
+
+		else:
+			raise OpenReviewException('There is already a profile with this first, middle and last name')
 	else:
-		raise OpenReviewException('There is already a profile with this first, middle and last name')
+		raise OpenReviewException('There is already a profile with this email: ' + email)
 
 
