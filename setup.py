315830ce006d1341846f987b1f313c966d7bf7e8
Melisa Bok <melisabok@gmail.com>
2017-12-08 13:43:01 -0500

    upgrade version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='openreview-py',
-      version='0.6.3',
+      version='0.6.5',
       description='OpenReview client library',
       url='https://github.com/iesl/openreview-py',
       author='Michael Spector, Melisa Bok',
