dd9da3e5f5d8c3eb0d2801edbd1b6283fdfab5b4
Michael Spector <michaelspector@gmail.com>
2018-02-02 16:19:59 -0500

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='openreview-py',
-      version='0.6.7',
+      version='0.7.0',
       description='OpenReview client library',
       url='https://github.com/iesl/openreview-py',
       author='Michael Spector, Melisa Bok',
