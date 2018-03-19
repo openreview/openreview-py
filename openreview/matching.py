a0be298395e58dd447602f6432ea76c8ff527b51
Melisa Bok <melisabok@gmail.com>
2018-03-19 18:09:04 -0400

    get domains function

--- openreview/matching.py
+++ openreview/matching.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import openreview
-import tools
 
 def get_conflicts(author_profiles, user_profile):
 
@@ -28,21 +27,36 @@ def get_profile_conflicts(profile):
 
     profile_domains = []
     for e in profile.content['emails']:
-        profile_domains += tools.subdomains(e)
+        profile_domains += get_domains(e, subdomains = True)
     domain_conflicts.update(profile_domains)
 
     institution_domains = []
     for h in profile.content.get('history', []):
         domain = h.get('institution', {}).get('domain', None)
         if domain:
-            institution_domains += tools.subdomains(domain)
+            institution_domains += get_domains(domain, subdomains = True)
     domain_conflicts.update(institution_domains)
 
     domain_conflicts.update(institution_domains)
 
-    relation_conflicts.update([r['email'] for r in profile.content.get('relations', [])])
+    relation_conflicts.update([get_domains(r['email'], subdomains = False) for r in profile.content.get('relations', [])])
 
     if 'gmail.com' in domain_conflicts:
         domain_conflicts.remove('gmail.com')
 
     return (domain_conflicts, relation_conflicts)
+
+def get_domains(entity, subdomains = False):
+
+    if '@' in entity:
+        full_domain = entity.split('@')[1]
+    else:
+        full_domain = entity
+
+    if subdomains:
+        domain_components = full_domain.split('.')
+        domains = ['.'.join(domain_components[index:len(domain_components)]) for index, path in enumerate(domain_components)]
+        valid_domains = [d for d in domains if '.' in d]
+        return valid_domains
+    else:
+        return full_domain
