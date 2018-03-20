1de635455d77fde0fc84b7bdaeca29b64f556d84
Melisa Bok <melisabok@gmail.com>
2018-03-20 10:55:19 -0400

    Fix/conflicts (#92)
    * fix tabs
    
    * get domains function
    
    * use full email address for relation conflicts
    
    * add profile email to the relation conflicts

--- openreview/matching.py
+++ openreview/matching.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import openreview
-import tools
 
 def get_conflicts(author_profiles, user_profile):
 
@@ -15,13 +14,12 @@ def get_conflicts(author_profiles, user_profile):
         authors_domain_conflicts.update(author_domain_conflicts)
         authors_relation_conflicts.update(author_relation_conflicts)
 
+    user_domain_conflicts, user_relation_conflicts = get_profile_conflicts(user_profile)
 
-	user_domain_conflicts, user_relation_conflicts = get_profile_conflicts(user_profile)
+    conflicts.update(authors_domain_conflicts.intersection(user_domain_conflicts))
+    conflicts.update(authors_relation_conflicts.intersection(user_relation_conflicts))
 
-	conflicts.update(authors_domain_conflicts.intersection(user_domain_conflicts))
-	conflicts.update(authors_relation_conflicts.intersection(user_relation_conflicts))
-
-	return list(conflicts)
+    return list(conflicts)
 
 def get_profile_conflicts(profile):
     domain_conflicts = set()
@@ -29,21 +27,37 @@ def get_profile_conflicts(profile):
 
     profile_domains = []
     for e in profile.content['emails']:
-    	profile_domains += tools.subdomains(e)
+        profile_domains += get_domains(e, subdomains = True)
     domain_conflicts.update(profile_domains)
 
     institution_domains = []
     for h in profile.content.get('history', []):
-    	domain = h.get('institution', {}).get('domain', None)
-    	if domain:
-    		institution_domains += tools.subdomains(domain)
+        domain = h.get('institution', {}).get('domain', None)
+        if domain:
+            institution_domains += get_domains(domain, subdomains = True)
     domain_conflicts.update(institution_domains)
 
     domain_conflicts.update(institution_domains)
 
     relation_conflicts.update([r['email'] for r in profile.content.get('relations', [])])
+    relation_conflicts.update(profile.content['emails'])
 
     if 'gmail.com' in domain_conflicts:
         domain_conflicts.remove('gmail.com')
 
     return (domain_conflicts, relation_conflicts)
+
+def get_domains(entity, subdomains = False):
+
+    if '@' in entity:
+        full_domain = entity.split('@')[1]
+    else:
+        full_domain = entity
+
+    if subdomains:
+        domain_components = full_domain.split('.')
+        domains = ['.'.join(domain_components[index:len(domain_components)]) for index, path in enumerate(domain_components)]
+        valid_domains = [d for d in domains if '.' in d]
+        return valid_domains
+    else:
+        return full_domain
