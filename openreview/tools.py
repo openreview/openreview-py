ddc2496011942b2cca8046bafdfd6b9c5d2e74a1
Pam Mandler <mandler@cs.umass.edu>
2018-03-27 10:42:26 -0400

    Add timestamp_GMT

--- openreview/tools.py
+++ openreview/tools.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import openreview
 import re
+import datetime
 
 super_user_id = 'OpenReview.net'
 
@@ -406,5 +407,8 @@ def assign(client, paper_number, conference,
 
     if reviewer_to_add:
         add_assignment(reviewer_to_add, parent_group, individual_groups, unassigned_individual_groups)
+
+def timestamp_GMT(year, month, day, hour=0, minute=0, second=0):
+    return int((datetime.datetime(year, month, day, hour, minute, second) - datetime.datetime(1970, 1, 1)).total_seconds() * 1000)
 
 
