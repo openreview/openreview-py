d6a5cc88bdeb8251f938a6ef517af6dad0f14cb9
Mohit Uniyal <mohituniyal16apr@gmail.com>
2018-07-13 16:11:04 -0400

    Feature/get all venues (#135)
    * Added function get_all_venues in tool.py
    
    * Adding exception handing for the get_groups calls
    
    * Added __pycache__ to gitignore & a test_tools file to test get_all_venues
    
    * Removed exception handing from get_all_venues, updated test

---
+++ tests/test_tools.py
@@ -0,0 +1,14 @@
+from openreview import *
+
+class TestTools():
+
+    def setup_method(self, method):
+        # Password should be saved in the environment variable OPENREVIEW_PASSWORD
+        self.client = openreview.Client(baseurl = 'https://dev.openreview.net', username = "OpenReview.net")
+        assert self.client is not None, "Client is none"
+
+    def test_get_all_venues(self):
+        venues = get_all_venues(self.client)
+        assert venues, "Venues could not be retrieved"
+
+
