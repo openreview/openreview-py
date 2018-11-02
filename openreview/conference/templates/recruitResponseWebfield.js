4f4c317cf2c9ec23a2cfb9d32dc812caf8aba2bb
Melisa Bok <melisabok@gmail.com>
2018-11-02 12:25:14 -0400

    update recruit template

--- openreview/conference/templates/recruitResponseWebfield.js
+++ openreview/conference/templates/recruitResponseWebfield.js
@@ -18,24 +18,30 @@ function renderConferenceHeader() {
 }
 
 function render() {
-  var accepted = args.response === 'Yes';
-  var message = accepted ?
-    'Thank you for accepting the invitation!' :
-    'You have declined the invitation.';
 
   var $response = $('#notes');
-  $response.empty().append('<div class="panel"><div class="row"><strong>' + message + '</strong></div></div>');
+  $response.empty();
 
-  if (accepted) {
-    // Display response text
-    $response.append([
-      '<div class="panel">',
-        '<div class="row">',
-          '<p>If you do not already have an OpenReview account, please sign up <a href="/signup">here</a>.</p>',
-          '<p>If you have an existing OpenReview account, please ensure that the email address that received this invitation is linked to your <a href="/profile?mode=edit">profile page</a> and has been confirmed.</p>',
-        '</div>',
-      '</div>'
-    ].join('\n'));
+  if (args.response) {
+    var accepted = (args.response === 'Yes');
+    var message = accepted ? 'Thank you for accepting the invitation!' : 'You have declined the invitation.';
+
+    $response.append('<div class="panel"><div class="row"><strong>' + message + '</strong></div></div>');
+
+    if (accepted) {
+      // Display response text
+      $response.append([
+        '<div class="panel">',
+          '<div class="row">',
+            '<p>If you do not already have an OpenReview account, please sign up <a href="/signup">here</a>.</p>',
+            '<p>If you have an existing OpenReview account, please ensure that the email address that received this invitation is linked to your <a href="/profile?mode=edit">profile page</a> and has been confirmed.</p>',
+          '</div>',
+        '</div>'
+      ].join('\n'));
+    }
+
+  } else {
+    promptError('Response parameter missing');
   }
 
   Webfield.ui.done();
