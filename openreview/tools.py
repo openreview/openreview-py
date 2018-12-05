b0cd95dead71f9d56a091d5a47f4f0ac06a53fd0
David Marshall <marshall62@users.noreply.github.com>
2018-12-05 08:50:42 -0500

    Fix/add matcher conflict utils (#193)
    * Added optional token argument to Client constructor
    
    * Removed functions from matcher utils.py and put them in tools.
    These functions are no longer used by the matcher but they are being used by scripts in the
    or-scripts repo.
    
    * increased version so that this will be released.

--- openreview/tools.py
+++ openreview/tools.py
@@ -1047,3 +1047,65 @@ def fill_template(template, paper):
 
     return new_template
 
+
+def get_conflicts(author_profiles, user_profile):
+    author_domains = set()
+    author_emails = set()
+    author_relations = set()
+
+    for author_email, profile in author_profiles.items():
+        author_info = get_author_info(profile, author_email)
+
+        author_domains.update(author_info['domains'])
+        author_emails.update(author_info['emails'])
+        author_relations.update(author_info['relations'])
+
+    user_info = get_profile_info(user_profile)
+
+    conflicts = set()
+    conflicts.update(author_domains.intersection(user_info['domains']))
+    conflicts.update(author_relations.intersection(user_info['emails']))
+    conflicts.update(author_emails.intersection(user_info['relations']))
+    conflicts.update(author_emails.intersection(user_info['emails']))
+
+    return list(conflicts)
+
+
+def get_author_info(profile, email):
+    if profile:
+        return get_profile_info(profile)
+    else:
+        return {
+            'domains': openreview.tools.subdomains(email),
+            'emails': [email],
+            'relations': []
+        }
+
+
+def get_profile_info(profile):
+    domains = set()
+    emails = set()
+    relations = set()
+
+    ## Emails section
+    for email in profile.content['emails']:
+        domains.update(openreview.tools.subdomains(email))
+        emails.add(email)
+
+    ## Institution section
+    for h in profile.content.get('history', []):
+        domain = h.get('institution', {}).get('domain', '')
+        domains.update(openreview.tools.subdomains(domain))
+
+    ## Relations section
+    relations.update([r['email'] for r in profile.content.get('relations', [])])
+
+    ## Filter common domains
+    if 'gmail.com' in domains:
+        domains.remove('gmail.com')
+
+    return {
+        'domains': domains,
+        'emails': emails,
+        'relations': relations
+    }
