d41907f72fe658d2e1dc5353ae06d9e994a2a6e9
Zach Bialecki <zach.bialecki@gmail.com>
2019-02-22 14:39:48 -0500

    Simplify and refactor recommendation webfield

--- openreview/conference/templates/recommendationWebfield.js
+++ openreview/conference/templates/recommendationWebfield.js
@@ -15,24 +15,54 @@ function main() {
 
   Webfield.ui.header(HEADER.title, HEADER.instructions);
 
-  renderConferenceTabs();
-
   Webfield.ui.spinner('#notes', { inline: true });
 
-  Webfield.get('/groups', {
-    member: user.id, regex: AREACHAIR_WILDCARD
-  })
-  .then(function(result) {
-    return getPaperNumbersfromGroups(result.groups);
-  })
-  .then(load)
-  .then(renderContent)
-  .then(Webfield.ui.done);
+  load().then(renderContent).then(Webfield.ui.done);
 }
 
-// Util functions
-var getNumberfromGroup = function(groupId, name) {
 
+// Perform all the required API calls
+function load() {
+  return Webfield.get('/groups', {
+    member: user.id, regex: AREACHAIR_WILDCARD
+  }).then(function(result) {
+    var noteNumbers = getPaperNumbersFromGroups(result.groups);
+
+    var notesP;
+    if (noteNumbers.length) {
+      var noteNumbersStr = noteNumbers.join(',');
+
+      notesP = Webfield.getAll('/notes', {
+        invitation: BLIND_SUBMISSION_ID, number: noteNumbersStr, details: 'tags'
+      }).then(function(allNotes) {
+        return allNotes.map(function(note) {
+          note.details.tags = note.details.tags.filter(function(tag) {
+            return tag.tauthor;
+          });
+          return note;
+        });
+      });
+    } else {
+      notesP = $.Deferred().resolve([]);
+    }
+
+    var tagInvitationsP = Webfield.getAll('/invitations', {
+      regex: CONFERENCE_ID + '/-/Paper.*/Recommendation', tags: true, invitee: true
+    });
+
+    return $.when(notesP, tagInvitationsP);
+  });
+}
+
+
+// Util functions
+function getPaperNumbersFromGroups(groups) {
+  return _.filter(_.map(groups, function(group) {
+    return getNumberFromGroup(group.id, 'Paper');
+  }), _.isInteger);
+}
+
+function getNumberFromGroup(groupId, name) {
   var tokens = groupId.split('/');
   paper = _.find(tokens, function(token) { return token.startsWith(name); });
   if (paper) {
@@ -40,70 +70,31 @@ var getNumberfromGroup = function(groupId, name) {
   } else {
     return null;
   }
-};
-
-var getPaperNumbersfromGroups = function(groups) {
-  return _.filter(_.map(groups, function(group) {
-    return getNumberfromGroup(group.id, 'Paper');
-  }), _.isInteger);
-};
-
-function renderConferenceTabs() {
-  var sections = [
-    {
-      heading: 'Your assigned papers',
-      id: 'all-submissions',
-    }
-  ];
-
-  Webfield.ui.tabPanel(sections, {
-    container: '#notes',
-    hidden: true
-  });
 }
 
 
-// Perform all the required API calls
-function load(noteNumbers) {
-
-  var notesP = $.Deferred().resolve([]);
-
-  if (noteNumbers.length) {
-    var noteNumbersStr = noteNumbers.join(',');
-
-    notesP = Webfield.getAll('/notes', {invitation: BLIND_SUBMISSION_ID, number: noteNumbersStr, details: 'tags'}).then(function(allNotes) {
-      return allNotes.map(function(note) {
-        note.details.tags = note.details.tags.filter(function(tag) {
-          return tag.tauthor;
-        });
-        return note;
-      });
-    });
-  }
-
-  var tagInvitationsP = Webfield.getAll('/invitations', { regex: CONFERENCE_ID + '/-/Paper.*/Recommendation', tags: true, invitee: true });
-
-  return $.when(notesP, tagInvitationsP);
-}
-
-
-// Display the bid interface populated with loaded data
+// Display the recommend interface populated with loaded data
 function renderContent(notes, tagInvitations) {
 
-  $('#notes .tabs-container').remove();
-  var sections = [
-    {
-      heading: 'Your assigned papers',
-      id: 'all-submissions',
-    }
-  ];
+  // Nothing to dispay
+  if (!notes.length) {
+    $('#notes').empty();
+    $('#notes').append('<p class="empty-message">You have no assigned papers at this time.</p>');
+    return;
+  }
+
+  // Set up tabs
+  var sections = [{
+    heading: 'Your Assigned Papers',
+    id: 'all-submissions',
+  }];
 
   Webfield.ui.tabPanel(sections, {
     container: '#notes',
     hidden: true
   });
 
-  // All Submitted Papers tab
+  // Your Assigned Papers tab
   var submissionListOptions = {
     pdfLink: true,
     showContents: true,
@@ -112,38 +103,29 @@ function renderContent(notes, tagInvitations) {
     container: '#all-submissions'
   };
 
-  $(submissionListOptions.container).empty();
-
-  if (notes.length){
-    Webfield.ui.submissionList(notes, {
-      heading: null,
-      container: '#all-submissions',
-      search: {
-        enabled: true,
-        localSearch: true,
-        subjectAreas: SUBJECT_AREAS,
-        subjectAreaDropdown: 'basic',
-        onResults: function(searchResults) {
-          Webfield.ui.searchResults(searchResults, submissionListOptions);
-          Webfield.disableAutoLoading();
-        },
-        onReset: function() {
-          Webfield.ui.searchResults(notes, submissionListOptions);
-        }
+  Webfield.ui.submissionList(notes, {
+    heading: null,
+    container: submissionListOptions.container,
+    search: {
+      enabled: true,
+      localSearch: true,
+      subjectAreas: SUBJECT_AREAS,
+      subjectAreaDropdown: 'basic',
+      onResults: function(searchResults) {
+        Webfield.ui.searchResults(searchResults, submissionListOptions);
+        Webfield.disableAutoLoading();
       },
-      displayOptions: submissionListOptions,
-      fadeIn: false
-    });
-
-  } else {
-    $('.tabs-container a[href="#all-submissions"]').parent().hide();
-  }
-
+      onReset: function() {
+        Webfield.ui.searchResults(notes, submissionListOptions);
+      }
+    },
+    displayOptions: submissionListOptions,
+    fadeIn: false
+  });
 
   $('#notes > .spinner-container').remove();
   $('#notes .tabs-container').show();
 
-  Webfield.ui.done();
 }
 
 // Go!
