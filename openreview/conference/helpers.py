4ba9e032cfe7ea5b0729453fd8161871747afeb4
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-05-03 16:25:27 -0400

    change occurences of conference to venue

--- openreview/conference/helpers.py
+++ openreview/conference/helpers.py
@@ -14,11 +14,11 @@ def get_conference(client, request_form_id):
     builder = openreview.conference.ConferenceBuilder(client)
 
     conference_start_date_str = 'TBD'
-    if note.content.get('Conference Start Date'):
+    if note.content.get('Venue Start Date'):
         try:
-            conference_start_date = datetime.datetime.strptime(note.content.get('Conference Start Date'), '%Y/%m/%d %H:%M')
+            conference_start_date = datetime.datetime.strptime(note.content.get('Venue Start Date'), '%Y/%m/%d %H:%M')
         except ValueError:
-            conference_start_date = datetime.datetime.strptime(note.content.get('Conference Start Date'), '%Y/%m/%d')
+            conference_start_date = datetime.datetime.strptime(note.content.get('Venue Start Date'), '%Y/%m/%d')
         conference_start_date_str = conference_start_date.strftime('%b %d %Y')
 
     submission_start_date_str = ''
@@ -40,18 +40,18 @@ def get_conference(client, request_form_id):
         submission_due_date_str = submission_due_date.strftime('%b %d %Y %I:%M%p')
 
     builder.set_conference_id(note.content.get('venue_id') if note.content.get('venue_id', None) else note.content.get('conference_id'))
-    builder.set_conference_name(note.content['Official Conference Name'])
-    builder.set_conference_short_name(note.content['Abbreviated Conference Name'])
+    builder.set_conference_name(note.content['Official Venue Name'])
+    builder.set_conference_short_name(note.content['Abbreviated Venue Name'])
     builder.set_homepage_header({
     'title': note.content['title'],
-    'subtitle': note.content['Abbreviated Conference Name'],
+    'subtitle': note.content['Abbreviated Venue Name'],
     'deadline': 'Submission Start: ' + submission_start_date_str + ', End: ' + submission_due_date_str,
     'date': conference_start_date_str,
     'website': note.content['Official Website URL'],
-    'location': note.content.get('Conference Location')
+    'location': note.content.get('Location')
     })
 
-    if 'Yes, our conference has Area Chairs' == note.content.get('Area Chairs (Metareviewers)', ''):
+    if 'Yes, our venue has Area Chairs' == note.content.get('Area Chairs (Metareviewers)', ''):
         builder.has_area_chairs(True)
 
     if 'Double-blind' == note.content.get('Author and Reviewer Anonymity', ''):
