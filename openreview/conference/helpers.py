d97a64d1b933cd4713743286820703a459801104
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-05-08 14:14:43 -0400

    no need to check data type

--- openreview/conference/helpers.py
+++ openreview/conference/helpers.py
@@ -67,7 +67,7 @@ def get_conference(client, request_form_id):
     conference = builder.get_result()
 
     submission_additional_options = note.content.get('Additional Submission Options', {})
-    if submission_additional_options and isinstance(submission_additional_options, str):
+    if submission_additional_options:
             submission_additional_options = json.loads(submission_additional_options)
 
     conference.open_submissions(start_date = submission_start_date, due_date = submission_due_date, additional_fields = submission_additional_options)
