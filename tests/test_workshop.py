ee996bb3dd3e51ca4a540800b834bda762be65f4
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-05-15 16:26:05 -0400

    test fixed

--- tests/test_workshop.py
+++ tests/test_workshop.py
@@ -375,17 +375,17 @@ class TestWorkshop():
         assert len(process_logs) == 1
         assert process_logs[0]['status'] == 'ok'
 
-        messages = client.get_messages(subject = '.*ICAPS HSDIP 2019.*Your submission has received a comment. Paper Title')
+        messages = client.get_messages(subject = '.*ICAPS HSDIP 2019.*Your submission has received a comment. Paper Number: 1, Paper Title')
         assert len(messages) == 3
         recipients = [m['content']['to'] for m in messages]
         assert 'test@mail.com' in recipients
         assert 'peter@mail.com' in recipients
         assert 'andrew@mail.com' in recipients
 
-        messages = client.get_messages(subject = '.*ICAPS HSDIP 2019.*Comment posted to a paper you are reviewing. Paper Number')
+        messages = client.get_messages(subject = '.*ICAPS HSDIP 2019.*Comment posted to a paper you are reviewing. Paper Number: 1, Paper Number')
         assert len(messages) == 0
 
-        messages = client.get_messages(subject = '.*ICAPS HSDIP 2019.*Comment posted to a paper in your area. Paper Number')
+        messages = client.get_messages(subject = '.*ICAPS HSDIP 2019.*Comment posted to a paper in your area. Paper Number: 1, Paper Number')
         assert len(messages) == 0
 
         messages = client.get_messages(subject = '.*ICAPS HSDIP 2019.*A comment was posted. Paper Number')
