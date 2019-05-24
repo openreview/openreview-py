b35e180c7dfcc556d454767e7fa94add510e1d35
Melisa Bok <melisabok@gmail.com>
2019-05-24 14:49:44 -0400

    Fix process function

--- openreview/conference/templates/commentProcess.js
+++ openreview/conference/templates/commentProcess.js
@@ -11,17 +11,19 @@ function(){
     .then(function(result) {
 
       var forumNote = result.notes[0];
+      var ignoreGroups = note.nonreaders || [];
+      ignoreGroups.push(note.tauthor);
 
       var ac_mail = {
         groups: [AREA_CHAIRS_ID],
-        ignoreGroups: [note.tauthor],
+        ignoreGroups: ignoreGroups,
         subject: '[' + SHORT_PHRASE + '] Comment posted to a paper in your area. Paper Number: ' + forumNote.number + ', Paper Title: "' + forumNote.content.title + '"',
         message: 'A comment was posted to a paper for which you are serving as Area Chair.\n\nPaper Number: ' + forumNote.number + '\n\nPaper Title: "' + forumNote.content.title + '"\n\nComment title: ' + note.content.title + '\n\nComment: ' + note.content.comment + '\n\nTo view the comment, click here: ' + baseUrl + '/forum?id=' + note.forum + '&noteId=' + note.id
       };
 
       var reviewer_mail = {
         groups: [REVIEWERS_ID],
-        ignoreGroups: [note.tauthor],
+        ignoreGroups: ignoreGroups,
         subject: '[' + SHORT_PHRASE + '] Comment posted to a paper you are reviewing. Paper Number: ' + forumNote.number + ', Paper Title: "' + forumNote.content.title + '"',
         message: 'A comment was posted to a paper for which you are serving as reviewer.\n\nPaper Number: ' + forumNote.number + '\n\nPaper Title: "' + forumNote.content.title + '"\n\nComment title: ' + note.content.title + '\n\nComment: ' + note.content.comment + '\n\nTo view the comment, click here: ' + baseUrl + '/forum?id=' + note.forum + '&noteId=' + note.id
       };
@@ -34,7 +36,7 @@ function(){
 
       var paper_author_mail = {
         groups: forumNote.content.authorids,
-        ignoreGroups: [note.tauthor],
+        ignoreGroups: ignoreGroups,
         subject: '[' + SHORT_PHRASE + '] Your submission has received a comment. Paper Number: ' + forumNote.number + ', Paper Title: "' + forumNote.content.title + '"',
         message: 'Your submission to ' + SHORT_PHRASE + ' has received a comment.\n\nPaper Number: ' + forumNote.number + '\n\nPaper Title: "' + forumNote.content.title + '"\n\nComment title: ' + note.content.title + '\n\nComment: ' + note.content.comment + '\n\nTo view the comment, click here: ' + baseUrl + '/forum?id=' + note.forum + '&noteId=' + note.id
       };
@@ -57,8 +59,6 @@ function(){
 
       if(PROGRAM_CHAIRS_ID && (note.readers.includes(PROGRAM_CHAIRS_ID) || note.readers.includes('everyone'))){
 
-        var ignoreGroups = note.nonreaders || [];
-        ignoreGroups.push(note.tauthor);
         var pc_mail = {
           groups: [PROGRAM_CHAIRS_ID],
           ignoreGroups: ignoreGroups,
--- openreview/conference/templates/reviewProcess.js
+++ openreview/conference/templates/reviewProcess.js
@@ -18,10 +18,13 @@ function(){
       //TODO: use the variable instead, when we have anonymous groups integrated
       var REVIEWERS_ID = CONFERENCE_ID + '/Paper' + forum.number + '/Reviewers';
       var AREA_CHAIRS_ID = CONFERENCE_ID + '/Paper' + forum.number + '/Area_Chairs';
+      var ignoreGroups = note.nonreaders || [];
+      ignoreGroups.push(note.tauthor);
 
       if (PROGRAM_CHAIRS_ID){
         var program_chair_mail = {
           groups: [PROGRAM_CHAIRS_ID],
+          ignoreGroups: ignoreGroups,
           subject: '[' + SHORT_PHRASE + '] A review has been received on Paper number: ' + forum.number + ', Paper title: "' + forum.content.title + '"',
           message: 'We have received a review on a submission to ' + SHORT_PHRASE + '.\n\nPaper number: ' + forum.number + ', \n\nPaper title: ' + forum.content.title + '\n\nReview title: ' + note.content.title + '\n\nReview comment: ' + note.content.review + '\n\nTo view the review, click here: ' + baseUrl + '/forum?id=' + note.forum + '&noteId=' + note.id
         };
@@ -38,6 +41,7 @@ function(){
       if (AREA_CHAIRS_ID && (note.readers.includes('everyone') || note.readers.includes(AREA_CHAIRS_ID))) {
         var areachair_mail = {
           groups: [AREA_CHAIRS_ID],
+          ignoreGroups: ignoreGroups,
           subject : '[' + SHORT_PHRASE + '] Review posted to your assigned Paper number: ' + forum.number + ', Paper title: "' + forum.content.title + '"',
           message: 'A submission to ' + SHORT_PHRASE + ', for which you are an official area chair, has received a review. \n\nPaper number: ' + forum.number + '\n\nPaper title: ' + forum.content.title + '\n\nReview title: ' + note.content.title + '\n\nReview comment: ' + note.content.review + '\n\nTo view the review, click here: ' + baseUrl + '/forum?id=' + note.forum + '&noteId=' + note.id
         };
@@ -48,7 +52,7 @@ function(){
       if (note.readers.includes('everyone') || note.readers.includes(REVIEWERS_ID)){
         var reviewer_mail = {
           groups: [REVIEWERS_ID],
-          ignoreGroups: [note.tauthor],
+          ignoreGroups: ignoreGroups,
           subject: '[' + SHORT_PHRASE + '] Review posted to your assigned Paper number: ' + forum.number + ', Paper title: "' + forum.content.title + '"',
           message: 'A submission to ' + SHORT_PHRASE + ', for which you are a reviewer, has received a review. \n\nPaper number: ' + forum.number + '\n\nPaper title: ' + forum.content.title + '\n\nReview title: ' + note.content.title + '\n\nReview comment: ' + note.content.review + '\n\nTo view the review, click here: ' + baseUrl + '/forum?id=' + note.forum + '&noteId=' + note.id
         };
@@ -56,7 +60,7 @@ function(){
       } else if (note.readers.includes(reviewers_submitted)) {
         var reviewer_mail = {
           groups: [reviewers_submitted],
-          ignoreGroups: [note.tauthor],
+          ignoreGroups: ignoreGroups,
           subject: '[' + SHORT_PHRASE + '] Review posted to your assigned Paper number: ' + forum.number + ', Paper title: "' + forum.content.title + '"',
           message: 'A submission to ' + SHORT_PHRASE + ', for which you are a reviewer, has received a review. \n\nPaper number: ' + forum.number + '\n\nPaper title: ' + forum.content.title + '\n\nReview title: ' + note.content.title + '\n\nReview comment: ' + note.content.review + '\n\nTo view the review, click here: ' + baseUrl + '/forum?id=' + note.forum + '&noteId=' + note.id
         };
@@ -66,6 +70,7 @@ function(){
       if (note.readers.includes('everyone') || note.readers.includes(AUTHORS_ID)) {
         var author_mail = {
           groups: [AUTHORS_ID],
+          ignoreGroups: ignoreGroups,
           subject: '[' + SHORT_PHRASE + '] Review posted to your submission - Paper number: ' + forum.number + ', Paper title: "' + forum.content.title + '"',
           message: 'Your submission to ' + SHORT_PHRASE + ' has received a review. \n\nTitle: ' + note.content.title + '\n\nComment: ' + note.content.review + '\n\nTo view the review, click here: ' + baseUrl + '/forum?id=' + note.forum + '&noteId=' + note.id
         };
