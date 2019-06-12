a94f5b88ae8dd0ed90482690f95cd12137f81eef
Pam Mandler <mandler@cs.umass.edu>
2019-06-12 14:21:34 -0400

    install pylatexenc

--- openreview/tools.py
+++ openreview/tools.py
@@ -10,7 +10,6 @@ import openreview
 import re
 import datetime
 import time
-import pylatexenc
 from pylatexenc.latexencode import utf8tolatex
 from Crypto.Hash import HMAC, SHA256
 from multiprocessing import Pool
