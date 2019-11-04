533334d3276034292174911086110f6b5eb2064c
Melisa Bok <melisabok@gmail.com>
2019-11-04 14:13:05 -0500

    Increase version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='openreview-py',
-      version='1.0.3',
+      version='1.0.4',
       description='OpenReview client library',
       url='https://github.com/openreview/openreview-py',
       author='Michael Spector, Melisa Bok, Pam Mander, Mohit Uniyal',
