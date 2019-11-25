fdf6f2d5ab2f27d2061eb1b54aa12d4887787eff
Mohit Uniyal <mohituniyal16apr@gmail.com>
2019-11-25 09:49:52 -0500

    fixes the issue of conf_start_date

--- openreview/conference/helpers.py
+++ openreview/conference/helpers.py
@@ -16,8 +16,8 @@ def get_conference(client, request_form_id):
     builder.set_request_form_id(request_form_id)
 
     conference_start_date_str = 'TBD'
-    start_date = note.content.get('Venue Start Date', note.content.get('Conference Start Date', '')).strip()
-    if start_date:
+    conference_start_date = note.content.get('Venue Start Date', note.content.get('Conference Start Date', '')).strip()
+    if conference_start_date:
         try:
             conference_start_date = datetime.datetime.strptime(start_date, '%Y/%m/%d %H:%M')
         except ValueError:
@@ -49,7 +49,8 @@ def get_conference(client, request_form_id):
     builder.set_conference_id(note.content.get('venue_id') if note.content.get('venue_id', None) else note.content.get('conference_id'))
     builder.set_conference_name(note.content.get('Official Venue Name', note.content.get('Official Conference Name')))
     builder.set_conference_short_name(note.content.get('Abbreviated Venue Name', note.content.get('Abbreviated Conference Name')))
-    builder.set_conference_year(conference_start_date.year)
+    if conference_start_date:
+        builder.set_conference_year(conference_start_date.year)
     builder.set_homepage_header({
     'title': note.content['title'],
     'subtitle': note.content.get('Abbreviated Venue Name', note.content.get('Abbreviated Conference Name')),
