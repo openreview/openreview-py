337b808b2b3497af1ce201734da1f8fc591d8693
Zach Bialecki <zach.bialecki@gmail.com>
2020-01-22 18:36:27 -0500

    Fix recommendation invitation values

--- openreview/conference/invitation.py
+++ openreview/conference/invitation.py
@@ -1160,7 +1160,7 @@ class InvitationBuilder(object):
                         'required': True
                     },
                     'weight': {
-                        'value-dropdown': [1, 2, 3, 4, 5, 6, 7, 9, 10],
+                        'value-dropdown': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
                         'required': True
                     }
                 }
