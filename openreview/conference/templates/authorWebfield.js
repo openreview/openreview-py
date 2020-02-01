68a1a06dcc7ee50939dc86e686cbf80d9ff59d39
Melisa Bok <melisabok@gmail.com>
2020-02-01 15:51:56 -0500

    Render a status table for author console

--- openreview/conference/templates/authorWebfield.js
+++ openreview/conference/templates/authorWebfield.js
@@ -116,18 +116,169 @@ function renderContent(authorNotes, invitations, edgeInvitations) {
   Webfield.ui.newTaskList(invitations, edgeInvitations, tasksOptions);
 
   // Your Private Versions and Your Anonymous Versions tabs
-  Webfield.ui.submissionList(authorNotes, {
-    heading: null,
-    container: '#your-submissions',
-    search: { enabled: false },
-    fadeIn: false,
-    displayOptions: paperDisplayOptions
-  });
+  // Webfield.ui.submissionList(authorNotes, {
+  //   heading: null,
+  //   container: '#your-submissions',
+  //   search: { enabled: false },
+  //   fadeIn: false,
+  //   displayOptions: paperDisplayOptions
+  // });
+
+  //Render table like AC console
+  renderStatusTable(authorNotes, {}, [], {}, '#your-submissions');
+
+  $('.author-console-table th').eq(0).css('width', '4%');
+  $('.author-console-table th').eq(1).css('width', '40%');
+  $('.author-console-table th').eq(2).css('width', '28%');
+  $('.author-console-table th').eq(3).css('width', '28%');
 
   // Remove spinner and show content
   $('#notes .spinner-container').remove();
   $('.tabs-container').show();
 }
+
+
+var renderStatusTable = function(notes, completedReviews, metaReviews, reviewerIds, container) {
+  var rows = _.map(notes, function(note) {
+    var revIds = reviewerIds[note.number] || Object.create(null);
+    var metaReview = _.find(metaReviews, ['invitation', 'replace_invitation_id']);
+    var noteCompletedReviews = completedReviews[note.number] || Object.create(null);
+
+    return buildTableRow(note, revIds, noteCompletedReviews, metaReview);
+  });
+
+  if (rows.length) {
+    renderTableRows(rows, container);
+  } else {
+    $(container).empty().append('<p class="empty-message">No papers to display at this time</p>');
+  }
+};
+
+var renderTableRows = function(rows, container) {
+  var templateFuncs = [
+    function(data) {
+      return '<strong class="note-number">' + data.number + '</strong>';
+    },
+    Handlebars.templates.noteSummary,
+    Handlebars.templates.noteReviewers,
+    Handlebars.templates.noteMetaReviewStatus
+  ];
+
+  var rowsHtml = rows.map(function(row) {
+    return row.map(function(cell, i) {
+      return templateFuncs[i](cell);
+    });
+  });
+
+  var tableHtml = Handlebars.templates['components/table']({
+    headings: [
+      '#', 'Paper Summary'
+      // Temporally hide review status
+      //, 'Review Progress', 'Meta Review Status'
+    ],
+    rows: rowsHtml,
+    extraClasses: 'author-console-table'
+  });
+
+  $('.table-container', container).remove();
+  $(container).append(tableHtml);
+
+}
+
+var buildTableRow = function(note, reviewerIds, completedReviews, metaReview) {
+  var cellCheck = { selected: false, noteId: note.id };
+
+  // Paper number cell
+  var cell0 = { number: note.number};
+
+  // Note summary cell
+  var cell1 = note;
+
+  // Review progress cell
+  var reviewObj;
+  var combinedObj = {};
+  var ratings = [];
+  var confidences = [];
+  for (var reviewerNum in reviewerIds) {
+    var reviewer = reviewerIds[reviewerNum];
+    if (reviewerNum in completedReviews) {
+      reviewObj = completedReviews[reviewerNum];
+      combinedObj[reviewerNum] = {
+        id: reviewer.id,
+        name: reviewer.name,
+        email: reviewer.email,
+        completedReview: true,
+        forum: reviewObj.forum,
+        note: reviewObj.id,
+        rating: reviewObj.rating,
+        confidence: reviewObj.confidence,
+        reviewLength: reviewObj.content.review.length
+      };
+      ratings.push(reviewObj.rating);
+      confidences.push(reviewObj.confidence);
+    } else {
+      var forumUrl = 'https://openreview.net/forum?' + $.param({
+        id: note.forum,
+        noteId: note.id
+      });
+      combinedObj[reviewerNum] = {
+        id: reviewer.id,
+        name: reviewer.name,
+        email: reviewer.email,
+        forum: note.forum,
+        forumUrl: forumUrl,
+        paperNumber: note.number,
+        reviewerNumber: reviewerNum
+      };
+    }
+  }
+
+  var averageRating = 'N/A';
+  var minRating = 'N/A';
+  var maxRating = 'N/A';
+  if (ratings.length) {
+    averageRating = _.round(_.sum(ratings) / ratings.length, 2);
+    minRating = _.min(ratings);
+    maxRating = _.max(ratings);
+  }
+
+  var averageConfidence = 'N/A';
+  var minConfidence = 'N/A';
+  var maxConfidence = 'N/A';
+  if (confidences.length) {
+    averageConfidence = _.round(_.sum(confidences) / confidences.length, 2);
+    minConfidence = _.min(confidences);
+    maxConfidence = _.max(confidences);
+  }
+
+  var cell2 = {
+    noteId: note.id,
+    paperNumber: note.number,
+    numSubmittedReviews: Object.keys(completedReviews).length,
+    numReviewers: Object.keys(reviewerIds).length,
+    reviewers: combinedObj,
+    averageRating: averageRating,
+    maxRating: maxRating,
+    minRating: minRating,
+    averageConfidence: averageConfidence,
+    minConfidence: minConfidence,
+    maxConfidence: maxConfidence,
+    sendReminder: false,
+    expandReviewerList: false,
+    enableReviewerReassignment : false
+  };
+
+  // Status cell
+  var cell3 = {};
+  if (metaReview) {
+    cell3.recommendation = metaReview.content.recommendation;
+    cell3.editUrl = '/forum?id=' + note.forum + '&noteId=' + metaReview.id;
+  }
+
+  // Temporally hide review status
+  //return [cell0, cell1, cell2, cell3];
+  return [cell0, cell1];
+};
 
 // Go!
 main();
