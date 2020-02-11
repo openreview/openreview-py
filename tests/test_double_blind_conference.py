237b35ba2d8051622a548d55d3378851f8e85902
xk <60613434+xkopenreview@users.noreply.github.com>
2020-02-11 14:46:17 -0500

    Update test_double_blind_conference.py
    added a test case in double blind conference for changed in 23b9c16, area chair reassign reviewers with conflicts to verify that all reviewers(3) has been filterd to show only 2 reviewers

--- tests/test_double_blind_conference.py
+++ tests/test_double_blind_conference.py
@@ -1334,6 +1334,36 @@ class TestDoubleBlindConference():
         assert 'Status' == tabs.find_element_by_id('paper-status').find_element_by_class_name('row-4').text
         assert 'Decision' == tabs.find_element_by_id('paper-status').find_element_by_class_name('row-5').text
 
+    def test_reassign_reviewers_with_conflict_of_interest(self,client,test_client,selenium,request_page):
+        builder=openreview.conference.ConferenceBuilder(client)
+        builder.set_conference_id('AKBC.ws/2019/Conference')
+        builder.set_submission_stage(double_blind = True, public = True)
+        builder.set_conference_short_name('AKBC 2019')
+        builder.has_area_chairs(True)
+        builder.enable_reviewer_reassignment(True)#enable review reassignment so that the assign_Reviewer_Textbox is rendered on page
+        conference = builder.get_result()
+
+        #area chair to reassign reviewer
+        ac_client=openreview.Client(baseurl = 'http://localhost:3000', username='ac@mail.com', password='1234')
+        request_page(selenium,"http://localhost:3000/group?id=AKBC.ws/2019/Conference/Area_Chairs",ac_client.token)
+
+        show_Reviewers_AnchorTag=selenium.find_element_by_xpath('//*[@id="1-reviewer-progress"]/a')
+        selenium.execute_script("arguments[0].click()",show_Reviewers_AnchorTag) #click on "show reviewers anchor tag"
+        time.sleep(5) #leave some time for the API and js to complete
+
+        #get the reviewers textbox and click on it to show dropdown
+        assign_Reviewer_Textbox_Present=EC.presence_of_element_located((By.XPATH,'//*[@id="1-add-reviewer"]/div/input'))
+        WebDriverWait(selenium,5).until(assign_Reviewer_Textbox_Present)
+        assign_Reviewer_Textbox=selenium.find_element_by_xpath('//*[@id="1-add-reviewer"]/div/input')
+        selenium.execute_script("arguments[0].click()",assign_Reviewer_Textbox)
+
+        #get the dropdown div and check how many dropdown options in it
+        dropdown_Options_Present=EC.presence_of_element_located((By.XPATH,'//*[@id="1-add-reviewer"]/div/div'))
+        WebDriverWait(selenium,5).until(dropdown_Options_Present)
+        dropdown_Options=selenium.find_element_by_xpath('//*[@id="1-add-reviewer"]/div/div')
+        assert len(dropdown_Options.find_elements_by_xpath('//*[@id="1-add-reviewer"]/div/div/div'))==2
+
+        
     def test_open_revise_submissions(self, client, test_client, helpers):
 
         builder = openreview.conference.ConferenceBuilder(client)
