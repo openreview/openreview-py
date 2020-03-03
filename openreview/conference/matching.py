de70eb9417e77648570c178ed588fc34e942ec2c
Carlos Mondragon <carlos@openreview.net>
2020-03-03 12:18:42 -0500

    Use get_edges_count for sanity check

--- openreview/conference/matching.py
+++ openreview/conference/matching.py
@@ -185,8 +185,7 @@ class Matching(object):
                     ))
         openreview.tools.post_bulk_edges(client=self.client, edges=edges)
         # Perform sanity check
-        edges_iter = openreview.tools.iterget_edges(self.client, invitation=invitation.id)
-        edges_posted = len(list(edges_iter))
+        edges_posted = self.client.get_edges_count(invitation=invitation.id)
         if edges_posted < len(edges):
             raise openreview.OpenReviewException('Failed during bulk post of Conflict edges! Scores found: {0}, Edges posted: {1}'.format(len(edges), edges_posted))
         return invitation
@@ -230,8 +229,7 @@ class Matching(object):
 
         openreview.tools.post_bulk_edges(client=self.client, edges=edges)
         # Perform sanity check
-        edges_iter = openreview.tools.iterget_edges(self.client, invitation=invitation.id)
-        edges_posted = len(list(edges_iter))
+        edges_posted = self.client.get_edges_count(invitation=invitation.id)
         if edges_posted < len(edges):
             raise openreview.OpenReviewException('Failed during bulk post of TPMS edges! Input file:{0}, Scores found: {1}, Edges posted: {2}'.format(tpms_score_file, len(edges), edges_posted))
         return invitation
@@ -263,8 +261,7 @@ class Matching(object):
 
         openreview.tools.post_bulk_edges(client=self.client, edges=edges)
         # Perform sanity check
-        edges_iter = openreview.tools.iterget_edges(self.client, invitation=invitation.id)
-        edges_posted = len(list(edges_iter))
+        edges_posted = self.client.get_edges_count(invitation=invitation.id)
         if edges_posted < len(edges):
             raise openreview.OpenReviewException('Failed during bulk post of {0} edges! Input file:{1}, Scores found: {2}, Edges posted: {3}'.format(score_invitation_id, score_file, len(edges), edges_posted))
         return invitation
@@ -300,8 +297,7 @@ class Matching(object):
 
         openreview.tools.post_bulk_edges(client=self.conference.client, edges=edges)
         # Perform sanity check
-        edges_iter = openreview.tools.iterget_edges(self.conference.client, invitation=invitation.id)
-        edges_posted = len(list(edges_iter))
+        edges_posted = self.conference.client.get_edges_count(invitation=invitation.id)
         if edges_posted < len(edges):
             raise openreview.OpenReviewException('Failed during bulk post of edges! Scores found: {0}, Edges posted: {1}'.format(len(edges), edges_posted))
         return invitation
