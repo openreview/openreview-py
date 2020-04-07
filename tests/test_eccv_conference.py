86c5f8927da306744f5cc7138d6fc29c290659f3
Zach Bialecki <zach.bialecki@gmail.com>
2020-04-07 19:34:13 -0400

    Fix tests for real

--- tests/test_eccv_conference.py
+++ tests/test_eccv_conference.py
@@ -428,7 +428,7 @@ Please contact info@openreview.net with any questions or concerns about this int
         notes = header.find_elements_by_class_name("description")
         assert notes
         assert len(notes) == 1
-        assert notes[0].text == 'This page provides information and status updates for the ECCV 2020. It will be regularly updated as the conference progresses, so please check back frequently.\nYou agreed to review up to 7 papers.'
+        assert notes[0].text == 'This page provides information and status updates for the ECCV 2020. It will be regularly updated as the conference progresses, so please check back frequently.\nYou have agreed to review up to 7 papers.'
 
         reviewer2_client = helpers.create_user('mohit+1@mail.com', 'Mohit', 'EccvReviewer')
         request_page(selenium, 'http://localhost:3000/group?id=thecvf.com/ECCV/2020/Conference/Reviewers', reviewer2_client.token)
@@ -437,7 +437,7 @@ Please contact info@openreview.net with any questions or concerns about this int
         notes = header.find_elements_by_class_name("description")
         assert notes
         assert len(notes) == 1
-        assert notes[0].text == 'This page provides information and status updates for the ECCV 2020. It will be regularly updated as the conference progresses, so please check back frequently.\nYou agreed to review up to 4 papers.'
+        assert notes[0].text == 'This page provides information and status updates for the ECCV 2020. It will be regularly updated as the conference progresses, so please check back frequently.\nYou have agreed to review up to 4 papers.'
 
         #Area Chairs
         conference.set_area_chairs(['test_ac_eccv@mail.com'])
