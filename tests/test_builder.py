424fddf239de45c45b379b4dbb5099eb71c22b6a
Mohit Uniyal <mohituniyal16apr@gmail.com>
2020-04-20 21:34:15 +0530

    used ui_test marker

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -44,7 +44,7 @@ class TestBuilder():
     def test_web_set_landing_page(self, client):
         builder = openreview.conference.ConferenceBuilder(client)
         builder.set_conference_id("ds.cs.umass.edu/Test_I/2019/Conference")
-        conference = builder.get_result()
+        builder.get_result()
         group = client.get_group(id='ds.cs.umass.edu/Test_I/2019')
         assert group.web,'Venue parent group missing webfield'
 
@@ -60,13 +60,14 @@ class TestBuilder():
         client.post_group(group)
 
         builder.set_conference_id("ds.cs.umass.edu/Test_I/2019/Workshop/WS_1")
-        conference = builder.get_result()
+        builder.get_result()
         # check webfield contains 'Conference', 'Party' and 'Workshop'
         group = client.get_group(id='ds.cs.umass.edu/Test_I/2019')
         assert 'ds.cs.umass.edu/Test_I/2019/Conference' in group.web, 'Venue parent group missing child conference group'
         assert 'ds.cs.umass.edu/Test_I/2019/Workshop' in group.web, 'Venue parent group missing child workshop group'
         assert 'Party' in group.web, 'Venue parent group missing child inserted group'
 
+    @pytest.mark.ui_test
     def test_modify_review_form(self, client, test_client, selenium, request_page, helpers):
 
         builder = openreview.conference.ConferenceBuilder(client)
@@ -91,7 +92,7 @@ class TestBuilder():
         conference.set_program_chairs()
         conference.set_reviewers(emails = ['reviewer_test1@mail.com'])
 
-        author_client = helpers.create_user('author_test1@mail.com', 'Test', 'Author')
+        helpers.create_user('author_test1@mail.com', 'Test', 'Author')
         note = openreview.Note(invitation = conference.get_submission_id(),
             readers = ['~Test_Author1', 'drew@mail.com', 'test.org/2019/Conference'],
             writers = [conference.id, '~Test_Author1', 'drew@mail.com'],
@@ -177,6 +178,7 @@ class TestBuilder():
         review_note = reviewer_client.post_note(note)
         assert review_note
 
+    @pytest.mark.ui_test
     def test_PC_console_sort_by_options(self, client, test_client, selenium, request_page, helpers):
 
         builder = openreview.conference.ConferenceBuilder(client)
