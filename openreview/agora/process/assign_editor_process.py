4ffcedb8276d304e0228248dce5db94155f8a48d
Melisa Bok <melisabok@gmail.com>
2020-05-16 15:26:52 -0400

    Update openreview/agora/process/assign_editor_process.py
    Co-authored-by: Zach Bialecki <86174+zbialecki@users.noreply.github.com>

--- openreview/agora/process/assign_editor_process.py
+++ openreview/agora/process/assign_editor_process.py
@@ -28,7 +28,7 @@ def process_update(client, note, invitation, existing_note):
         client.post_message(subject='[Agora/COVID-19] You have been assigned to be an editor of the article titled "{title}"'.format(title=article.content['title']),
             recipients=new_editors,
             message='''You have been assigned to be an editor of the paper titled "{title}" by {signature}, Editor-in-Chief of the Agora COVID-19 venue.
-Your can start assigning reviewers now.
+You may start assigning reviewers now.
 
 To view the article, click here: https://openreview.net/forum?id={forum}'''.format(title=article.content['title'], signature=note.signatures[0], forum=note.forum),
             ignoreRecipients=None,
