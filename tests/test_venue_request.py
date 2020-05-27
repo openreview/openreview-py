26592b6303b6c38ad267646fd71896a7982f6328
Mohit Uniyal <mohituniyal16apr@gmail.com>
2020-05-27 22:41:11 +0530

    adding test

--- tests/test_venue_request.py
+++ tests/test_venue_request.py
@@ -8,7 +8,7 @@ class TestVenueRequest():
 
     def test_venue_request_setup(self, client):
         
-        venue = VenueRequest(client)
+        venue = VenueRequest(client, 'openreview.net/Support', 'openreview.net')
 
         assert venue.support_group
         assert venue.bid_stage_super_invitation
@@ -21,5 +21,22 @@ class TestVenueRequest():
         assert venue.comment_super_invitation
         assert venue.recruitment_super_invitation
 
-    def test_post_venue_request(self, client):
-        pass
+    def test_post_venue_request(self, client, selenium, request_page):
+        
+        venue = VenueRequest(client, 'openreview.net/Support', 'openreview.net')
+        request_page(selenium, "http://localhost:3000/group?id=" + venue.support_group.id, client.token)
+
+        header_div = selenium.find_element_by_id('header')
+        assert header_div
+        assert header_div.find_element_by_tag_name("h1").text == 'Host a Venue'
+
+        request_invitation_div = selenium.find_element_by_id('invitation')
+        assert request_invitation_div
+        request_button_panel = request_invitation_div.find_element_by_tag_name('div')
+        request_button = request_button_panel.find_element_by_class_name('btn')
+        assert request_button.text == 'Support Request Form'
+
+        request_button.click()
+
+        request_form_div = selenium.find_element_by_class_name('note_editor')
+        assert request_form_div
