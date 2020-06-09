c1ac10553a1513a83600fea569b06002552a4752
Mohit Uniyal <mohituniyal16apr@gmail.com>
2020-06-09 18:09:13 +0530

    add only root

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,11 +14,11 @@
 #
 import os
 import sys
-sys.path.append(os.path.abspath('.'))
-sys.path.append(os.path.join(os.path.abspath('.'), '../../openreview/conference'))
-sys.path.append(os.path.join(os.path.abspath('.'), '../../openreview/invitations'))
-sys.path.append(os.path.join(os.path.abspath('.'), '../../openreview/agora'))
-sys.path.append(os.path.join(os.path.abspath('.'), '../../openreview'))
+sys.path.insert(0, os.path.abspath('../..'))
+# sys.path.append(os.path.join(os.path.abspath('.'), '../../openreview/conference'))
+# sys.path.append(os.path.join(os.path.abspath('.'), '../../openreview/invitations'))
+# sys.path.append(os.path.join(os.path.abspath('.'), '../../openreview/agora'))
+# sys.path.append(os.path.join(os.path.abspath('.'), '../../openreview'))
 
 # -- Project information -----------------------------------------------------
 
