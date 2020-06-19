25e6e5d9dae740bcebd5a80e87422f61bc06abf1
Mohit Uniyal <mohituniyal16apr@gmail.com>
2020-06-19 20:53:16 +0530

    Zach's comments

--- openreview/conference/templates/recruitResponseWebfield.js
+++ openreview/conference/templates/recruitResponseWebfield.js
@@ -26,7 +26,7 @@ function render() {
       // Display response text
       var message = 'Thank you for accepting this invitation from ' + HEADER.title;
       $response.append('<div class="panel"><div class="row"><h3 style="line-height:normal;">' + message + '</h3></div></div>');
-      var email = args.user.indexOf('@') > -1 ? '(<strong>' + args.user + '</strong>) ' : '';
+      var email = args.user.indexOf('@') > -1 ? '(<strong>' + args.user + '</strong>)' : '';
 
       $response.append([
         '<div class="panel">',
@@ -34,7 +34,7 @@ function render() {
             '<h4>Please complete the following steps now:</h4>',
               '<ol>',
                 '<li><p>Log in to your OpenReview account (if you do not already have an OpenReview account, you can sign up <a style="font-weight:bold;" href="/signup">here</a>).</p></li>',
-                '<li><p>Ensure that the email address ' + email + 'that received this invitation is linked to your <a style="font-weight:bold;" href="/profile?mode=edit">profile page</a> and has been confirmed.</p></li>',
+                '<li><p>Ensure that the email address ' + email + ' that received this invitation is linked to your <a style="font-weight:bold;" href="/profile?mode=edit">profile page</a> and has been confirmed.</p></li>',
                 '<li><p>Complete your pending <a style="font-weight:bold;" href="/tasks">tasks</a> (if any) for ' + HEADER.subtitle + '.</p></li>',
               '</ol>',
           '</div>',
