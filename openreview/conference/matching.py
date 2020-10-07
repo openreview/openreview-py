6bdab95754df5e6baad2ac6eae88000f382b9f64
Melisa Bok <melisabok@gmail.com>
2020-10-07 12:30:33 -0400

    add deploying statte

--- openreview/conference/matching.py
+++ openreview/conference/matching.py
@@ -458,6 +458,7 @@ class Matching(object):
                             'Error',
                             'No Solution',
                             'Complete',
+                            'Deploying',
                             'Deployed'
                         ],
                         'order': 16
