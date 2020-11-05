7f517aee6f9d843ab9b5ee17a943f41b1de42de8
Melisa Bok <melisabok@gmail.com>
2020-11-05 09:53:42 -0500

    add venue homepage

---
+++ tests/data/homepage.js
@@ -0,0 +1,256 @@
+// ------------------------------------
+// Venue homepage template
+//
+// This webfield displays the conference header (#header), the submit button (#invitation),
+// and a tabbed interface for viewing various types of notes.
+// ------------------------------------
+
+// Constants
+var CONFERENCE_ID = '.TMLR';
+var SUBMISSION_ID = '.TMLR/-/Author_Submission';
+var SUBMITTED_ID = '.TMLR/Submitted';
+var UNDER_REVIEW_ID = '.TMLR/Under_Review';
+var DESK_REJECTED_ID = '.TMLR/Desk_Rejection';
+var HEADER = {};
+
+var WILDCARD_INVITATION = CONFERENCE_ID + '/.*';
+var PAGE_SIZE = 50;
+
+var paperDisplayOptions = {
+  pdfLink: true,
+  replyCount: true,
+  showContents: true,
+  showTags: false
+};
+var commentDisplayOptions = {
+  pdfLink: false,
+  replyCount: true,
+  showContents: false,
+  showParent: true
+};
+
+// Main is the entry point to the webfield code and runs everything
+function main() {
+  Webfield.ui.setup('#group-container', CONFERENCE_ID);  // required
+
+  renderConferenceHeader();
+
+  renderSubmissionButton();
+
+  renderConferenceTabs();
+
+  load().then(renderContent).then(Webfield.ui.done);
+}
+
+// Load makes all the API calls needed to get the data to render the page
+// It returns a jQuery deferred object: https://api.jquery.com/category/deferred-object/
+function load() {
+
+  var submittedNotesP = Webfield.api.getSubmissions(SUBMISSION_ID, {
+    'content.venueid': SUBMITTED_ID,
+    pageSize: PAGE_SIZE,
+    details: 'replyCount',
+    includeCount: true
+  });
+
+  var underReviewNotesP = Webfield.api.getSubmissions(SUBMISSION_ID, {
+    'content.venueid': UNDER_REVIEW_ID,
+    pageSize: PAGE_SIZE,
+    details: 'replyCount',
+    includeCount: true
+  });
+
+  var deskRejectedNotesP = Webfield.api.getSubmissions(SUBMISSION_ID, {
+    'content.venueid': DESK_REJECTED_ID,
+    pageSize: PAGE_SIZE,
+    details: 'replyCount',
+    includeCount: true
+  });
+
+  var activityNotesP = $.Deferred().resolve([]);
+  if (user && !_.startsWith(user.id, 'guest_')) {
+    activityNotesP = Webfield.api.getSubmissions(WILDCARD_INVITATION, {
+      pageSize: PAGE_SIZE,
+      details: 'forumContent,writable'
+    });
+  }
+
+  return $.when(submittedNotesP, underReviewNotesP, deskRejectedNotesP, activityNotesP);
+}
+
+// Render functions
+function renderConferenceHeader() {
+  Webfield.ui.venueHeader(HEADER);
+
+  Webfield.ui.spinner('#notes', { inline: true });
+}
+
+function renderSubmissionButton() {
+  Webfield.api.getSubmissionInvitation(SUBMISSION_ID)
+    .then(function(invitation) {
+      Webfield.ui.submissionButton(invitation, user, {
+        onNoteCreated: function() {
+          // Callback funtion to be run when a paper has successfully been submitted (required)
+          promptMessage('Your submission is complete. Check your inbox for a confirmation email. ');
+
+          load().then(renderContent).then(function() {
+            $('.tabs-container a[href="#your-consoles"]').click();
+          });
+        }
+      });
+    });
+}
+
+function renderConferenceTabs() {
+
+  var sections = [
+  {
+    heading: 'Under Review',
+    id: 'under-review-submissions',
+  },
+  {
+    heading: 'Submissions',
+    id: 'submissions',
+  },
+  {
+    heading: 'Desk Rejected Submissions',
+    id: 'desk-rejected-submissions',
+  },
+  {
+    heading: 'Recent Activity',
+    id: 'recent-activity',
+  }];
+
+  Webfield.ui.tabPanel(sections, {
+    container: '#notes',
+    hidden: true
+  });
+}
+
+function renderContent(submittedResponse, underReviewResponse, deskRejectedResponse, activityNotes) {
+
+  var underReviewSubmissions = underReviewResponse.notes || [];
+  var underReviewCount = underReviewResponse.count || 0;
+
+  $('#under-review-submissions').empty();
+
+  if (underReviewCount) {
+    var searchResultsListOptions = _.assign({}, paperDisplayOptions, {
+      container: '#under-review-submissions',
+      autoLoad: false
+    });
+
+    Webfield.ui.submissionList(underReviewSubmissions, {
+      heading: null,
+      container: '#under-review-submissions',
+      search: {
+        enabled: true,
+        localSearch: false,
+        invitation: SUBMISSION_ID,
+        onResults: function(searchResults) {
+          Webfield.ui.searchResults(searchResults, searchResultsListOptions);
+        },
+        onReset: function() {
+          Webfield.ui.searchResults(notes, searchResultsListOptions);
+          $('#under-review-submissions').append(view.paginationLinks(underReviewCount, PAGE_SIZE, 1));
+        }
+      },
+      displayOptions: paperDisplayOptions,
+      autoLoad: false,
+      noteCount: underReviewCount,
+      pageSize: PAGE_SIZE,
+      onPageClick: function(offset) {
+        return Webfield.api.getSubmissions(SUBMISSION_ID, {
+          'content.venueid': UNDER_REVIEW_ID,
+          details: 'replyCount',
+          pageSize: PAGE_SIZE,
+          offset: offset
+        });
+      },
+      fadeIn: false
+    });
+  } else {
+    $('.tabs-container a[href="#submissions"]').parent().hide();
+  }
+
+  var submissionNotesCount = submittedResponse.count || 0;
+  if (submissionNotesCount) {
+    $('#submissions').empty();
+
+    var notes = submittedResponse.notes || [];
+    Webfield.ui.submissionList(notes, {
+      heading: null,
+      container: '#submissions',
+      search: {
+        enabled: false
+      },
+      displayOptions: paperDisplayOptions,
+      autoLoad: false,
+      noteCount: submissionNotesCount,
+      pageSize: PAGE_SIZE,
+      onPageClick: function(offset) {
+        return Webfield.api.getSubmissions(SUBMISSION_ID, {
+          'content.venueid': SUBMITTED_ID,
+          details: 'replyCount',
+          pageSize: PAGE_SIZE,
+          offset: offset
+        });
+      },
+      fadeIn: false
+    });
+  } else {
+    $('.tabs-container a[href="#submissions"]').parent().hide();
+  }
+
+  var deskRejectedNotesCount = deskRejectedResponse.count || 0;
+  if (deskRejectedNotesCount) {
+    $('#desk-rejected-submissions').empty();
+
+    var deskRejectedNotesArray = deskRejectedResponse.notes || [];
+    Webfield.ui.submissionList(deskRejectedNotesArray, {
+      heading: null,
+      container: '#desk-rejected-submissions',
+      search: {
+        enabled: false
+      },
+      displayOptions: paperDisplayOptions,
+      autoLoad: false,
+      noteCount: deskRejectedNotesCount,
+      pageSize: PAGE_SIZE,
+      onPageClick: function(offset) {
+        return Webfield.api.getSubmissions(SUBMISSION_ID, {
+          'content.venueid': DESK_REJECTED_ID,
+          details: 'replyCount',
+          pageSize: PAGE_SIZE,
+          offset: offset
+        });
+      },
+      fadeIn: false
+    });
+  } else {
+    $('.tabs-container a[href="#desk-rejected-submissions"]').parent().hide();
+  }
+
+  // Activity Tab
+  if (activityNotes.length) {
+    var displayOptions = {
+      container: '#recent-activity',
+      user: user && user.profile,
+      showActionButtons: true
+    };
+
+    $(displayOptions.container).empty();
+
+    Webfield.ui.activityList(activityNotes, displayOptions);
+
+    $('.tabs-container a[href="#recent-activity"]').parent().show();
+  } else {
+    $('.tabs-container a[href="#recent-activity"]').parent().hide();
+  }
+
+  $('#notes .spinner-container').remove();
+  $('.tabs-container').show();
+}
+
+// Go!
+main();
--- tests/test_journal.py
+++ tests/test_journal.py
@@ -1,6 +1,7 @@
 import openreview
 import pytest
 import time
+import json
 
 
 class TestJournal():
@@ -14,7 +15,7 @@ class TestJournal():
         super_user = 'openreview.net'
 
         ## venue group
-        client.post_group(openreview.Group(id=venue_id,
+        venue_group=client.post_group(openreview.Group(id=venue_id,
                         readers=['everyone'],
                         writers=[venue_id],
                         signatures=['~Super_User1'],
@@ -22,15 +23,73 @@ class TestJournal():
                         members=[]
                         ))
 
+        client.add_members_to_group('host', venue_id)
+
         ## editor in chief
         editor_in_chief_id = f"{venue_id}/{editor_in_chief}"
-        client.post_group(openreview.Group(id=editor_in_chief_id,
+        editor_in_chief_group=client.post_group(openreview.Group(id=editor_in_chief_id,
                         readers=['everyone'],
                         writers=[editor_in_chief_id],
                         signatures=[venue_id],
                         signatories=[editor_in_chief_id],
                         members=['~Raia_Hadsell1', '~Kyunghyun_Cho1']
                         ))
+
+        editors=""
+        for m in editor_in_chief_group.members:
+            name=m.replace('~', ' ').replace('_', ' ')[:-1]
+            editors+=f'<a href="https://openreview.net/profile?id={m}">{name}</a></br>'
+
+        header = {
+            "title": "Transactions of Machine Learning Research Journal",
+            "subtitle": "To de defined",
+            "location": "Everywhere",
+            "date": "Ongoing",
+            "website": "https://openreview.net",
+            "instructions": '''
+            <p>
+                <strong>Editors-in-chief:</strong></br>
+                {editors}
+            </p>
+            <p>
+                <strong>[TBD]Submission, Reviewing, Commenting, and Approval Workflow:</strong><br>
+                <p>Any OpenReview logged-in user may submit an article. The article submission form allows the Authors to suggest for their article one or
+                multiple Editors (from among people who have created OpenReview profiles). The article is not immediately visible to the public, but is sent
+                to the Editors-in-Chief, any of whom may perform a basic evaluation of the submission (e.g. checking for spam). If not immediately rejected
+                at this step, an Editor-in-Chief assigns one or more Editors to the article (perhaps from the authors’ suggestions, perhaps from their own choices),
+                and the article is made visible to the public. Authors may upload revisions to any part of their submission at any time. (The full history of past
+                revisions is  available through the "Show Revisions" link.)</p>
+            </p>
+            <p>
+                Assigned Editors are non-anonymous. The article Authors may revise their list of requested editors by revising their submission. The Editors-in-Chief
+                may add or remove Editors for the article at any time.
+            </p>
+            <p>
+                Reviewers are assigned to the article by any of the Editors of the article.  Any of the Editors can add (or remove) Reviewers at any time. Any logged-in
+                user can suggest additional Reviewers for this article; these suggestions are public and non-anonymous.  (Thus the public may apply social pressure on
+                the Editors for diversity of views in reviewing and commenting.) To avoid spam, only assigned Reviewers, Editors and the Editors-in-Chief can contribute
+                comments (or reviews) on the article.  Such comments are public and associated with their non-anonymous reviewers.  There are no system-enforced deadlines
+                for any of the above steps, (although social pressure may be applied out-of-band).
+            </p>
+            <p>
+                At some point, any of the Editors may contribute a meta-review, making an Approval recommendation to the Editors-in-Chief.  Any of the Editors-in-Chief may
+                 at any time add or remove the venue’s Approval from the article (indicating a kind of “acceptance” of the article).
+            </p>
+            <p>
+                For questions about editorial content and process, email the Editors-in-Chief.<br>
+                For questions about software infrastructure or profiles, email the OpenReview support team at
+                <a href="mailto:info@openreview.net">info@openreview.net</a>.
+            </p>
+            '''.format(editors=editors),
+            "deadline": "",
+            "contact": "info@openreview.net"
+        }
+
+        with open('./tests/data/homepage.js') as f:
+            content = f.read()
+            content = content.replace("var HEADER = {};", "var HEADER = " + json.dumps(header) + ";")
+            venue_group.web = content
+            client.post_group(venue_group)
 
         ## action editor
         action_editors_id = f"{venue_id}/{action_editors}"
@@ -73,17 +132,20 @@ class TestJournal():
                                                             'content': {
                                                                 'title': {
                                                                     'value': {
-                                                                        'value-regex': '.*'
+                                                                        'value-regex': '.*',
+                                                                        'required': True
                                                                     }
                                                                 },
                                                                 'abstract': {
                                                                     'value': {
-                                                                        'value-regex': '.*'
+                                                                        'value-regex': '.*',
+                                                                        'required': True
                                                                     }
                                                                 },
                                                                 'authors': {
                                                                     'value': {
-                                                                        'values-regex': '.*'
+                                                                        'values-regex': '.*',
+                                                                        'required': True
                                                                     },
                                                                     'readers': {
                                                                         'values': [ venue_id, '${signatures}', f'{venue_id}/Paper${{number}}/Authors']
@@ -91,10 +153,21 @@ class TestJournal():
                                                                 },
                                                                 'authorids': {
                                                                     'value': {
-                                                                        'values-regex': '.*'
+                                                                        'values-regex': '.*',
+                                                                        'required': True
                                                                     },
                                                                     'readers': {
                                                                         'values': [ venue_id, '${signatures}', f'{venue_id}/Paper${{number}}/Authors']
+                                                                    }
+                                                                },
+                                                                'venue': {
+                                                                    'value': {
+                                                                        'value': 'Submitted to TMLR'
+                                                                    }
+                                                                },
+                                                                'venueid': {
+                                                                    'value': {
+                                                                        'value': '.TMLR/Submitted'
                                                                     }
                                                                 }
                                                             }
@@ -130,7 +203,7 @@ class TestJournal():
                                                                 },
                                                                 'venueid': {
                                                                     'value': {
-                                                                        'value': '.TMLR'
+                                                                        'value': '.TMLR/Under_Review'
                                                                     }
                                                                 }
                                                             }
@@ -165,7 +238,7 @@ class TestJournal():
                                                                 },
                                                                 'venueid': {
                                                                     'value': {
-                                                                        'value': '.TMLR'
+                                                                        'value': '.TMLR/Desk_Rejection'
                                                                     }
                                                                 }
                                                             }
