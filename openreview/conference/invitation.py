ae497c73002cc964daeb9c2cef3deebc43292bc3
Melisa Bok <melisabok@gmail.com>
2021-01-27 11:12:45 -0500

    use value-radio instead

--- openreview/conference/invitation.py
+++ openreview/conference/invitation.py
@@ -71,7 +71,7 @@ class BlindSubmissionsInvitation(openreview.Invitation):
 
         content = {
             'authors': {
-                'values': ['Anonymous']
+                'values-radio': ['Anonymous']
             },
             'authorids': {
                 'values-regex': '.*'
