efe1683de4f11417d8fd02adf26a23a541408ccb
celestemartinez <32438984+celestemartinez@users.noreply.github.com>
2021-02-10 13:19:38 -0500

    Add second due date to request form (#829)
    * Add second due date to request form
    
    * Make pdf optional in builder instead of helpers

--- openreview/venue_request/venue_request.py
+++ openreview/venue_request/venue_request.py
@@ -593,15 +593,20 @@ class VenueRequest():
                 'value-regex': '.*',
                 'order': 8
             },
+            'Abstract Registration Deadline': {
+                'value-regex': r'^[0-9]{4}\/([1-9]|0[1-9]|1[0-2])\/([1-9]|0[1-9]|[1-2][0-9]|3[0-1])(\s+)?((2[0-3]|[01][0-9]|[0-9]):[0-5][0-9])?(\s+)?$',
+                'description': 'By when do authors need to register their manuscripts? Please specify the due date in GMT using the following format: YYYY/MM/DD HH:MM(e.g. 2019/01/31 23:59) (Skip this if there is no abstract registration deadline)',
+                'order': 9
+            },
             'Submission Deadline': {
                 'value-regex': r'^[0-9]{4}\/([1-9]|0[1-9]|1[0-2])\/([1-9]|0[1-9]|[1-2][0-9]|3[0-1])(\s+)?((2[0-3]|[01][0-9]|[0-9]):[0-5][0-9])?(\s+)?$',
                 'description': 'By when do authors need to submit their manuscripts? Please specify the due date in GMT using the following format: YYYY/MM/DD HH:MM(e.g. 2019/01/31 23:59)',
-                'order': 9
+                'order': 10
             },
             'Venue Start Date': {
                 'description': 'What date does the venue start? Please use the following format: YYYY/MM/DD (e.g. 2019/01/31)',
                 'value-regex': r'^[0-9]{4}\/([1-9]|0[1-9]|1[0-2])\/([1-9]|0[1-9]|[1-2][0-9]|3[0-1])(\s+)?((2[0-3]|[01][0-9]|[0-9]):[0-5][0-9])?(\s+)?$',
-                'order': 10,
+                'order': 11,
                 'required': True
             },
             'Location': {
