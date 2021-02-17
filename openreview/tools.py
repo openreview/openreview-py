fb2f4c2b278895a2fd23dc756cf1deb50c03cc53
Melisa Bok <melisabok@gmail.com>
2021-02-17 12:13:22 -0500

    Feature/tmlr journal (#833)
    * intial commit
    
    * rename action editor group
    
    * recruitment refactor
    
    * Update openreview/journal/journal.py
    
    Co-authored-by: celestemartinez <32438984+celestemartinez@users.noreply.github.com>
    
    Co-authored-by: celestemartinez <32438984+celestemartinez@users.noreply.github.com>

--- openreview/tools.py
+++ openreview/tools.py
@@ -1327,11 +1327,10 @@ def recruit_reviewer(client, user, first,
         decline_url = url + "No"
     )
 
+    client.add_members_to_group(reviewers_invited_id, [user])
+
     # send the email through openreview
     response = client.post_message(recruit_message_subj, [user], personalized_message, parentGroup=reviewers_invited_id)
-
-    if 'groups' in response and response['groups']:
-        client.add_members_to_group(reviewers_invited_id, [user])
 
     if verbose:
         print("Sent to the following: ", response)
