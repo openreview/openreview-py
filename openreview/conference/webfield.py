84fa2fafecf627db456c46496b114f885d674459
Melisa Bok <melisabok@gmail.com>
2021-04-21 13:57:49 -0400

    keep backward compatibility

--- openreview/conference/webfield.py
+++ openreview/conference/webfield.py
@@ -354,7 +354,9 @@ class WebfieldBuilder(object):
             content = content.replace("var AREACHAIR_NAME = '';", "var AREACHAIR_NAME = '" + conference.area_chairs_name + "';")
             content = content.replace("var OFFICIAL_REVIEW_NAME = '';", "var OFFICIAL_REVIEW_NAME = '" + conference.review_stage.name + "';")
             content = content.replace("var LEGACY_INVITATION_ID = false;", "var LEGACY_INVITATION_ID = true;" if conference.legacy_invitation_id else "var LEGACY_INVITATION_ID = false;")
-            if conference.default_reviewer_load:
+            if conference.legacy_anonids:
+                content = content.replace("var REVIEW_LOAD = 0;", "var REVIEW_LOAD = " + str(conference.default_reviewer_load) + ";")
+            else if conference.default_reviewer_load:
                 content = content.replace("var REVIEW_LOAD = '';", "var REVIEW_LOAD = '" + str(conference.default_reviewer_load) + "';")
             return self.__update_group(group, content)
 
