48fcc6b76657c97f5187e2763f3b32fbf04b6c58
Melisa Bok <melisabok@gmail.com>
2021-06-25 15:04:15 -0400

    force to the ACs to see the identity of the reviewers

--- openreview/venue_request/process/request_form_pre_process.py
+++ openreview/venue_request/process/request_form_pre_process.py
@@ -10,3 +10,9 @@ def process(client, note, invitation):
     if 'Double-blind' in note.content['Author and Reviewer Anonymity'] or 'Submissions and reviews should both be private.' in note.content['Open Reviewing Policy']:
         if 'submissions_visibility' in note.content and 'Yes' in note.content['submissions_visibility']:
             raise openreview.OpenReviewException('Submissions can only be immediately released to the public for non double-blind, public venues')
+
+    if 'Yes, our venue has Area Chairs' in note.content['Area Chairs (Metareviewers)'] and 'All Area Chairs' not in note.content['reviewer_identity'] and 'Assigned Area Chair' not in note.content['reviewer_identity']:
+        raise openreview.OpenReviewException('Assigned area chairs must see the reviewer identity')
+
+    if 'Yes, our venue has Senior Area Chairs' in note.content.get('senior_area_chairs', '') and 'All Senior Area Chairs' not in note.content['reviewer_identity'] and 'Assigned Senior Area Chair' not in note.content['reviewer_identity']:
+        raise openreview.OpenReviewException('Assigned senior area chairs must see the reviewer identity')
