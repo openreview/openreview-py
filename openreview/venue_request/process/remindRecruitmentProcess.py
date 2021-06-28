ffa53dc208df8f5269e41fd22f47f77ce076d819
haroldrubio <harrywill.rubio@gmail.com>
2021-06-28 16:40:11 -0400

    Explicitly set default reduced load

--- openreview/venue_request/process/remindRecruitmentProcess.py
+++ openreview/venue_request/process/remindRecruitmentProcess.py
@@ -5,7 +5,7 @@ def process(client, note, invitation):
     conference = openreview.helpers.get_conference(client, note.forum)
     print('Conference: ', conference.get_id())
 
-    reduced_load=note.content.get('invitee_reduced_load')
+    reduced_load=note.content.get('invitee_reduced_load', None)
 
     note.content['invitation_email_subject'] = note.content['invitation_email_subject'].replace('{invitee_role}', note.content.get('invitee_role', 'reviewer'))
     note.content['invitation_email_content'] = note.content['invitation_email_content'].replace('{invitee_role}', note.content.get('invitee_role', 'reviewer'))
