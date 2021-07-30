368c9892c42d4e1bbaa1d6002152e5dd1da5fa43
Melisa Bok <melisabok@gmail.com>
2021-07-30 16:08:47 -0400

    editors in chief webfield

--- openreview/journal/webfield/editorsInChiefWebfield.js
+++ openreview/journal/webfield/editorsInChiefWebfield.js
@@ -5,9 +5,10 @@
 var VENUE_ID = '.TMLR';
 var SHORT_PHRASE = 'TMLR';
 var SUBMISSION_ID = '.TMLR/-/Author_Submission';
-var ACTION_EDITOR_NAME = 'Action_Editors'
+var EDITORS_IN_CHIEF_NAME = 'Editors_In_Chief'
 var REVIEWERS_NAME = 'Reviewers'
-var ACTION_EDITOR_ID = '.TMLR/Action_Editors'
+var ACTION_EDITOR_NAME = 'Action_Editors'
+var EDITORS_IN_CHIEF_ID = VENUE_ID + '/' + EDITORS_IN_CHIEF_NAME
 var HEADER = {
   title: 'TMLR',
   instructions: 'Put instructions here'
@@ -19,7 +20,7 @@ var main = function() {
   Webfield2.ui.setup('#group-container', VENUE_ID, {
     title: HEADER.title,
     instructions: HEADER.instructions,
-    tabs: ['Assigned Papers', 'Action Editor Tasks', 'test table'],
+    tabs: ['Paper Status', 'test table'],
     referrer: args && args.referrer
   })
 
@@ -33,29 +34,25 @@ var main = function() {
 
 var loadData = function() {
 
-  return Webfield2.utils.getGroupsByNumber(VENUE_ID, ACTION_EDITOR_NAME, { assigned: true })
-  .then(function(assignedGroups) {
-    return $.when(
-      assignedGroups,
-      Webfield2.utils.getGroupsByNumber(VENUE_ID, REVIEWERS_NAME),
-      Webfield2.utils.getAssignedInvitations(VENUE_ID, ACTION_EDITOR_NAME),
-      Webfield2.utils.getSubmissions(SUBMISSION_ID, { numbers: Object.keys(assignedGroups)})
-    );
-  })
+  return $.when(
+    Webfield2.utils.getGroupsByNumber(VENUE_ID, ACTION_EDITOR_NAME),
+    Webfield2.utils.getGroupsByNumber(VENUE_ID, REVIEWERS_NAME),
+    Webfield2.utils.getSubmissions(SUBMISSION_ID)
+  );
 
 }
 
-var formatData = function(assignedGroups, reviewersByNumber, invitations, submissions) {
-  var referrerUrl = encodeURIComponent('[Action Editor Console](/group?id=' + ACTION_EDITOR_ID + '#assigned-papers)');
+var formatData = function(aeByNumber, reviewersByNumber, submissions) {
+  var referrerUrl = encodeURIComponent('[Action Editor Console](/group?id=' + EDITORS_IN_CHIEF_ID + '#paper-status)');
 
   var submissionsByNumber = _.keyBy(submissions, 'number');
 
   //build the rows
   var rows = [];
 
-  Object.keys(assignedGroups).forEach(function(number) {
-    var submission = submissionsByNumber[number];
+  submissions.forEach(function(submission) {
     if (submission) {
+      var number = submission.number;
 
       var reviews = submission.details.directReplies.filter(function(reply) {
         return reply.invitations.indexOf(VENUE_ID + '/Paper' + number + '/-/Review') >= 0;
@@ -64,6 +61,7 @@ var formatData = function(assignedGroups, reviewersByNumber, invitations, submis
         return reply.invitations.indexOf(VENUE_ID + '/Paper' + number + '/-/Decision') >= 0;
       });
       var reviewers = reviewersByNumber[number];
+      var actionEditors = aeByNumber[number];
       var reviewerStatus = {};
       var confidences = []
 
@@ -104,6 +102,11 @@ var formatData = function(assignedGroups, reviewersByNumber, invitations, submis
         }
       };
 
+      var metaReview = null;
+      if (decision) {
+        metaReview = { id: decision.id, forum: submission.id, content: { recommendation: decision.content.recommendation.value }};
+      }
+
       rows.push({
         checkbox: { selected: false, noteId: submission.id },
         submissionNumber: { number: number},
@@ -117,17 +120,20 @@ var formatData = function(assignedGroups, reviewersByNumber, invitations, submis
           stats: stats,
           sendReminder: true,
         },
-        actionEditorData: {
-          recommendation: decision && decision.content.recommendation.value,
-          editUrl: decision ? ('/forum?id=' + submission.id + '&noteId=' + decision.id + '&referrer=' + referrerUrl) : null
-        }
+        areachairProgressData: {
+          recommendation: metaReview && metaReview.content.recommendation,
+          numMetaReview: metaReview ? 'One' : 'No',
+          areachair: { name: actionEditors.map(function(ae) { return view.prettyId(ae.id); }), email: actionEditors.map(function(ae) { return ae.id; }) },
+          metaReview: metaReview,
+          referrer: referrerUrl
+        },
+        decision: submission.content.venue.value
       })
     }
   })
 
 
   return venueStatusData = {
-    invitations: invitations,
     rows: rows
   };
 }
@@ -135,11 +141,9 @@ var formatData = function(assignedGroups, reviewersByNumber, invitations, submis
 // Render functions
 var renderData = function(venueStatusData) {
 
-  Webfield2.ui.renderTasks('#action-editor-tasks', venueStatusData.invitations, { referrer: encodeURIComponent('[Action Editor Console](/group?id=' + ACTION_EDITOR_ID + '#action-editor-tasks)')});
-
-  Webfield2.ui.renderTable('#assigned-papers', venueStatusData.rows, {
+  Webfield2.ui.renderTable('#paper-status', venueStatusData.rows, {
       headings: ['<input type="checkbox" id="select-all-papers">', '#', 'Paper Summary',
-      'Review Progress', 'Decision Status'],
+      'Review Progress', 'Action Editor Recommendation', 'Decision'],
       renders: [
         function(data) {
           var checked = data.selected ? 'checked="checked"' : '';
@@ -151,7 +155,10 @@ var renderData = function(venueStatusData) {
         },
         Handlebars.templates.noteSummary,
         Handlebars.templates.noteReviewers,
-        Handlebars.templates.noteMetaReviewStatus
+        Handlebars.templates.noteAreaChairs,
+        function(data) {
+          return '<h4>' + data + '</h4>';
+        },
       ],
       sortOptions: {
         Paper_Number: function(row) { return row.submissionNumber.number; },
@@ -161,7 +168,8 @@ var renderData = function(venueStatusData) {
         Average_Confidence: function(row) { return row.reviewProgressData.stats.Confidence.avg === 'N/A' ? 0 : row.reviewProgressData.stats.Confidence.avg; },
         Max_Confidence: function(row) { return row.reviewProgressData.stats.Confidence.max === 'N/A' ? 0 : row.reviewProgressData.stats.Confidence.max; },
         Min_Confidence: function(row) { return row.reviewProgressData.stats.Confidence.min === 'N/A' ? 0 : row.reviewProgressData.stats.Confidence.min; },
-        Recommendation: function(row) { return row.actionEditorData.recommendation; }
+        Recommendation: function(row) { return row.areachairProgressData.recommendation; },
+        Decision: function(row) { return row.decision; }
       },
       searchProperties: {
         number: ['submissionNumber.number'],
@@ -175,16 +183,25 @@ var renderData = function(venueStatusData) {
         confidenceAvg: ['reviewProgressData.stats.Confidence.avg'],
         confidenceMax: ['reviewProgressData.stats.Confidence.max'],
         confidenceMin: ['reviewProgressData.stats.Confidence.min'],
-        recommendation: ['actionEditorData.recommendation']
+        recommendation: ['areachairProgressData.recommendation'],
+        decision: ['decision']
       },
       reminderOptions: {
         container: 'a.send-reminder-link',
         defaultSubject: SHORT_PHRASE + ' Reminder',
         defaultBody: 'Hi {{fullname}},\n\nThis is a reminder to please submit your review for ' + SHORT_PHRASE + '.\n\n' +
         'Click on the link below to go to the review page:\n\n{{submit_review_link}}' +
-        '\n\nThank you,\n' + SHORT_PHRASE + ' Action Editor'
+        '\n\nThank you,\n' + SHORT_PHRASE + ' Editor in Chief'
       },
-      extraClasses: 'ac-console-table'
+      extraClasses: 'console-table paper-table',
+      postRenderTable: function() {
+        $('.console-table th').eq(0).css('width', '3%');
+        $('.console-table th').eq(1).css('width', '5%');
+        $('.console-table th').eq(2).css('width', '22%');
+        $('.console-table th').eq(3).css('width', '30%');
+        $('.console-table th').eq(4).css('width', '28%');
+        $('.console-table th').eq(5).css('width', '12%');
+      }
   })
 
   var options = {
