5ee9bbe88c9f96539c1d0579be8bf58f9515c3d6
celestemg <acmartinez@umass.edu>
2021-09-22 16:25:50 -0400

    add metareview name

--- openreview/conference/builder.py
+++ openreview/conference/builder.py
@@ -2239,7 +2239,7 @@ class ConferenceBuilder(object):
         self.comment_stage = CommentStage(name, start_date, end_date, allow_public_comments, anonymous, unsubmitted_reviewers, reader_selection, email_pcs, authors)
 
     def set_meta_review_stage(self, name='Meta_Review', start_date = None, due_date = None, public = False, release_to_authors = False, release_to_reviewers = MetaReviewStage.Readers.NO_REVIEWERS, additional_fields = {}, remove_fields = [], process = None):
-        self.meta_review_stage = MetaReviewStage(start_date, due_date, public, release_to_authors, release_to_reviewers, additional_fields, remove_fields, process)
+        self.meta_review_stage = MetaReviewStage(name, start_date, due_date, public, release_to_authors, release_to_reviewers, additional_fields, remove_fields, process)
 
     def set_decision_stage(self, options = ['Accept (Oral)', 'Accept (Poster)', 'Reject'], start_date = None, due_date = None, public = False, release_to_authors = False, release_to_reviewers = False, email_authors = False):
         self.decision_stage = DecisionStage(options, start_date, due_date, public, release_to_authors, release_to_reviewers, email_authors)
