6e520ac8c59410fce286582b7786f6889fd707db
celestemartinez <32438984+celestemartinez@users.noreply.github.com>
2021-12-14 13:59:03 -0500

    feature/journal recruitment (#1046)
    * add recruitment status comment
    
    * add action editor recruitment of reviewers
    
    * Melisa's feedback and comment process fucntion
    
    * check transitive members
    
    * add replyto for comment inv

--- openreview/journal/recruitment.py
+++ openreview/journal/recruitment.py
@@ -17,24 +17,39 @@ class Recruitment(object):
         action_editors_invited_id = action_editors_id + '/Invited'
         hash_seed = self.journal.secret_key
 
-        invited_members = self.client.get_group(action_editors_invited_id).members
+        recruitment_status = {
+            'invited': [],
+            'already_invited': [],
+            'already_member' : [],
+            'errors': []
+        }
 
         for index, invitee in enumerate(tqdm(invitees, desc='send_invitations')):
-            profile=openreview.tools.get_profile(self.client, invitee)
-            invitee = profile.id if profile else invitee
-            if invitee not in invited_members:
+            memberships = [g.id for g in self.client.get_groups(member=invitee, regex=action_editors_id)] if tools.get_group(self.client, invitee) else []
+            if action_editors_id in memberships:
+                recruitment_status['already_member'].append(invitee)
+            elif action_editors_invited_id in memberships:
+                recruitment_status['already_invited'].append(invitee)
+            else:
+                profile=openreview.tools.get_profile(self.client, invitee)
+                invitee = profile.id if profile else invitee
                 name = invitee_names[index] if (invitee_names and index < len(invitee_names)) else None
                 if not name:
                     name = re.sub('[0-9]+', '', invitee.replace('~', '').replace('_', ' ')) if invitee.startswith('~') else 'invitee'
-                r=tools.recruit_reviewer(self.client, invitee, name,
-                    hash_seed,
-                    self.journal.get_ae_recruitment_id(),
-                    message,
-                    subject,
-                    action_editors_invited_id,
-                    verbose = False)
+                try:
+                    r=tools.recruit_reviewer(self.client, invitee, name,
+                        hash_seed,
+                        self.journal.get_ae_recruitment_id(),
+                        message,
+                        subject,
+                        action_editors_invited_id,
+                        verbose = False)
+                    recruitment_status['invited'].append(invitee)
+                except openreview.OpenReviewException as e:
+                    self.client.remove_members_from_group(action_editors_invited_id, invitee)
+                    recruitment_status['errors'].append(e)
 
-        return self.client.get_group(id = action_editors_invited_id)
+        return recruitment_status
 
     def invite_reviewers(self, message, subject, invitees, invitee_names=None):
 
@@ -43,22 +58,38 @@ class Recruitment(object):
         reviewers_invited_id = reviewers_id + '/Invited'
         hash_seed = self.journal.secret_key
 
+        recruitment_status = {
+            'invited': [],
+            'already_invited': [],
+            'already_member' : [],
+            'errors': []
+        }
+
         invited_members = self.client.get_group(reviewers_invited_id).members
 
         for index, invitee in enumerate(tqdm(invitees, desc='send_invitations')):
-            memberships = [g.id for g in self.client.get_groups(member=invitee, regex=reviewers_id)] if (invitee.startswith('~') or tools.get_group(self.client, invitee)) else []
-            profile=openreview.tools.get_profile(self.client, invitee)
-            invitee = profile.id if profile else invitee
-            if invitee not in invited_members:
+            memberships = [g.id for g in self.client.get_groups(member=invitee, regex=reviewers_id)] if tools.get_group(self.client, invitee) else []
+            if reviewers_id in memberships:
+                recruitment_status['already_member'].append(invitee)
+            elif reviewers_invited_id in memberships:
+                recruitment_status['already_invited'].append(invitee)
+            else:
+                profile=openreview.tools.get_profile(self.client, invitee)
+                invitee = profile.id if profile else invitee
                 name = invitee_names[index] if (invitee_names and index < len(invitee_names)) else None
                 if not name:
                     name = re.sub('[0-9]+', '', invitee.replace('~', '').replace('_', ' ')) if invitee.startswith('~') else 'invitee'
-                r=tools.recruit_reviewer(self.client, invitee, name,
-                    hash_seed,
-                    self.journal.get_reviewer_recruitment_id(),
-                    message,
-                    subject,
-                    reviewers_invited_id,
-                    verbose = False)
+                try:
+                    r=tools.recruit_reviewer(self.client, invitee, name,
+                        hash_seed,
+                        self.journal.get_reviewer_recruitment_id(),
+                        message,
+                        subject,
+                        reviewers_invited_id,
+                        verbose = False)
+                    recruitment_status['invited'].append(invitee)
+                except openreview.OpenReviewException as e:
+                    self.client.remove_members_from_group(reviewers_invited_id, invitee)
+                    recruitment_status['errors'].append(e)
 
-        return self.client.get_group(id = reviewers_invited_id)
+        return recruitment_status
