df31c9f31739cf37e563d4bbad700e70f6d2b284
Melisa Bok <melisabok@gmail.com>
2022-01-04 21:57:04 -0500

    get all replies

--- openreview/journal/webfield/authorsWebfield.js
+++ openreview/journal/webfield/authorsWebfield.js
@@ -42,7 +42,7 @@ var loadData = function() {
   var notesP = Webfield2.getAll('/notes', {
     'content.authorids': user.profile.id,
     invitation: SUBMISSION_ID,
-    details: 'directReplies'
+    details: 'replies'
   });
 
   return $.when(
