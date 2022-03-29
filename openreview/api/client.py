1efc0de5374a4b1fec3440f5abab7db82fa137d7
xukun <xukun@openreview.net>
2022-03-29 11:24:35 -0400

    remove web assignment

--- openreview/api/client.py
+++ openreview/api/client.py
@@ -2073,7 +2073,6 @@ class Invitation(object):
         self.bulk = bulk
         self.details = details
         self.reply_forum_views = reply_forum_views
-        self.web = None
         self.process = process
         self.preprocess = preprocess
         self.date_processes = date_processes
