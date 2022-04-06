3e519b323b46f367aa0fe574d3f2f3d512bca02e
Melisa Bok <melisabok@gmail.com>
2022-04-06 16:36:52 -0400

    add review invitation template

--- openreview/venue/raw_templates.py
+++ openreview/venue/raw_templates.py
@@ -91,3 +91,133 @@ def process(client, edit, invitation):
         }    
     }
 }
+
+review_template = {
+    'id': 'OpenReview.net/Support/-/Review_Template', ## template should be part of the name?
+    'invitees': ['active_venues'], ## how to give access to the PCs only from all the venues
+    'readers': ['everyone'],
+    'writers': ['OpenReview.net'],
+    'signatures': ['OpenReview.net'],
+    'params': {
+        'venueid': { 'type': 'group' }, ## any other validation? can we use any group id as venueid?
+        'name': { 'type': 'string', 'default': 'Official_Review'},
+        'submission_name': { 'type': 'string'}, ## need to know the submission name to validate and create the paper groups
+        'cdate': { 'type': 'date', 'range': [ 0, 9999999999999 ] },
+        'duedate': { 'type': 'date', 'range': [ 0, 9999999999999 ] }       
+    },
+    'invitation': {
+        'id': '${..params.venueid}/-/${..params.name}',
+        'signatures': ['${..params.venueid}'],
+        'readers': ['${signatures}'],
+        'writers': ['${signatures}'],
+        'invitees': ['${signatures}'],
+        'cdate': { '${..params.cdate}' },
+        'duedate': { '${..params.duedate}' },
+        'params': {
+            'noteId': { 'type': 'note', 'withInvitation': '${...params.venueid}/-/${...params.submission_name}' }, ## any other validation? use withInvitation?
+            'noteNumber': { 'type': 'integer'},
+            'submissionGroupId': '${...params.venueid}/${...params.submission_name}${.params.noteNumber}' ## constant so I can use it in several places
+        },
+        'dateprocesses': [{
+            'dates': ['#{cdate}'],
+            'script': '''
+def process(client, invitation):
+    submissions = client.get_notes(invitation='${...params.venueid}/-/${...params.submission_name}')
+    ## Create review invitations for all the active submissions
+    for submission in submissions:
+        client.post_invitation_edit({
+            invitation: '${...params.venueid}/-/${...params.name}',
+            params: {
+                noteId: submission.id,
+                noteNumber: submission.number
+            }
+        })
+'''
+        }],        
+        'edit': {
+            'signatures': ['${...params.venueid}'],
+            'readers': ['${signatures}'],
+            'writers': ['${signatures}'],
+            'invitation': {
+                'id': '${...params.submissionGroupId}/-/${....params.name}',
+                'signatures': ['${....params.venueid}'],
+                'readers': ['everyone'], ## everyone or just the reviewers?
+                'writers': ['${signatures}'],
+                'invitees': ['${...params.submissionGroupId}/Reviewers'], ## should we parametrize the role names?
+                'cdate': { '${....params.cdate}' },
+                'duedate': { '${....params.duedate}' },
+                'process': '''
+def process(client, edit, invitation):
+    ## send confirmation email to the reviewer
+    ## add the reviewer to the submitted group (?)
+                ''',
+                'edit': {
+                    'signatures': { 'param': { 'regex': '${...params.submissionGroupId}/Reviewer_', 'type': 'group' }},
+                    'readers': ['${.....params.venueid}', '${signatures}'], 
+                    'writers': ['${.....params.venueid}'],
+                    'note': {
+                        'id': {
+                            'param': {
+                                'withInvitation': '${edit.invitation}', ## can I reference the invitation id of the edit
+                                'optional': True
+                            }
+                        },
+                        'signatures': ['${edit.signatures}'],
+                        'readers': ['${......params.venueid}', '${edit.signatures}'],
+                        'writers': ['${......params.venueid}', '${edit.signatures}'], ## only visible to the submitted reviewer, should we create another template if we want to have public reviews?
+                        'content': {
+                            'title': {
+                                'order': 1, ## we store this value in the invitation edit but we don't store it in the note, should we assume that only 'value' and 'readers' will be saved in the note content?
+                                'description': 'Brief summary of your review.', ## Same here.
+                                'value': { 'param': { 'type': 'string', 'regex': '.{1,500}' } }
+                            },
+                            'review': {
+                                'order': 2, 
+                                'description': 'Please provide an evaluation of the quality, clarity, originality and significance of this work, including a list of its pros and cons (max 200000 characters). Add formatting using Markdown and formulas using LaTeX. For more information see https://openreview.net/faq',
+                                'value': { 'param': { 'type': 'string', 'maxLength': 200000 } },
+                                'presentation': { 'markdown': True }
+                            },
+                            'rating': {
+                                'order': 3,
+                                'value': { 
+                                    'param': { 
+                                        'type': 'string',  
+                                        'enum': [
+                                            '10: Top 5% of accepted papers, seminal paper',
+                                            '9: Top 15% of accepted papers, strong accept',
+                                            '8: Top 50% of accepted papers, clear accept',
+                                            '7: Good paper, accept',
+                                            '6: Marginally above acceptance threshold',
+                                            '5: Marginally below acceptance threshold',
+                                            '4: Ok but not good enough - rejection',
+                                            '3: Clear rejection',
+                                            '2: Strong rejection',
+                                            '1: Trivial or wrong'
+                                        ]
+                                    } 
+                                },
+                                'presentation': { 'input': 'select' }
+                            },
+                            'confidence': {
+                                'order': 4,
+                                'value': { 
+                                    'param': { 
+                                        'type': 'string',  
+                                        'enum': [
+                                            '5: The reviewer is absolutely certain that the evaluation is correct and very familiar with the relevant literature',
+                                            '4: The reviewer is confident but not absolutely certain that the evaluation is correct',
+                                            '3: The reviewer is fairly confident that the evaluation is correct',
+                                            '2: The reviewer is willing to defend the evaluation, but it is quite likely that the reviewer did not understand central parts of the paper',
+                                            '1: The reviewer\'s evaluation is an educated guess'
+                                        ]
+                                    } 
+                                },
+                                'presentation': { 'input': 'radio' }
+                            }                          
+                        }                    
+                    }                   
+                }                
+            }          
+        }
+   }
+}
