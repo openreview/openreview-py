1747b49a43d665188dd870964b42df6af46f8041
Melisa Bok <melisabok@gmail.com>
2022-04-26 17:25:27 -0400

    refactor all the invitations

--- openreview/venue/raw_templates.py
+++ openreview/venue/raw_templates.py
@@ -178,12 +178,59 @@ review_template = {
         'readers': ['OpenReview.net/Support', '${content.venueid.value}'],
         'writers': ['OpenReview.net/Support', '${content.venueid.value}'],
         'signatures': ['${content.venueid.value}'],        
-        'params': {
-            'venueid': { 'type': 'group' }, ## any other validation? can we use any group id as venueid?
-            'name': { 'type': 'string', 'default': 'Official_Review'},
-            'submission_name': { 'type': 'string'}, ## need to know the submission name to validate and create the paper groups
-            'cdate': { 'type': 'date', 'range': [ 0, 9999999999999 ] },
-            'duedate': { 'type': 'date', 'range': [ 0, 9999999999999 ] }       
+        'content': {
+            'venueid': { 
+                'value': {
+                    'param': {
+                        'description': 'Venue id',
+                        'memberOf': 'active_venues' ## any other validation? can we use any group id as venueid?
+                    }
+                },
+                'order': 1,
+                'type': 'group'
+            }, 
+            'name': { 
+                'value': {
+                    'param': {
+                        'regex': '.*',
+                        'description': 'Official Review name.', 
+                        'optional': True,
+                        'default': 'Official_Review'
+                    }
+                },
+                'order': 2,
+                'type': 'string'
+            },
+            'submission_name': { 
+                'value': {
+                    'param': {
+                        'regex': '.*',
+                        'description': 'Submission name.'
+                    }
+                },
+                'order': 3,
+                'type': 'string'
+            },            
+            'cdate': {
+                'value': {
+                    'param': { 
+                        'range': [ 0, 9999999999999 ],
+                        'description': 'Activation date.'
+                    },
+                },
+                'order': 4,
+                'type': 'date',
+            },
+            'duedate': {
+                'value': {
+                    'param': { 
+                        'description': 'Submission due date.',
+                        'range': [ 0, 9999999999999 ] 
+                    },
+                },
+                'order': 5,
+                'type': 'date'
+            },     
         },
         'invitation': {
             'id': '${../content.venueid.value}/-/${../content.name.value}',
@@ -191,20 +238,20 @@ review_template = {
             'readers': ['${signatures}'],
             'writers': ['${signatures}'],
             'invitees': ['${signatures}'],
-            'cdate': { '${../params.cdate}' },
-            'duedate': { '${../params.duedate}' },
+            'cdate': { '${../content.cdate.value}' },
+            'duedate': { '${../content.duedate.value}' },
             'dateprocesses': [{
                 'dates': ['#{cdate}'],
                 'script': '''
 def process(client, invitation):
-    submissions = client.get_notes(invitation='${../../content.venueid.value}/-/${../../params.submission_name}')
+    submissions = client.get_notes(invitation='${../../content.venueid.value}/-/${../../content.submission_name.value}')
     ## Create review invitations for all the active submissions
     for submission in submissions:
         client.post_invitation_edit({
             invitation: '${../../content.venueid.value}/-/${../../content.name.value}',
-            params: {
-                noteId: submission.id,
-                noteNumber: submission.number
+            content: {
+                noteId: { 'value': submission.id },
+                noteNumber: { 'value': submission.number }
             }
         })
     '''
@@ -213,26 +260,42 @@ def process(client, invitation):
                 'signatures': ['${../../content.venueid.value}'],
                 'readers': ['${signatures}'],
                 'writers': ['${signatures}'],
-                'params': {
-                    'noteId': { 'type': 'note', 'withInvitation': '${../../content.venueid.value}/-/${../../params.submission_name}' }, ## any other validation? use withInvitation?
-                    'noteNumber': { 'type': 'integer'},
-                    'submissionGroupId': '${../../content.venueid.value}/${../../params.submission_name}${noteNumber}' ## constant so I can use it in several places
+                'content': {
+                    'noteId': {
+                        'value': {
+                           'type': 'note', 
+                            'param': {
+                                'withInvitation': '${../../content.venueid.value}/-/${../../content.submission_name.value}' 
+                            }
+                        }
+                    }, ## any other validation? use withInvitation?
+                    'noteNumber': { 
+                        'type': 'integer',
+                        'value': {
+                            'param': {
+                                ##???
+                            },
+                        }
+                    },
+                    'submissionGroupId': {
+                        'value': '${../../content.venueid.value}/${../../content.submission_name.value}${noteNumber}' ## constant so I can use it in several places
+                    }
                 },                
                 'invitation': {
-                    'id': '${../../params.submissionGroupId}/-/${../../../content.name.value}',
+                    'id': '${../../content.submissionGroupId.value}/-/${../../../content.name.value}',
                     'signatures': ['${../../../content.venueid.value}'],
                     'readers': ['everyone'], ## everyone or just the reviewers?
                     'writers': ['${signatures}'],
-                    'invitees': ['${../../params.submissionGroupId}/Reviewers'], ## should we parametrize the role names?
-                    'cdate': { '${../../../params.cdate}' },
-                    'duedate': { '${../../../params.duedate}' },
+                    'invitees': ['${../../content.submissionGroupId.value}/Reviewers'], ## should we parametrize the role names?
+                    'cdate': { '${../../../content.cdate.value}' },
+                    'duedate': { '${../../../content.duedate.value}' },
                     'process': '''
 def process(client, edit, invitation):
     ## send confirmation email to the reviewer
     ## add the reviewer to the submitted group (?)
                     ''',
                     'edit': {
-                        'signatures': { 'param': { 'regex': '${../../../params.submissionGroupId}/Reviewer_', 'type': 'group' }},
+                        'signatures': { 'param': { 'regex': '${../../../content.submissionGroupId.value}/Reviewer_' }},
                         'readers': ['${../../../../content.venueid.value}', '${signatures}'], 
                         'writers': ['${../../../../content.venueid.value}'],
                         'note': {
@@ -242,28 +305,38 @@ def process(client, edit, invitation):
                                     'optional': True
                                 }
                             },
-                            'forum': '${../../params.noteId}',
-                            'replyto': '${../../params.noteId}',
+                            'forum': '${../../content.noteId.value}',
+                            'replyto': '${../../content.noteId.value}',
                             'signatures': ['${../signatures}'], ## how to resolve this in the UI?
                             'readers': ['${../../../../../content.venueid.value}', '${signatures}'],
                             'writers': ['${../../../../../content.venueid.value}', '${signatures}'], ## only visible to the submitted reviewer, should we create another template if we want to have public reviews?
                             'content': {
                                 'title': {
                                     'order': 1, ## we store this value in the invitation edit but we don't store it in the note, should we assume that only 'value' and 'readers' will be saved in the note content?
-                                    'description': 'Brief summary of your review.', ## Same here.
-                                    'value': { 'param': { 'type': 'string', 'regex': '.{1,500}' } }
+                                    'type': 'string', 
+                                    'value': { 
+                                        'param': { 
+                                            'description': 'Brief summary of your review.', ## Same here.
+                                            'regex': '.{1,500}' 
+                                        } 
+                                    }
                                 },
                                 'review': {
                                     'order': 2, 
-                                    'description': 'Please provide an evaluation of the quality, clarity, originality and significance of this work, including a list of its pros and cons (max 200000 characters). Add formatting using Markdown and formulas using LaTeX. For more information see https://openreview.net/faq',
-                                    'value': { 'param': { 'type': 'string', 'maxLength': 200000 } },
-                                    'presentation': { 'markdown': True }
+                                    'type': 'string',
+                                    'value': { 
+                                        'param': { 
+                                            'description': 'Please provide an evaluation of the quality, clarity, originality and significance of this work, including a list of its pros and cons (max 200000 characters). Add formatting using Markdown and formulas using LaTeX. For more information see https://openreview.net/faq',
+                                            'maxLength': 200000,
+                                            'markdown': True 
+                                        }
+                                    }
                                 },
                                 'rating': {
                                     'order': 3,
+                                    'type': 'string',  
                                     'value': { 
                                         'param': { 
-                                            'type': 'string',  
                                             'enum': [
                                                 '10: Top 5% of accepted papers, seminal paper',
                                                 '9: Top 15% of accepted papers, strong accept',
@@ -275,26 +348,26 @@ def process(client, edit, invitation):
                                                 '3: Clear rejection',
                                                 '2: Strong rejection',
                                                 '1: Trivial or wrong'
-                                            ]
+                                            ],
+                                            'input': 'select'
                                         } 
-                                    },
-                                    'presentation': { 'input': 'select' }
+                                    }
                                 },
                                 'confidence': {
                                     'order': 4,
+                                    'type': 'string',  
                                     'value': { 
                                         'param': { 
-                                            'type': 'string',  
                                             'enum': [
                                                 '5: The reviewer is absolutely certain that the evaluation is correct and very familiar with the relevant literature',
                                                 '4: The reviewer is confident but not absolutely certain that the evaluation is correct',
                                                 '3: The reviewer is fairly confident that the evaluation is correct',
                                                 '2: The reviewer is willing to defend the evaluation, but it is quite likely that the reviewer did not understand central parts of the paper',
                                                 '1: The reviewer\'s evaluation is an educated guess'
-                                            ]
+                                            ],
+                                            'input': 'radio'
                                         } 
-                                    },
-                                    'presentation': { 'input': 'radio' }
+                                    }
                                 }                          
                             }                    
                         }                   
@@ -318,11 +391,49 @@ submission_release_template = {
         'readers': ['OpenReview.net/Support', '${content.venueid.value}'],
         'writers': ['OpenReview.net/Support', '${content.venueid.value}'],
         'signatures': ['${content.venueid.value}'],
-        'params': {
-            'venueid': { 'type': 'group' }, ## any other validation? can we use any group id as venueid?
-            'name': { 'type': 'string', 'default': 'Submission_Release'},
-            'submission_name': { 'type': 'string'},
-            'cdate': { 'type': 'date', 'range': [ 0, 9999999999999 ] }       
+        'content': {
+            'venueid': { 
+                'value': {
+                    'param': {
+                        'description': 'Venue id',
+                        'memberOf': 'active_venues' ## any other validation? can we use any group id as venueid?
+                    }
+                },
+                'order': 1,
+                'type': 'group'
+            }, 
+            'name': { 
+                'value': {
+                    'param': {
+                        'regex': '.*',
+                        'description': 'Official Review name.', 
+                        'optional': True,
+                        'default': 'Official_Review'
+                    }
+                },
+                'order': 2,
+                'type': 'string'
+            },
+            'submission_name': { 
+                'value': {
+                    'param': {
+                        'regex': '.*',
+                        'description': 'Submission name.'
+                    }
+                },
+                'order': 3,
+                'type': 'string'
+            },            
+            'cdate': {
+                'value': {
+                    'param': { 
+                        'range': [ 0, 9999999999999 ],
+                        'description': 'Activation date.'
+                    },
+                },
+                'order': 4,
+                'type': 'date',
+            }      
         },
         'invitation': {
             'id': '${../content.venueid.value}/-/${../content.name.value}',
@@ -330,12 +441,12 @@ submission_release_template = {
             'readers': ['everyone'],
             'writers': ['${signatures}'],
             'invitees': ['${../content.venueid.value}'],
-            'cdate': { '${../params.cdate}' },
+            'cdate': { '${../content.cdate.value}' },
             'dateprocesses': [{
                 'dates': ['#{cdate}'],
                 'script': '''
 def process(client, invitation):
-    submissions = client.get_notes(invitation='${../../content.venueid.value}/-/${../../params.submission_name}')
+    submissions = client.get_notes(invitation='${../../content.venueid.value}/-/${../../content.submission_name.value}')
     ## Set submissions to be under review
     for submission in submissions:
         client.post_invitation_edit({
@@ -353,7 +464,7 @@ def process(client, invitation):
                 'note': {
                     'id': {
                         'param': {
-                            'withInvitation': '${../../../../../content.venueid.value}/-/${../../../../../params.submission_name}'
+                            'withInvitation': '${../../../../../content.venueid.value}/-/${../../../../../content.submission_name.value}'
                         }
                     },
                     'readers': ['everyone'],  ## parametrize readers of have a template per readers combination?
