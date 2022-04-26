63c7a3bef712b63e8d69ff977296e19e89d0be84
celestemartinez <32438984+celestemartinez@users.noreply.github.com>
2022-04-26 14:30:55 -0400

    TMLR recruitment feedback (#1210)
    * do not check invited when sending recruitment emails from AEs
    
    * add reinvite param
    
    * post comment to request and email AEs
    
    * check recruitment notes only if request form is being used
    
    * use comment inv readers for comment readers
    
    * Melisa's feedback
    
    * add support group constant

--- openreview/journal/journal_request/process/comment_process.py
+++ openreview/journal/journal_request/process/comment_process.py
@@ -6,8 +6,17 @@ def process(client, edit, invitation):
     full_name = forum.content['official_venue_name']['value']
     editors = forum.content['editors']['value']
 
-    subject_eic = f'Comment posted to your journal request: {full_name}'
-    message_eic = f'''A comment was posted to your journal request.
+    if edit.note.content['title']['value'] == 'New Recruitment Response':
+        subject = f'A new recruitment response has been posted to your journal request: {full_name}'
+        message = f'''Comment: {edit.note.content['comment']['value']}
+        
+To view the comment, click here: https://openreview.net/forum?id={edit.note.forum}&noteId={edit.note.id}'''
+
+        client.post_message(subject, edit.note.readers, message)
+
+    else:
+        subject_eic = f'Comment posted to your journal request: {full_name}'
+        message_eic = f'''A comment was posted to your journal request.
 
 Comment title {edit.note.content['title']['value']}
 
@@ -15,10 +24,10 @@ Comment: {edit.note.content['comment']['value']}
 
 To view the comment, click here: https://openreview.net/forum?id={edit.note.forum}&noteId={edit.note.id}'''
 
-    client.post_message(subject_eic, editors, message_eic)
+        client.post_message(subject_eic, editors, message_eic)
 
-    subject_support = f'Comment posted to a journal request: {full_name}'
-    message_support = f'''A comment was posted to a journal request.
+        subject_support = f'Comment posted to a journal request: {full_name}'
+        message_support = f'''A comment was posted to a journal request.
 
 Comment title {edit.note.content['title']['value']}
 
@@ -26,4 +35,4 @@ Comment: {edit.note.content['comment']['value']}
 
 To view the comment, click here: https://openreview.net/forum?id={edit.note.forum}&noteId={edit.note.id}'''
 
-    client.post_message(subject_eic, [SUPPORT_GROUP], message_eic)
+        client.post_message(subject_eic, [SUPPORT_GROUP], message_eic)
