dd0a8a821f73637cb6d791f6271dc159c7b0e15f
purujitgoyal <purujit@openreview.net>
2022-05-18 14:28:56 -0400

    Adds assertion to test forum_url is replaced correctly

--- openreview/venue_request/process/revisionProcess.py
+++ openreview/venue_request/process/revisionProcess.py
@@ -121,8 +121,7 @@ def process(client, note, invitation):
                         'default': f'''Dear {{fullname}},
 
 Thank you for submitting your paper, {{submission_title}}, to {short_name}. We are delighted to inform you that your submission has been accepted. Congratulations!
-You can find the final reviews for your paper on the submission page in OpenReview at:
-{{forum_url}}
+You can find the final reviews for your paper on the submission page in OpenReview at: {{forum_url}}
 
 Best,
 {short_name} Program Chairs
@@ -135,8 +134,7 @@ Best,
                         'default': f'''Dear {{fullname}},
                         
 Thank you for submitting your paper, {{submission_title}}, to {short_name}. We regret to inform you that your submission was not accepted.
-You can find the final reviews for your paper on the submission page in OpenReview at:
-{{forum_url}}
+You can find the final reviews for your paper on the submission page in OpenReview at: {{forum_url}}
 
 Best,
 {short_name} Program Chairs
@@ -149,8 +147,7 @@ Best,
                         'default': f'''Dear {{fullname}},
 
 Thank you for submitting your paper, {{submission_title}}, to {short_name}.
-You can find the final reviews for your paper on the submission page in OpenReview at:
-{{forum_url}}
+You can find the final reviews for your paper on the submission page in OpenReview at: {{forum_url}}
 
 Best,
 {short_name} Program Chairs
