8fca2a1afe13424415d9b1eeacc5fe274ed62054
carlosmondra <carlos@openreview.net>
2022-05-26 12:16:16 -0500

    Fix/replace regex (#1278)
    * Feat: Add ids as a valid param when retrieving invitations
    
    * Feat: Replace regex with ids
    
    Co-authored-by: Melisa Bok <melisabok@gmail.com>

--- openreview/conference/templates/desk_reject_process.py
+++ openreview/conference/templates/desk_reject_process.py
@@ -43,8 +43,18 @@ def process_update(client, note, invitation, existing_note):
             forum_note = client.post_note(forum_note)
 
             # Restore review, meta-review and decision invitations
-            invitation_regex = CONFERENCE_ID + '/Paper' + str(forum_note.number) + '/-/(Official_Review|Meta_Review|Decision|Revision|Desk_Reject|Withdraw|Supplementary_Material|Official_Comment|Public_Comment)$'
-            all_paper_invitations = openreview.tools.iterget_invitations(client, regex=invitation_regex, expired=True)
+            invitation_ids = ','.join([
+                f'{CONFERENCE_ID}/Paper{str(forum_note.number)}/-/Official_Review',
+                f'{CONFERENCE_ID}/Paper{str(forum_note.number)}/-/Meta_Review',
+                f'{CONFERENCE_ID}/Paper{str(forum_note.number)}/-/Decision',
+                f'{CONFERENCE_ID}/Paper{str(forum_note.number)}/-/Revision',
+                f'{CONFERENCE_ID}/Paper{str(forum_note.number)}/-/Desk_Reject',
+                f'{CONFERENCE_ID}/Paper{str(forum_note.number)}/-/Withdraw',
+                f'{CONFERENCE_ID}/Paper{str(forum_note.number)}/-/Supplementary_Material',
+                f'{CONFERENCE_ID}/Paper{str(forum_note.number)}/-/Official_Comment',
+                f'{CONFERENCE_ID}/Paper{str(forum_note.number)}/-/Public_Comment'
+            ])
+            all_paper_invitations = openreview.tools.iterget_invitations(client, ids=invitation_ids, expired=True)
             for invitation in all_paper_invitations:
                 invitation.expdate = None
                 client.post_invitation(invitation)
@@ -89,8 +99,18 @@ def process_update(client, note, invitation, existing_note):
         forum_note = client.post_note(forum_note)
 
         # Expire review, meta-review and decision invitations
-        invitation_regex = CONFERENCE_ID + '/Paper' + str(forum_note.number) + '/-/(Official_Review|Meta_Review|Decision|Revision|Desk_Reject|Withdraw|Supplementary_Material|Official_Comment|Public_Comment)$'
-        all_paper_invitations = client.get_all_invitations(regex=invitation_regex)
+        invitation_ids = ','.join([
+            f'{CONFERENCE_ID}/Paper{str(forum_note.number)}/-/Official_Review',
+            f'{CONFERENCE_ID}/Paper{str(forum_note.number)}/-/Meta_Review',
+            f'{CONFERENCE_ID}/Paper{str(forum_note.number)}/-/Decision',
+            f'{CONFERENCE_ID}/Paper{str(forum_note.number)}/-/Revision',
+            f'{CONFERENCE_ID}/Paper{str(forum_note.number)}/-/Desk_Reject',
+            f'{CONFERENCE_ID}/Paper{str(forum_note.number)}/-/Withdraw',
+            f'{CONFERENCE_ID}/Paper{str(forum_note.number)}/-/Supplementary_Material',
+            f'{CONFERENCE_ID}/Paper{str(forum_note.number)}/-/Official_Comment',
+            f'{CONFERENCE_ID}/Paper{str(forum_note.number)}/-/Public_Comment'
+        ])
+        all_paper_invitations = client.get_all_invitations(ids=invitation_ids)
         now = openreview.tools.datetime_millis(datetime.utcnow())
         for invitation in all_paper_invitations:
             invitation.expdate = now
