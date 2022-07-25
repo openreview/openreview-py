6a1d2e8fc02f3cb006e1fc933ff9121e6805c5f6
purujitgoyal <purujit@openreview.net>
2022-07-25 12:55:23 -0400

    fix more tests

--- tests/test_single_blind_private_conference.py
+++ tests/test_single_blind_private_conference.py
@@ -89,10 +89,11 @@ class TestSingleBlindPrivateConference():
         assert invitations[0].id == 'MICCAI.org/2021/Challenges/Paper5/-/Withdraw'
 
         invitations = client.get_invitations(replyForum=notes[0].id)
-        assert len(invitations) == 2
+        assert len(invitations) == 3
         invitation_ids = [invitation.id for invitation in invitations]
         assert 'MICCAI.org/2021/Challenges/Paper5/-/Desk_Reject' in invitation_ids
         assert 'MICCAI.org/2021/Challenges/Paper5/-/Withdraw' in invitation_ids
+        assert 'MICCAI.org/2021/Challenges/Paper5/-/Revision' in invitation_ids
 
     def test_public_comments(self, conference, helpers, test_client, client):
         notes = test_client.get_notes(invitation='MICCAI.org/2021/Challenges/-/Submission', sort='tmdate')
