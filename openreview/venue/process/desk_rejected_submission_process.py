317107352a4c8ffa35ef3e44c927a7fc08f79b5d
celestemartinez <32438984+celestemartinez@users.noreply.github.com>
2022-10-18 16:26:14 -0400

    Venue class: desk-reject invitations (#1423)
    * withdraw submissions
    
    * release authors after withdrawn
    
    * expire invitation during withdraw
    
    * expire all the existing invitation when withdrawal
    
    * withdraw and revert withdrawal
    
    * home page integration
    
    * message the committee about the withdrawn paper
    
    * add email notifications
    
    * fix adding expdate and cdate for child invitations
    
    * fix serialization
    
    * add creation of desk-reject inv and dek-rejection reversion
    
    * remove extra space and fix tests
    
    Co-authored-by: Melisa Bok <melisabok@gmail.com>

--- openreview/venue/process/desk_rejected_submission_process.py
+++ openreview/venue/process/desk_rejected_submission_process.py
@@ -18,29 +18,29 @@ def process(client, edit, invitation):
             invitations=EXPIRE_INVITATION_ID,
             invitation=openreview.api.Invitation(id=invitation.id,
                 expdate=now
-            )            
+            )
         )
 
     formatted_committee = [committee.format(number=submission.number) for committee in COMMITTEE]
-    email_subject = f'''[{SHORT_NAME}]: Paper #{submission.number} withdrawn by paper authors'''
-    email_body = f'''The {SHORT_NAME} paper "{submission.content.get('title', {}).get('value', '#'+str(submission.number))}" has been withdrawn by the paper authors.
+    email_subject = f'''[{SHORT_NAME}]: Paper #{submission.number} desk-rejected by program chairs'''
+    email_body = f'''The {SHORT_NAME} paper "{submission.content.get('title', {}).get('value', '#'+str(submission.number))}" has been desk-rejected by the program chairs.
 
 For more information, click here https://openreview.net/forum?id={submission.id}
 '''
 
-    client.post_message(email_subject, formatted_committee, email_body)    
+    client.post_message(email_subject, formatted_committee, email_body)
 
-    withdrawal_notes = client.get_notes(forum=submission.id, invitation=PAPER_INVITATION_PREFIX + f'{submission.number}/-/Withdrawal')
-    if withdrawal_notes:
-        print(f'Create withdrawal reversion invitation')
+    desk_rejection_notes = client.get_notes(forum=submission.id, invitation=PAPER_INVITATION_PREFIX + f'{submission.number}/-/Desk_Rejection')
+    if desk_rejection_notes:
+        print(f'Create desk-rejection reversion invitation')
         client.post_invitation_edit(
-            invitations=VENUE_ID + '/-/Withdrawal_Reversion',
+            invitations=VENUE_ID + '/-/Desk_Rejection_Reversion',
             content={
                 'noteId': {
                     'value': submission.id
-                },                
-                'withdrawalId': {
-                    'value': withdrawal_notes[0].id
+                },
+                'deskRejectionId': {
+                    'value': desk_rejection_notes[0].id
                 }
             }
         )
