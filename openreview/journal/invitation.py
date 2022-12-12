72c4f2b8ddb8da643abc6d6b1dccbdc7b0a9f1f1
Melisa Bok <melisabok@gmail.com>
2022-12-12 15:41:33 -0500

    add certification field to the EIC console

--- openreview/journal/invitation.py
+++ openreview/journal/invitation.py
@@ -5016,6 +5016,20 @@ If you have questions please contact the Editors-In-Chief: tmlr-editors@jmlr.org
             'process': self.process_script
         }
 
+        if self.journal.get_certifications():
+            invitation['edit']['note']['content']['certifications'] = {
+                "order": 13,
+                "description": "Certifications are meant to highlight particularly notable accepted submissions. Notably, it is through certifications that we make room for more speculative/editorial judgement on the significance and potential for impact of accepted submissions. Certification selection is the responsibility of the AE, however you are asked to submit your recommendation.",
+                "value": {
+                    "param": {
+                        "type": "string[]",
+                        "enum": self.journal.get_certifications(),
+                        "optional": True,
+                        "input": "select"
+                    }
+                }
+            }            
+
         self.save_super_invitation(self.journal.get_eic_revision_id(), invitation_content, edit_content, invitation)
 
     def set_note_eic_revision_invitation(self, note):
