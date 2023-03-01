531932c213de6ff483f5022c363a975a0e51a5de
celestemartinez <32438984+celestemartinez@users.noreply.github.com>
2023-03-01 11:43:46 -0500

    increase package version (#1585)
    Co-authored-by: Melisa Bok <melisabok@gmail.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='openreview-py',
 
-    version='1.19.5',
+    version='1.20.0',
 
     description='OpenReview API Python client library',
     url='https://github.com/openreview/openreview-py',
