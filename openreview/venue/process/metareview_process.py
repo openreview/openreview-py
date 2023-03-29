4d5465f6440e9b0374bfbc3f9f4ea24b99e9be24
Celeste Martinez <celeste@openreview.net>
2023-03-29 14:35:39 -0400

    get only invitations of invitations

--- openreview/venue/process/metareview_process.py
+++ openreview/venue/process/metareview_process.py
@@ -5,24 +5,23 @@ def process(client, edit, invitation):
     submission_name = domain.content['submission_name']['value']
 
     metareview = client.get_note(edit.note.id)
+    submission = client.get_note(edit.note.forum)
 
     #create children invitation if applicable
-    venue_invitations = [i for i in client.get_all_invitations(prefix=venue_id + '/-/') if i.is_active()]
+    venue_invitations = [i for i in client.get_all_invitations(prefix=venue_id + '/-/', type='invitation') if i.is_active()]
 
     for invitation in venue_invitations:
         print('processing invitation: ', invitation.id)
-        if isinstance(invitation.edit, dict) and 'invitation' in invitation.edit:
-            metareview_reply = invitation.content.get('reply_to', {}).get('value', False) if invitation.content else False
-            content_keys = invitation.edit.get('content', {}).keys()
-            if 'metareviews' == metareview_reply and 'replytoSignatures' in content_keys and 'replyto' in content_keys and len(content_keys) == 4:
-                print('create invitation: ', invitation.id)
-                paper_number = metareview.signatures[0].split(submission_name)[-1].split('/')[0]
-                client.post_invitation_edit(invitations=invitation.id,
-                    content={
-                        'noteId': { 'value': metareview.forum },
-                        'noteNumber': { 'value': int(paper_number) },
-                        'replytoSignatures': { 'value': metareview.signatures[0] },
-                        'replyto': { 'value': metareview.id }
-                    },
-                    invitation=openreview.api.Invitation()
-                )
+        metareview_reply = invitation.content.get('reply_to', {}).get('value', False) if invitation.content else False
+        content_keys = invitation.edit.get('content', {}).keys()
+        if 'metareviews' == metareview_reply and 'replytoSignatures' in content_keys and 'replyto' in content_keys and len(content_keys) == 4:
+            print('create invitation: ', invitation.id)
+            client.post_invitation_edit(invitations=invitation.id,
+                content={
+                    'noteId': { 'value': metareview.forum },
+                    'noteNumber': { 'value': submission.number },
+                    'replytoSignatures': { 'value': metareview.signatures[0] },
+                    'replyto': { 'value': metareview.id }
+                },
+                invitation=openreview.api.Invitation()
+            )
