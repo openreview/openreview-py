f86797100c74eb5055951cd07bbb3b6c37e2f76a
celestemartinez <32438984+celestemartinez@users.noreply.github.com>
2023-05-24 13:58:59 -0400

    Fix/ Venue Request: check if comment has title (#1706)
    * check if comment has title
    
    * add host to venue group

--- openreview/venue_request/process/commentProcess.js
+++ openreview/venue_request/process/commentProcess.js
@@ -8,18 +8,20 @@ function(){
   .then(function(result) {
 
     var forumNote = result.notes[0];
+    var prettySignature = note.signatures[0].startsWith('~') ? note.signatures[0].replace(/~|\d+/g, '').replace(/_/g, ' ') : 'Support';
+    var commentTitle = note.content.title ? note.content.title : 'Comment by ' + prettySignature;
 
     var message = {
       groups: note.readers,
       ignoreGroups: [SUPPORT_GROUP],
       subject: 'Comment posted to your request for service: ' + forumNote.content.title,
-      message: 'A comment was posted to your service request. \n\nComment title: ' + note.content.title + '\n\nComment: ' + note.content.comment + '\n\nTo view the comment, click here: ' + baseUrl + '/forum?id=' + note.forum + '&noteId=' + note.id + '\n\nPlease note that with the exception of urgent issues, requests made on weekends or US holidays can expect to receive a response on the following business day. Thank you for your patience!'
+      message: 'A comment was posted to your service request. \n\nComment title: ' + commentTitle + '\n\nComment: ' + note.content.comment + '\n\nTo view the comment, click here: ' + baseUrl + '/forum?id=' + note.forum + '&noteId=' + note.id + '\n\nPlease note that with the exception of urgent issues, requests made on weekends or US holidays can expect to receive a response on the following business day. Thank you for your patience!'
     };
 
     var support_message = {
       groups: [SUPPORT_GROUP],
       subject: 'Comment posted to a service request: ' + forumNote.content.title,
-      message: 'A comment was posted to a service request. \n\nComment title: ' + note.content.title + '\n\nComment: ' + note.content.comment + '\n\nTo view the comment, click here: ' + baseUrl + '/forum?id=' + note.forum + '&noteId=' + note.id
+      message: 'A comment was posted to a service request. \n\nComment title: ' + commentTitle + '\n\nComment: ' + note.content.comment + '\n\nTo view the comment, click here: ' + baseUrl + '/forum?id=' + note.forum + '&noteId=' + note.id
     };
 
     return or3client.or3request(or3client.mailUrl, message, 'POST', token)
