1700dc8b4d6b2ace3c4ee03bd383a218c5eac812
Melisa Bok <melisabok@gmail.com>
2023-06-05 18:08:53 -0400

    remove extra comma

--- openreview/stages/venue_stages.py
+++ openreview/stages/venue_stages.py
@@ -998,7 +998,7 @@ class MetaReviewStage(object):
         self.additional_fields = additional_fields
         self.remove_fields = remove_fields
         self.process = None
-        self.recommendation_field_name = 'recommendation',
+        self.recommendation_field_name = 'recommendation'
         self.sub_venue = sub_venue
 
     def _get_reviewer_readers(self, conference, number):
