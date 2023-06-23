0f54cf02ec0b6ae8150e83ae101902f9af1aac68
celestemartinez <32438984+celestemartinez@users.noreply.github.com>
2023-06-23 15:04:48 -0400

    Fix/ Venue Withdrawl: check groups exist before sending email (#1749)
    * check groups exist before sending email
    
    * check groups exist for desk-rejection
    
    * add test
    
    * use saved committee to send emails to
    
    * check if PCs should be emailed for withdrawals

--- openreview/venue/process/withdrawn_submission_process.py
+++ openreview/venue/process/withdrawn_submission_process.py
@@ -5,17 +5,12 @@ def process(client, edit, invitation):
     short_name = domain.content['subtitle']['value']
     withdraw_reversion_id = domain.content['withdraw_reversion_id']['value']
     withdraw_expiration_id = domain.content['withdraw_expiration_id']['value']
+    withdraw_committee = domain.content['withdraw_committee']['value']
     withdrawal_name = domain.content['withdrawal_name']['value']
     submission_name = domain.content['submission_name']['value']
     authors_name = domain.content['authors_name']['value']
-    reviewers_name = domain.content['reviewers_name']['value']
-    area_chairs_name = domain.content.get('area_chairs_name', {}).get('value')
-    senior_area_chairs_name = domain.content.get('senior_area_chairs_name', {}).get('value')
-    reviewers_id = domain.content['reviewers_id']['value']
-    area_chairs_id = domain.content.get('area_chairs_id', {}).get('value')
-    senior_area_chairs_id = domain.content.get('senior_area_chairs_id', {}).get('value')
-    program_chairs_id = domain.content['program_chairs_id']['value']
     withdrawal_email_pcs = domain.content.get('withdrawal_email_pcs', {}).get('value')
+    program_chairs_id = domain.content['program_chairs_id']['value']
 
     submission = client.get_note(edit.note.id)
     paper_group_id=f'{venue_id}/{submission_name}{submission.number}'    
@@ -51,19 +46,13 @@ def process(client, edit, invitation):
     print(f'Remove {paper_group_id}/{authors_name} from {venue_id}/{authors_name}')
     client.remove_members_from_group(f'{venue_id}/{authors_name}', f'{paper_group_id}/{authors_name}')
 
-    recipients = [f'{paper_group_id}/{authors_name}']
+    formatted_committee = [committee.format(number=submission.number) for committee in withdraw_committee]
+    final_committee = []
+    for group in formatted_committee:
+        if openreview.tools.get_group(client, group):
+            final_committee.append(group)
 
-    if reviewers_name and (f'{paper_group_id}/{reviewers_name}' in submission.readers or reviewers_id in submission.readers or 'everyone' in submission.readers):
-        recipients.append(f'{paper_group_id}/{reviewers_name}')
-
-    if area_chairs_name and (f'{paper_group_id}/{area_chairs_name}' in submission.readers or area_chairs_id in submission.readers or 'everyone' in submission.readers):
-        recipients.append(f'{paper_group_id}/{area_chairs_name}')
-
-    if senior_area_chairs_name and (f'{paper_group_id}/{senior_area_chairs_name}' in submission.readers or senior_area_chairs_id in submission.readers or 'everyone' in submission.readers):
-        recipients.append(f'{paper_group_id}/{senior_area_chairs_name}')
-
-    if program_chairs_id and withdrawal_email_pcs:
-        recipients.append(program_chairs_id)
+    ignoreRecipients = [program_chairs_id] if not withdrawal_email_pcs else []
     
     email_subject = f'''[{short_name}]: Paper #{submission.number} withdrawn by paper authors'''
     email_body = f'''The {short_name} paper "{submission.content.get('title', {}).get('value', '#'+str(submission.number))}" has been withdrawn by the paper authors.
@@ -71,4 +60,4 @@ def process(client, edit, invitation):
 For more information, click here https://openreview.net/forum?id={submission.id}&noteId={withdrawal_notes[0].id}
 '''
 
-    client.post_message(email_subject, recipients, email_body)        
+    client.post_message(email_subject, final_committee, email_body, ignoreRecipients=ignoreRecipients)
