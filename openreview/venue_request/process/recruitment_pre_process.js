3951f398bfeecfcab0f9c2710cb91baea638577b
Emilia Rubio <22416602+enrubio@users.noreply.github.com>
2023-08-16 13:52:15 -0400

    Fix variable declarations

--- openreview/venue_request/process/recruitment_pre_process.js
+++ openreview/venue_request/process/recruitment_pre_process.js
@@ -5,25 +5,25 @@ function(){
     const fields = ['invitation_email_subject', 'invitation_email_content', 'accepted_email_template'];
 
     
-    for (let field of fields) {
+    for (const field of fields) {
         if (field in note.content) {
             // Find all words wrapped in double curly braces. If it's not a token, raise an error.
             let regex = /{{([^{}]+)}}/g;
-            let parenthesized_token = '';
+            let parenthesizedToken = '';
             let match;
             while ((match = regex.exec(note.content[field])) !== null) {
-                parenthesized_token = match[1];
-                if (!tokens.includes(parenthesized_token)) {
-                    done(`Invalid token: {{${parenthesized_token}}} does not exist.`);
+                parenthesizedToken = match[1];
+                if (!tokens.includes(parenthesizedToken)) {
+                    done(`Invalid token: {{${parenthesizedToken}}} does not exist.`);
                 }
             }
 
             // Check for tokens that don't have double curly braces, raise an error.
-            for (let token of tokens) {
+            for (const token of tokens) {
                 regex = new RegExp(`(?<!{)[{]?${token}[}]+|[{]+${token}[}]?(?!})`, 'g');
                 while ((match = regex.exec(note.content[field])) !== null) {
-                    parenthesized_token = match[0];
-                    done(`Invalid token: ${parenthesized_token}. Tokens must be wrapped in double curly braces.`);
+                    parenthesizedToken = match[0];
+                    done(`Invalid token: ${parenthesizedToken}. Tokens must be wrapped in double curly braces.`);
                 }
             }
         }
