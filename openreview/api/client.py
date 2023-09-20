e29bf7508335879df81c8d82878b227eb158c5ad
Celeste Martinez <celeste@openreview.net>
2023-09-20 09:36:07 -0400

    add content to edit

--- openreview/api/client.py
+++ openreview/api/client.py
@@ -2077,7 +2077,8 @@ class Edit(object):
         nonreaders = None,
         cdate = None,
         ddate = None,
-        tauthor = None):
+        tauthor = None,
+        content = None):
 
         self.id = id
         self.domain = domain
@@ -2092,6 +2093,7 @@ class Edit(object):
         self.group = group
         self.invitation = invitation
         self.tauthor = tauthor
+        self.content = content
 
     def __repr__(self):
         content = ','.join([("%s = %r" % (attr, value)) for attr, value in vars(self).items()])
@@ -2132,6 +2134,8 @@ class Edit(object):
             body['invitation'] = self.invitation
         if (self.ddate):
             body['ddate'] = self.ddate
+        if (self.content):
+            body['content'] = self.content
 
         return body
 
@@ -2158,7 +2162,8 @@ class Edit(object):
             note = Note.from_json(e['note']) if 'note' in e else None,
             group = Group.from_json(e['group']) if 'group' in e else None,
             invitation = e.get('invitation'),
-            tauthor = e.get('tauthor')
+            tauthor = e.get('tauthor'),
+            content = e.get('content')
             )
 
         if isinstance(edit.invitation, dict):
