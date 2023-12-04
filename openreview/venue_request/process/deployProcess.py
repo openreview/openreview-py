9d5f8937160f3602a6a43ec7ad7b28ca90384ff3
racheljsmart <rachel@openreview.net>
2023-12-04 13:46:47 -0500

    Update deployProcess.py
    new phrasing

--- openreview/venue_request/process/deployProcess.py
+++ openreview/venue_request/process/deployProcess.py
@@ -78,7 +78,9 @@ Hi Program Chairs,
 
 Thank you for choosing OpenReview to host your upcoming venue.
 
-Users of new profiles that sign up with non institutional emails will go through a moderation process that **can take up to two weeks**. Users that sign up for a profile with their institutional email will be activated automatically. To encourage early profile registration, we recommend alerting authors of this extended moderation period in the Call for Papers.
+We recommend making authors aware of OpenReview's moderation policy for newly created profiles in the Call for Papers:
+- New profiles created without an institutional email will go through a moderation process that **can take up to two weeks**.
+- New profiles created with an institutional email will be activated automatically.
 
 We have set up the venue based on the information that you provided here: {baseurl}/forum?id={noteId}
 
