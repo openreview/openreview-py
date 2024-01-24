b08ff2665dbb4dc3cd5217f926b72b33fbe6836d
xukun <xukun@openreview.net>
2024-01-24 13:40:51 -0500

    fix format

--- openreview/profile/process/dblp_record_process.js
+++ openreview/profile/process/dblp_record_process.js
@@ -1,30 +1,28 @@
 async function process(client, edit, invitation) {
-  client.throwErrors = true
+  client.throwErrors = true;
 
-  const note = Tools.convertDblpXmlToNote(edit.content?.xml?.value)
+  const note = Tools.convertDblpXmlToNote(edit.content?.xml?.value);
 
-  note.id = edit.note.id
-  const authorids = edit.note.content.authorids?.value
+  note.id = edit.note.id;
+  const authorids = edit.note.content.authorids?.value;
   if (authorids) {
-    note.content.authorids.value = note.content.authorids.value.map(
-      (authorid, index) => authorids[index] || authorid
-    )
+    note.content.authorids.value = note.content.authorids.value.map((authorid, index) => authorids[index] || authorid)
   }
 
-  const html = note.content.html?.value
+  const html = note.content.html?.value;
 
   if (html) {
-    const abstract = await Tools.extractAbstract(html)
+    const abstract = await Tools.extractAbstract(html);
     if (abstract) {
-      note.content.abstract = { value: abstract }
+      note.content.abstract = { value: abstract };
     }
   }
 
   await client.postNoteEdit({
-    invitation: "DBLP.org/-/Edit",
-    signatures: ["DBLP.org/Uploader"],
-    readers: ["everyone"],
-    writers: ["DBLP.org"],
+    invitation: 'DBLP.org/-/Edit',
+    signatures: ['DBLP.org/Uploader'],
+    readers: ['everyone'],
+    writers: ['DBLP.org'],
     note: note,
-  })
+  });
 }
