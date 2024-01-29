75c0c4ad31dda4c1b9fa9449277b2f48ae1b253b
Emilia Rubio <22416602+enrubio@users.noreply.github.com>
2024-01-29 18:49:15 -0500

    Fix test

--- tests/test_iclr_conference_v2.py
+++ tests/test_iclr_conference_v2.py
@@ -271,8 +271,7 @@ class TestICLRConference():
         assert '_bibtex' in submissions[0].content
         assert 'author={Anonymous}' in submissions[0].content['_bibtex']['value']
         
-        # Assert that activation date of paper matching setup inv == abstract deadline
-        request_form=pc_client.get_notes(invitation='openreview.net/Support/-/Request_Form')[0]
+        # Assert that activation date of matching invitation == abstract deadline
         matching_invitation = client.get_invitation(f'openreview.net/Support/-/Request{request_form.number}/Paper_Matching_Setup')
         abstract_date_midnight = datetime.datetime.combine(abstract_date, datetime.datetime.min.time())
         abstract_date_ms = abstract_date_midnight.replace(tzinfo=datetime.timezone.utc).timestamp() * 1000
