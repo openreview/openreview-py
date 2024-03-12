0e7eb5bc52a1b19d5e1739270ef34b440be012c6
Emilia Rubio <22416602+enrubio@users.noreply.github.com>
2024-03-12 15:50:54 -0400

    Changes from feedback

--- openreview/venue_request/process/matching_pre_process.py
+++ openreview/venue_request/process/matching_pre_process.py
@@ -4,17 +4,17 @@ def process(client, note, invitation):
     forum_note = client.get_note(note.forum)
     venue_id = forum_note.content.get('venue_id', '')
     domain = client_v2.get_group(venue_id)
-    submission_venue_id = domain.content['submission_venue_id']['value']
-    senior_area_chairs_name = domain.content.get('senior_area_chairs_name', {}).get('value')
-    compute_affinity_scores = note.content.get('compute_affinity_scores') == 'Yes'
+    compute_affinity_scores = note.content.get('compute_affinity_scores') != 'No'
 
     if compute_affinity_scores and 'upload_affinity_scores' in note.content:
         raise openreview.OpenReviewException('Either upload your own affinity scores or select affinity scores computed by OpenReview')
 
-    matching_group = note.content['matching_group']
-    role_name = matching_group.split('/')[-1]
-    pretty_role = role_name.replace('_', ' ')
-    _, num_submissions = client_v2.get_notes(content={ 'venueid':submission_venue_id }, limit=1, with_count=True)
-
-    if compute_affinity_scores and num_submissions >= 2000 and role_name != senior_area_chairs_name:
-        raise openreview.OpenReviewException(f'Can not compute affinity scores between {pretty_role} and 2000+ papers. Please contact us at info@openreview.net to compute your scores.')
+    if forum_note.content.get('api_version', '1') == '2':
+        matching_group = note.content['matching_group']
+        senior_area_chairs_name = domain.get_content_value('senior_area_chairs_name')
+        if senior_area_chairs_name and matching_group.endswith(senior_area_chairs_name): return
+        
+        submission_venue_id = domain.get_content_value('submission_venue_id')
+        _, num_submissions = client_v2.get_notes(content={ 'venueid':submission_venue_id }, limit=1, with_count=True)
+        if compute_affinity_scores and num_submissions >= 2000:
+            raise openreview.OpenReviewException(f'Can not compute affinity scores for venues with 2000+ papers. Please contact us at info@openreview.net to compute your scores.')
