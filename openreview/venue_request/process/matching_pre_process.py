ec5083144fdaa30e182cd682b8647f98d726115d
Emilia Rubio <22416602+enrubio@users.noreply.github.com>
2024-03-14 17:55:23 -0400

    Fix preprocess

--- openreview/venue_request/process/matching_pre_process.py
+++ openreview/venue_request/process/matching_pre_process.py
@@ -9,18 +9,6 @@ def process(client, note, invitation):
     if compute_affinity_scores and 'upload_affinity_scores' in note.content:
         raise openreview.OpenReviewException('Either upload your own affinity scores or select affinity scores computed by OpenReview')
 
-    if forum_note.content.get('api_version', '1') == '2':
-        matching_group = note.content['matching_group']
-        senior_area_chairs_name = domain.get_content_value('senior_area_chairs_name')
-        if senior_area_chairs_name and matching_group.endswith(senior_area_chairs_name):
-            return
-
-        submission_venue_id = domain.get_content_value('submission_venue_id')
-        _, num_submissions = client_v2.get_notes(content={ 'venueid':submission_venue_id }, limit=1, with_count=True)
-        if compute_affinity_scores and num_submissions >= 2000:
-            raise openreview.OpenReviewException(f'Can not compute affinity scores for venues with 2000+ papers. Please contact us at info@openreview.net to compute your scores.')
-
-
     matching_group = note.content['matching_group']
     matching_notes = client.get_all_notes(invitation=invitation.id, sort='tmdate:desc')
     matching_group_notes = [matching_note for matching_note in matching_notes if matching_note.content['matching_group'] == matching_group]
@@ -29,3 +17,13 @@ def process(client, note, invitation):
         status_note = client.get_all_notes(invitation=invitation.id.replace('Paper_Matching_Setup', 'Paper_Matching_Setup_Status'), replyto=matching_group_notes[0].id)
         if not status_note:
             raise openreview.OpenReviewException('Paper matching is already being run for this group. Please wait for a status reply in the forum.')
+
+    if forum_note.content.get('api_version', '1') == '2':
+        senior_area_chairs_name = domain.get_content_value('senior_area_chairs_name')
+        if senior_area_chairs_name and matching_group.endswith(senior_area_chairs_name):
+            return
+
+        submission_venue_id = domain.get_content_value('submission_venue_id')
+        _, num_submissions = client_v2.get_notes(content={ 'venueid':submission_venue_id }, limit=1, with_count=True)
+        if compute_affinity_scores and num_submissions >= 2000:
+            raise openreview.OpenReviewException(f'Can not compute affinity scores for venues with 2000+ papers. Please contact us at info@openreview.net to compute your scores.')
