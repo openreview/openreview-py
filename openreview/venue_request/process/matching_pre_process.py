1f18db6b7f8771ecdf72efe7f2abe966a4236154
Emilia Rubio <22416602+enrubio@users.noreply.github.com>
2024-03-14 17:45:15 -0400

    Refactor preprocess

--- openreview/venue_request/process/matching_pre_process.py
+++ openreview/venue_request/process/matching_pre_process.py
@@ -5,17 +5,9 @@ def process(client, note, invitation):
 
     matching_group = note.content['matching_group']
     matching_notes = client.get_all_notes(invitation=invitation.id, sort='tmdate:desc')
-
-    if matching_notes:
-        latest_matching_note = None
-        for i, matching_note in enumerate(matching_notes):
-            if matching_note.content['matching_group'] == matching_group:
-                latest_matching_note = matching_notes[i]
-                break
-
-        if latest_matching_note:
-            status_inv = invitation.id.replace('Paper_Matching_Setup', 'Paper_Matching_Setup_Status')
-            status_note = client.get_all_notes(invitation=status_inv, replyto=latest_matching_note.id)
-
-            if not status_note:
-                raise openreview.OpenReviewException('Paper matching is already being run for this group. Please wait for a status reply in the forum.')
+    matching_group_notes = [matching_note for matching_note in matching_notes if matching_note.content['matching_group'] == matching_group]
+    
+    if matching_group_notes:
+        status_note = client.get_all_notes(invitation=invitation.id.replace('Paper_Matching_Setup', 'Paper_Matching_Setup_Status'), replyto=matching_group_notes[0].id)
+        if not status_note:
+            raise openreview.OpenReviewException('Paper matching is already being run for this group. Please wait for a status reply in the forum.')
