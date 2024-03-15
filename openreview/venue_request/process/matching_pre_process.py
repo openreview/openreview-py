9f2f904f2707378281e5a731731b607a4d48d309
Emilia Rubio <22416602+enrubio@users.noreply.github.com>
2024-03-15 13:03:02 -0400

    Paper matching: Don't allow multiple setup matchings per group if process is ongoing (#2047)
    * Prevent paper matching for the same group if already running
    
    * Add test
    
    * Refactor preprocess
    
    * Fix preprocess
    
    ---------
    
    Co-authored-by: celestemartinez <32438984+celestemartinez@users.noreply.github.com>

--- openreview/venue_request/process/matching_pre_process.py
+++ openreview/venue_request/process/matching_pre_process.py
@@ -9,8 +9,16 @@ def process(client, note, invitation):
     if compute_affinity_scores and 'upload_affinity_scores' in note.content:
         raise openreview.OpenReviewException('Either upload your own affinity scores or select affinity scores computed by OpenReview')
 
+    matching_group = note.content['matching_group']
+    matching_notes = client.get_all_notes(invitation=invitation.id, sort='tmdate:desc')
+    matching_group_notes = [matching_note for matching_note in matching_notes if matching_note.content['matching_group'] == matching_group]
+    
+    if matching_group_notes:
+        status_note = client.get_all_notes(invitation=invitation.id.replace('Paper_Matching_Setup', 'Paper_Matching_Setup_Status'), replyto=matching_group_notes[0].id)
+        if not status_note:
+            raise openreview.OpenReviewException('Paper matching is already being run for this group. Please wait for a status reply in the forum.')
+
     if forum_note.content.get('api_version', '1') == '2':
-        matching_group = note.content['matching_group']
         senior_area_chairs_name = domain.get_content_value('senior_area_chairs_name')
         if senior_area_chairs_name and matching_group.endswith(senior_area_chairs_name):
             return
