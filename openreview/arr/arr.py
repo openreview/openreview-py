495bd1a5039d4b76c5d02aba954071be8dc4e974
Harold Rubio <harrywill.rubio@gmail.com>
2024-03-26 18:01:20 -0400

    Change invitation saving

--- openreview/arr/arr.py
+++ openreview/arr/arr.py
@@ -311,7 +311,14 @@ class ARR(object):
         invitation.preprocess = self.invitation_builder.get_process_content('process/submission_preprocess.py')
         invitation.process = invitation.process + self.invitation_builder.get_process_content('process/submission_process_extension.py')
         invitation.signatures=['~Super_User1']
-        self.invitation_builder.save_invitation(invitation)
+        self.client.post_invitation_edit(
+            invitations=self.venue.get_meta_invitation_id(),
+            readers=[self.venue_id],
+            writers=[self.venue_id],
+            signatures=[self.venue_id],
+            replacement=False,
+            invitation=invitation
+        )
         return stage_value
 
     def create_post_submission_stage(self):
@@ -324,14 +331,21 @@ class ARR(object):
     def create_review_stage(self):
         self.venue.review_stage = self.review_stage
         stage_value = self.venue.create_review_stage()
-        invitation = self.client.get_invitation(f"{self.venue_id}/-/Official_Review")
+        invitation = self.client.get_invitation(self.get_invitation_id(self.review_stage.name))
         invitation.content = {
             'review_process_script': {
                 'value': self.invitation_builder.get_process_content('process/review_process.py')
             }
         }
         invitation.edit['invitation']['preprocess'] = self.invitation_builder.get_process_content('process/review_preprocess.py')
-        self.invitation_builder.save_invitation(invitation, replacement=False)
+        self.client.post_invitation_edit(
+            invitations=self.venue.get_meta_invitation_id(),
+            readers=[self.venue_id],
+            writers=[self.venue_id],
+            signatures=[self.venue_id],
+            replacement=False,
+            invitation=invitation
+        )
         return stage_value
 
     def create_review_rebuttal_stage(self):
@@ -341,14 +355,22 @@ class ARR(object):
     def create_meta_review_stage(self):
         self.venue.meta_review_stage = self.meta_review_stage
         stage_value = self.venue.create_meta_review_stage()
-        invitation = self.client.get_invitation(f"{self.venue_id}/-/Meta_Review")
+        invitation = self.client.get_invitation(self.get_invitation_id(self.meta_review_stage.name))
         invitation.content = {
             'meta_review_process_script': {
                 'value': self.invitation_builder.get_process_content('process/metareview_process.py')
             }
         }
         invitation.edit['invitation']['preprocess'] = self.invitation_builder.get_process_content('process/review_preprocess.py')
-        self.invitation_builder.save_invitation(invitation, replacement=False)
+        self.client.post_invitation_edit(
+            invitations=self.venue.get_meta_invitation_id(),
+            readers=[self.venue_id],
+            writers=[self.venue_id],
+            signatures=[self.venue_id],
+            replacement=False,
+            invitation=invitation
+        )
+        return stage_value
 
     def create_registration_stages(self):
         self.venue.registration_stages = self.registration_stages
@@ -382,9 +404,16 @@ class ARR(object):
     def create_ethics_review_stage(self):
         self.venue.ethics_review_stage = self.ethics_review_stage
         stage_value = self.venue.create_ethics_review_stage()
-        invitation = self.client.get_invitation(f"{self.venue_id}/-/Ethics_Review_Flag")
+        invitation = self.client.get_invitation(f"{self.venue_id}/-/{self.ethics_review_stage.name}_Flag")
         invitation.process = self.invitation_builder.get_process_content('process/ethics_flag_process.py')
-        self.invitation_builder.save_invitation(invitation, replacement=False)
+        self.client.post_invitation_edit(
+            invitations=self.venue.get_meta_invitation_id(),
+            readers=[self.venue_id],
+            writers=[self.venue_id],
+            signatures=[self.venue_id],
+            replacement=False,
+            invitation=invitation
+        )
         return stage_value
 
     def update_conflict_policies(self, committee_id, compute_conflicts, compute_conflicts_n_years):
