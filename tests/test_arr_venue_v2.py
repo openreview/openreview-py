56cddee29d511f525accac67abc8d73832fbd068
Harold Rubio <harrywill.rubio@gmail.com>
2024-04-01 13:47:51 -0400

    Assert all content fields have value

--- tests/test_arr_venue_v2.py
+++ tests/test_arr_venue_v2.py
@@ -1677,6 +1677,14 @@ class TestARRVenueV2():
                 note.content['authors']['value'].append('SAC ARROne')
                 note.content['authorids']['value'].append('~SAC_ARROne1')
 
+            if i == 3: ## Remove resubmission information from content
+                del note.content['previous_URL']
+                del note.content['previous_PDF']
+                del note.content['response_PDF']
+                del note.content['reassignment_request_action_editor']
+                del note.content['reassignment_request_reviewers']
+                del note.content['justification_for_not_keeping_action_editor_or_reviewers']
+
             test_client.post_note_edit(invitation='aclweb.org/ACL/ARR/2023/August/-/Submission',
                 signatures=['~SomeFirstName_User1'],
                 note=note)
@@ -1893,6 +1901,11 @@ class TestARRVenueV2():
             "aclweb.org/ACL/ARR/2023/August/Submission2/Reviewers",
             "aclweb.org/ACL/ARR/2023/August/Submission2/Authors"
         }
+
+        # Assert that all content fields have a value
+        for submission in submissions:
+            for key, value in submission.content.items():
+                assert 'value' in value, f"Submission{submission.number} id={submission.id} content.{key} has no 'value' key"
 
         # Post comment as PCs to all submissions
         for submission in submissions:
