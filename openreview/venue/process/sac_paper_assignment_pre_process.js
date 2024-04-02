12eaaeb2a9ad8cacb93333aec221c431c1e5c25f
Celeste Martinez <celeste@openreview.net>
2024-04-02 09:51:11 -0400

    fix formatting

--- openreview/venue/process/sac_paper_assignment_pre_process.js
+++ openreview/venue/process/sac_paper_assignment_pre_process.js
@@ -1,21 +1,21 @@
 async function process(client, edge, invitation) {
-    client.throwErrors = false
-  
-    const { groups } = await client.getGroups({ id: edge.domain })
-    const domain = groups[0]
-    const venueId = domain.id
-    const submissionName = domain.content.submission_name?.value
-    const sacsName = invitation.content.reviewers_name?.value
+  client.throwErrors = false
 
-    const { notes } = await client.getNotes({ id: edge.head })
-    const submission = notes[0]
-    const submissionGroupId = `${venueId}/${submissionName}${submission.number}`
+  const { groups } = await client.getGroups({ id: edge.domain })
+  const domain = groups[0]
+  const venueId = domain.id
+  const submissionName = domain.content.submission_name?.value
+  const sacsName = invitation.content.reviewers_name?.value
 
-    if (!edge.ddate) {
-        const { count } = await client.getGroups({ id: `${submissionGroupId}/${sacsName}` })
-        if ( count === 0) {
-          return Promise.reject(new OpenReviewError({ name: 'Error', message: `Can not make assignment, submission ${sacsName} group not found.` }))
-        }
-        return
-      }
+  const { notes } = await client.getNotes({ id: edge.head })
+  const submission = notes[0]
+  const submissionGroupId = `${venueId}/${submissionName}${submission.number}`
+
+  if (!edge.ddate) {
+    const { count } = await client.getGroups({ id: `${submissionGroupId}/${sacsName}` })
+    if ( count === 0) {
+      return Promise.reject(new OpenReviewError({ name: 'Error', message: `Can not make assignment, submission ${sacsName} group not found.` }))
+    }
+    return
+  }
 }
