711936a8724df91d6d35742b10161c789f505211
Celeste Martinez <celeste@openreview.net>
2024-04-05 14:55:05 -0400

    fix sending only one email to note.signatures

--- openreview/venue/process/custom_stage_process.py
+++ openreview/venue/process/custom_stage_process.py
@@ -53,7 +53,7 @@ To view the {invitation_name}, click here: https://openreview.net/forum?id={subm
 
     #email tauthor
     client.post_message(
-        recipients=note.signatures,
+        recipients=[edit.tauthor],
         replyTo=contact,
         subject=f'''[{short_name}] Your {invitation_name} has been received on Paper Number: {submission.number}, Paper Title: "{submission.content['title']['value']}"''',
         message=f'''We have received your {invitation_name} on a submission to {short_name}.
