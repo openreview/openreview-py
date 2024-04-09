52eac40d51d3320f4c6dee65e867d7cb0d99dffb
Emilia Rubio <22416602+enrubio@users.noreply.github.com>
2024-04-09 17:05:54 -0400

    Add check for sac-ac assignment

--- openreview/venue_request/process/matching_pre_process.py
+++ openreview/venue_request/process/matching_pre_process.py
@@ -21,7 +21,8 @@ def process(client, note, invitation):
 
     if forum_note.content.get('api_version', '1') == '2':
         senior_area_chairs_id = domain.get_content_value('senior_area_chairs_id')
-        if senior_area_chairs_id and matching_group == senior_area_chairs_id:
+        senior_area_chairs_assignment = forum_note.content.get('senior_area_chairs_assignment', 'Area Chairs')
+        if senior_area_chairs_id and matching_group == senior_area_chairs_id and senior_area_chairs_assignment == 'Area Chairs':
             if compute_conflicts:
                 raise openreview.OpenReviewException('Conflicts are not computed between SACs and ACs. Please select "No" for Compute Conflicts.')
             return
@@ -35,5 +36,5 @@ def process(client, note, invitation):
         if compute_conflicts and senior_area_chairs_id and area_chairs_id and matching_group == area_chairs_id:
             senior_area_chairs_assignment_id = domain.get_content_value('senior_area_chairs_assignment_id')
             num_edges = client_v2.get_edges_count(invitation=senior_area_chairs_assignment_id)
-            if not num_edges:
+            if not num_edges and senior_area_chairs_assignment == 'Area Chairs':
                 raise openreview.OpenReviewException(f'Please deploy SAC-AC assignments first. SAC-submission conflicts must be transferred to assigned ACs before computing AC-submission conflicts.')
