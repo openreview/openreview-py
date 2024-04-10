d7ecf23cffdb94e9d6369a82710d80bcc3727d98
Emilia Rubio <22416602+enrubio@users.noreply.github.com>
2024-04-10 12:22:25 -0400

    Use sac_paper_assignments

--- openreview/venue_request/process/matching_pre_process.py
+++ openreview/venue_request/process/matching_pre_process.py
@@ -21,8 +21,8 @@ def process(client, note, invitation):
 
     if forum_note.content.get('api_version', '1') == '2':
         senior_area_chairs_id = domain.get_content_value('senior_area_chairs_id')
-        senior_area_chairs_assignment = forum_note.content.get('senior_area_chairs_assignment', 'Area Chairs')
-        if senior_area_chairs_id and matching_group == senior_area_chairs_id and senior_area_chairs_assignment == 'Area Chairs':
+        sac_paper_assignments = domain.get_content_value('sac_paper_assignments')
+        if senior_area_chairs_id and matching_group == senior_area_chairs_id and not sac_paper_assignments:
             if compute_conflicts:
                 raise openreview.OpenReviewException('Conflicts are not computed between SACs and ACs. Please select "No" for Compute Conflicts.')
             return
@@ -33,8 +33,8 @@ def process(client, note, invitation):
             raise openreview.OpenReviewException(f'Can not compute affinity scores for venues with 2000+ papers. Please contact us at info@openreview.net to compute your scores.')
 
         area_chairs_id = domain.get_content_value('area_chairs_id')
-        if compute_conflicts and senior_area_chairs_id and area_chairs_id and matching_group == area_chairs_id:
+        if compute_conflicts and senior_area_chairs_id and area_chairs_id and matching_group == area_chairs_id and not sac_paper_assignments:
             senior_area_chairs_assignment_id = domain.get_content_value('senior_area_chairs_assignment_id')
             num_edges = client_v2.get_edges_count(invitation=senior_area_chairs_assignment_id)
-            if not num_edges and senior_area_chairs_assignment == 'Area Chairs':
+            if not num_edges:
                 raise openreview.OpenReviewException(f'Please deploy SAC-AC assignments first. SAC-submission conflicts must be transferred to assigned ACs before computing AC-submission conflicts.')
