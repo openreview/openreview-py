aa0b1c8a31b458cb813325d4b65334617be79f86
Celeste Martinez <celeste@openreview.net>
2024-05-03 15:18:16 -0400

    edit post submission cdate when submission deadline is updated

--- openreview/venue/process/edit_submission_deadline_process.py
+++ openreview/venue/process/edit_submission_deadline_process.py
@@ -3,6 +3,7 @@ def process(client, edit, invitation):
     domain = client.get_group(edit.domain)
     venue_id = domain.id
     meta_invitation_id = domain.get_content_value('meta_invitation_id')
+    submission_name = domain.get_content_value('submission_name')
     submission_id = edit.invitation.id
     duedate = edit.invitation.duedate
 
@@ -19,3 +20,11 @@ def process(client, edit, invitation):
     )
 
     # update post submission cdate
+    client.post_invitation_edit(
+        invitations=meta_invitation_id,
+        signatures=[venue_id],
+        invitation=openreview.api.Invitation(
+            id=f'{venue_id}/-/Post_{submission_name}',
+            cdate=expdate
+        )
+    )
