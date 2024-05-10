714159f9a7ab22cc47e68c1246e748be781ca105
Melisa Bok <melisabok@gmail.com>
2024-05-10 12:01:41 -0400

    Fix tests: change label from papers to submissions (#2145)
    * change label from papers to submissions
    
    * fix neurips test

--- tests/test_aaai_conference.py
+++ tests/test_aaai_conference.py
@@ -268,4 +268,4 @@ program_committee4@yahoo.com, Program Committee AAAIFour
 
         request_page(selenium, "http://localhost:3030/group?id=AAAI.org/2025/Conference/Program_Committee", reviewer_client.token, wait_for_element='header')
         header = selenium.find_element(By.ID, 'header')
-        assert 'You have agreed to review up to 1 papers' in header.text
+        assert 'You have agreed to review up to 1 submission' in header.text
