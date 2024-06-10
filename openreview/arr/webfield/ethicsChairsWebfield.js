4b55cc440f3f4fd49c4b4ee6fdba7e16f4457531
Harold Rubio <harrywill.rubio@gmail.com>
2024-06-10 13:31:15 -0400

    Build edge browser link

--- openreview/arr/webfield/ethicsChairsWebfield.js
+++ openreview/arr/webfield/ethicsChairsWebfield.js
@@ -1,11 +1,27 @@
 // Webfield component
+const venueId = domain.id
+const ethicsReviewersId = `${venueId}/${domain.content.ethics_reviewers_name.value}`
+
+const traverse = `${ethicsReviewersId}/-/Assignment`
+const edit = [
+  `${ethicsReviewersId}/-/Assignment`,
+  `${ethicsReviewersId}/-/Invite_Assignment`
+]
+const browse = [
+  `${ethicsReviewersId}/-/Affinity_Score`,
+  `${ethicsReviewersId}/-/Conflict`,
+]
+const other = `version=2&referrer=[Ethics Chairs Console](/group?id=${entity.id})`
+
+const edgeBrowserLink = `/edges/browse?traverse=${traverse}&edit=${edit.join(';')}&browse=${browse.join(';')}&${other}`
+
 return {
     component: 'EthicsChairConsole',
     version: 1,
     properties: {
         header: {
             title: 'Ethics Chair Console',
-            instructions: `<p class="dark">This page provides information and status updates for ${domain.content.subtitle?.value}. It will be regularly updated as the conference progresses, so please check back frequently.</p>`
+            instructions: `<p class="dark">This page provides information and status updates for ${domain.content.subtitle?.value}. It will be regularly updated as the conference progresses, so please check back frequently.</p><br><strong>Assignment Browser:</strong><a id="edge_browser_url" href="` + edgeBrowserLink + `"> Modify Ethics Reviewers Assignments</a>`
         },
         venueId: domain.id,
         ethicsChairsName: domain.content.ethics_chairs_name?.value,
