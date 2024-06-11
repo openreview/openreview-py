0ba1448b900ddb5d25534e182e0d48a97038aaa0
Harold Rubio <harrywill.rubio@gmail.com>
2024-06-10 21:46:32 -0400

    Add SAC webfield changes

--- openreview/arr/webfield/seniorAreaChairsWebfield.js
+++ openreview/arr/webfield/seniorAreaChairsWebfield.js
@@ -40,6 +40,38 @@ const other = `hide=${domain.content.area_chairs_conflict_id?.value}&maxColumns=
 
 const edgeBrowserLink = `/edges/browse?start=${start}&traverse=${traverse}&edit=${edit}&browse=${browse.join(';')}&${other}`
 
+const assignmentUrls = {}
+const reviewersId = domain.content.reviewers_id?.value
+const areaChairsId = domain.content.area_chairs_id?.value
+const automaticAssignment = domain.content.automatic_reviewer_assignment?.value
+
+const browseReviewerInvitations = [
+  domain.content.reviewers_affinity_score_id?.value,
+  domain.content.reviewers_conflict_id?.value,
+  `${reviewersId}/-/Research_Area`,
+  `${reviewersId}/-/Status`,
+].join(';')
+
+const headBrowseInvitations = [
+  `${reviewersId}/-/Registered_Load`,
+  `${reviewersId}/-/Emergency_Load`,
+  `${reviewersId}/-/Emergency_Area`,
+  `${reviewersId}/-/Reviewing_Resubmissions`,
+  `${reviewersId}/-/Author_In_Current_Cycle`,
+  `${reviewersId}/-/Seniority`,
+].map(invitationId => `${invitationId},head:ignore`).join(';')
+
+const allBrowseInvitations = [
+  browseReviewerInvitations,
+  headBrowseInvitations,
+].join(';')
+
+const manualReviewerAssignmentUrl = `/edges/browse?traverse=${domain.content.reviewers_assignment_id?.value}&edit=${domain.content.reviewers_assignment_id?.value};${domain.content.reviewers_invite_assignment_id?.value}&browse=${allBrowseInvitations}&version=2`
+assignmentUrls[domain.content.reviewers_name?.value] = {
+  manualAssignmentUrl: manualReviewerAssignmentUrl,
+  automaticAssignment: automaticAssignment
+}
+
 return {
   component: 'SeniorAreaChairConsole',
   version: 1,
@@ -58,8 +90,11 @@ return {
     deskRejectedVenueId: domain.content.desk_rejected_venue_id?.value,
     submissionName: domain.content.submission_name?.value,
     reviewerName: domain.content.reviewers_name?.value,
+    assignmentUrls: assignmentUrls,
+    reviewersId: reviewersId,
     anonReviewerName: domain.content.reviewers_anon_name?.value,
     areaChairName: domain.content.area_chairs_name?.value,
+    areaChairsId: areaChairsId,
     anonAreaChairName: domain.content.area_chairs_anon_name?.value,
     secondaryAreaChairName: domain.content.secondary_area_chairs_name?.value,
     secondaryAnonAreaChairName: domain.content.secondary_area_chairs_anon_name?.value, 
@@ -74,6 +109,25 @@ return {
     enableQuerySearch: true,
     emailReplyTo: domain.content.contact?.value,
     edgeBrowserDeployedUrl: `/edges/browse?start=${startParam}&traverse=${traverseParam}&edit=${editParam}&browse=${browseInvitations.join(';')}${otherParams}`,
-    filterFunction: entity.content?.track?.value && `return note.content?.track?.value==="${entity.content?.track?.value}"`
+    filterFunction: entity.content?.track?.value && `return note.content?.track?.value==="${entity.content?.track?.value}"`,
+    propertiesAllowed: {
+      reviewerChecklistCount: `
+      console.log(row);
+      const invitationToCheck="Reviewer_Checklist"; 
+      const checklistReplies = row.note?.details?.replies.filter(reply => {
+        const hasReply = reply.invitations.some(invitation => invitation.includes(invitationToCheck)); 
+        return hasReply;
+      })
+      return checklistReplies?.length??0;
+      `,
+      actionEditorChecklistCount: `
+      const invitationToCheck="Action_Editor_Checklist"; 
+      const checklistReplies = row.note?.details?.replies.filter(reply => {
+        const hasReply = reply.invitations.some(invitation => invitation.includes(invitationToCheck)); 
+        return hasReply;
+      })
+      return checklistReplies?.length??0;
+      `
+    }
   }
 }
