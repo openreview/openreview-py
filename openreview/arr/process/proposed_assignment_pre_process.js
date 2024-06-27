e76c7b50151a9c4aec1d2fd1b71cac86acfc05c8
Harold Rubio <harrywill.rubio@gmail.com>
2024-06-27 15:24:24 -0400

    Add proposed assignment preprocess

---
+++ openreview/arr/process/proposed_assignment_pre_process.js
@@ -0,0 +1,46 @@
+async function process(client, edge, invitation) {
+  client.throwErrors = true
+
+  const committeeName = invitation.content.committee_name?.value;
+  const { groups } = await client.getGroups({ id: invitation.domain });
+  const domain = groups[0];
+
+  const customMaxPapersId = domain.content[committeeName.toLowerCase() + '_custom_max_papers_id']?.value;
+
+  if (edge.ddate) {
+    return
+  }
+
+  if (edge.tcdate !== edge.tmdate) {
+    return
+  }
+
+  if (!customMaxPapersId) {
+    return
+  }
+
+  const [res1, res2, res3] = await Promise.all([
+    client.getEdges({ invitation: customMaxPapersId, tail: edge.tail }),
+    client.getEdges({ invitation: edge.invitation, label: edge.label, tail: edge.tail }),
+    client.getEdges({ invitation: edge.invitation, label: edge.label, head: edge.head })
+  ])
+  let customMaxPapers = (res1.count > 0 && res1.edges[0].weight) || 0;
+  const assignmentEdges = res2.edges;
+  const submissionEdges = res3.edges;
+
+  if (!customMaxPapers) {
+    const { invitations } = await client.getInvitations({ id: customMaxPapersId });
+    customMaxPapers = invitations[0].edge.weight.param.default;
+  }
+
+  if (assignmentEdges.length >= customMaxPapers) {
+    const { profiles } = await client.getProfiles({ id: edge.tail });
+    const profile = profiles[0];
+    return Promise.reject(new OpenReviewError({ name: 'Error', message: `Max Papers allowed reached for ${Tools.getPreferredName(profile)}` }));
+  }
+
+  if ((submissionEdges.length + 1) > 3 && committeeName.includes('Reviewers')) {
+    return Promise.reject(new OpenReviewError({ name: 'Error', message: `You cannot assign more than 3 reviewers to this paper` }));
+  }
+
+}
