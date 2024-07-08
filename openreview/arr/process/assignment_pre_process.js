9c1ea682b7f1aad08c32e0325467abc3b7d8b186
Harold Rubio <harrywill.rubio@gmail.com>
2024-07-08 12:22:04 -0400

    Copy correct pre-process

---
+++ openreview/arr/process/assignment_pre_process.js
@@ -0,0 +1,37 @@
+async function process(client, edge, invitation) {
+  client.throwErrors = false
+
+  const { groups } = await client.getGroups({ id: edge.domain })
+  const domain = groups[0]
+  const venueId = domain.id
+  const submissionName = domain.content.submission_name?.value
+  const reviewName = invitation.content.review_name?.value
+  const reviewersAnonName = invitation.content.reviewers_anon_name?.value
+  const reviewersName = invitation.content.reviewers_name?.value
+
+  const { notes } = await client.getNotes({ id: edge.head })
+  const submission = notes[0]
+  const submissionGroupId = `${venueId}/${submissionName}${submission.number}`
+
+  if (!edge.ddate) {
+    const { count } = await client.getGroups({ id: `${submissionGroupId}/${reviewersName}` })
+    if ( count === 0) {
+      return Promise.reject(new OpenReviewError({ name: 'Error', message: `Can not make assignment, submission ${reviewersName} group not found.` }))
+    }
+    return
+  }
+
+  if (reviewersAnonName) {
+    const { groups: anonGroups, count: groupCount } = await client.getGroups({ prefix: `${submissionGroupId}/${reviewersAnonName}`, signatory: edge.tail })
+
+    if (groupCount === 0) { 
+      return Promise.reject(new OpenReviewError({ name: 'Error', message: `Can remove assignment, signatory groups not found for ${edge.tail}` }))
+    }
+
+    const { count: noteCount } = await client.getNotes({ invitation: `${submissionGroupId}/-/` + reviewName, signatures: anonGroups[0].id })
+
+    if (noteCount > 0) {
+      return Promise.reject(new OpenReviewError({ name: 'Error', message: `Can not remove assignment, the user ${edge.tail} already posted a ${reviewName.replace('_', ' ')}` }))
+    }
+  }
+}
