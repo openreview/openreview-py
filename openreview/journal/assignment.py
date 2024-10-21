849f116dcb18d3d3b8797a92f88ca36ab5a94069
Celeste Martinez <celeste@openreview.net>
2024-10-21 16:02:44 -0400

    check if the active submission assignment was made within the last two months

--- openreview/journal/assignment.py
+++ openreview/journal/assignment.py
@@ -217,12 +217,14 @@ class Assignment(object):
         all_submissions = { s.id: s for s in self.client.get_all_notes(invitation= journal.get_author_submission_id(), details='directReplies')}
         available_edges = { e['id']['tail']: e['values'][0]['label'] for e in self.client.get_grouped_edges(invitation=journal.get_ae_availability_id(), groupby='tail', select='label') }
         quota_edges = { e['id']['tail']: e['values'][0]['weight'] for e in self.client.get_grouped_edges(invitation=journal.get_ae_custom_max_papers_id(), groupby='tail', select='weight') }
-        assignments_by_ae = { e['id']['tail']: [v['head'] for v in e['values']] for e in self.client.get_grouped_edges(invitation=journal.get_ae_assignment_id(), groupby='tail', select='head') }
+        assignments_by_ae = { e['id']['tail']: [v for v in e['values']] for e in self.client.get_grouped_edges(invitation=journal.get_ae_assignment_id(), groupby='tail') }
 
         ## Clear the quotas
         self.client.delete_edges(invitation=journal.get_ae_local_custom_max_papers_id(), soft_delete=True, wait_to_finish=True)
 
         max_active_submissions = 2
+        now = datetime.datetime.utcnow()
+        two_months_ago = now - datetime.timedelta(days=60)
 
         custom_load_edges = []
         for action_editor in tqdm(action_editors):
@@ -233,9 +235,14 @@ class Assignment(object):
                 # they have 0 or 1 assigned AE for which no decision was made
                 assignments = assignments_by_ae.get(action_editor, [])
                 no_decision_count = 0
-                for assignment in assignments:
-                    submission = all_submissions.get(assignment)
-                    if submission and journal.is_active_submission(submission) and not [d for d in submission.details['directReplies'] if journal.get_ae_decision_id(number=submission.number) in d['invitations']]:
+                for assignment_edge in assignments:
+                    submission = all_submissions.get(assignment_edge['head'])
+                    if (
+                        submission and
+                        journal.is_active_submission(submission) and
+                        not [d for d in submission.details['directReplies'] if journal.get_ae_decision_id(number=submission.number) in d['invitations']] and
+                        two_months_ago < datetime.datetime.fromtimestamp(assignment_edge['cdate']/1000)
+                    ):
                         no_decision_count += 1
                 if no_decision_count < max_active_submissions:
                     # they have sufficient total quota of assignment
