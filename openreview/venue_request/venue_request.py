d776721ac37ae05e9aea2a89d1bcfc349980b181
Akshat Chhabra <akshat@openreview.net>
2024-11-07 11:04:33 -0800

    docs: change add_to_index field description

--- openreview/venue_request/venue_request.py
+++ openreview/venue_request/venue_request.py
@@ -1651,7 +1651,7 @@ class VenueRequest():
                 'hidden': True
             },
             'iThenticate_plagiarism_check_add_to_index': {
-                'description': 'Your iThenticate account has a repository. This option controls whether or not the submission is added to your iThenticate account\'s repository. If set to Yes, future submissions can match against the indexed submissions. Your account repository is private and no other institutions can search against your indexed documents.',
+                'description': 'Your iThenticate account has a repository. Your account repository is private and no other iThenticate account can search against your indexed documents. The add to index option controls whether or not submissions are added to your iThenticate account\'s repository. If set to Yes, the submissions will be indexed and can be matched with future submissions made to the venue.',
                 'value-radio': ['Yes', 'No'],
                 'default': 'No',
                 'order': 53,
