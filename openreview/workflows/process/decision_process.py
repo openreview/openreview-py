b5487f66edddc868f41893a4abc69acbd0d8915d
Celeste Martinez <celeste@openreview.net>
2024-11-26 19:18:20 -0500

    add venue and venueid in decision process

--- openreview/workflows/process/decision_process.py
+++ openreview/workflows/process/decision_process.py
@@ -1,14 +1,17 @@
 def process(client, edit, invitation):
 
+    import datetime
+
     domain = client.get_group(edit.domain)
     venue_id = domain.id
     meta_invitation_id = domain.get_content_value('meta_invitation_id')
     short_name = domain.get_content_value('subtitle')
     contact = domain.get_content_value('contact')
     authors_name = domain.get_content_value('authors_name')
-    submission_name = domain.get_content_value('submission_name')    
-    authors_accepted_id = domain.get_content_value('authors_accepted_id') 
-    sender = domain.get_content_value('message_sender')   
+    submission_name = domain.get_content_value('submission_name')
+    authors_accepted_id = domain.get_content_value('authors_accepted_id')
+    sender = domain.get_content_value('message_sender')
+    rejected_venue_id = domain.get_content_value('rejected_venue_id')
 
     submission = client.get_note(edit.note.forum)
     decision = client.get_note(edit.note.id)
@@ -20,7 +23,7 @@ def process(client, edit, invitation):
 
     action = 'posted to' if decision.tcdate == decision.tmdate else 'edited on'
 
-    if (parent_invitation.get_content_value('email_pcs', False) and ('everyone' in decision.readers or paper_authors_id in decision.readers)):
+    if (parent_invitation.get_content_value('email_authors', False) and ('everyone' in decision.readers or paper_authors_id in decision.readers)):
 
         client.post_message(
             invitation=meta_invitation_id,
@@ -32,9 +35,34 @@ def process(client, edit, invitation):
             sender=sender
         )
 
-    if (authors_accepted_id):
-      accept_options = domain.get_content_value('accept_decision_options')
-      if openreview.tools.is_accept_decision(decision.content['decision']['value'], accept_options):
-        client.add_members_to_group(authors_accepted_id, paper_authors_id)
-      else:
-        client.remove_members_from_group(authors_accepted_id, paper_authors_id)
+    accept_options = parent_invitation.get_content_value('accept_decision_options', [])
+    paper_decision = decision.content['decision']['value']
+    note_accepted = openreview.tools.is_accept_decision(paper_decision, accept_options)
+
+    if note_accepted:
+      client.add_members_to_group(authors_accepted_id, paper_authors_id)
+    else:
+      client.remove_members_from_group(authors_accepted_id, paper_authors_id)
+
+    # update venue and venueid
+    venue = openreview.tools.decision_to_venue(short_name, paper_decision, accept_options)
+    venueid = venue_id if note_accepted else rejected_venue_id
+
+    client.post_note_edit(
+       invitation=meta_invitation_id,
+       readers=[venue_id, paper_authors_id],
+       writers=[venue_id],
+       signatures =[venue_id],
+       note=openreview.api.Note(
+          id=submission.id,
+          content={
+             'venueid': {
+                'value': venueid
+             },
+             'venue': {
+                'value': venue
+             }
+          },
+          pdate = openreview.tools.datetime_millis(datetime.datetime.utcnow()) if (submission.pdate is None and note_accepted) else None
+       )
+    )
