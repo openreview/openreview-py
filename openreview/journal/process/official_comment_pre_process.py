bc1e6a317a97eaf22f7495938ae6ef725fecb9b3
Celeste Martinez <celeste@openreview.net>
2025-01-22 09:36:22 -0500

    remove preprocess

--- openreview/journal/process/official_comment_pre_process.py
+++ openreview/journal/process/official_comment_pre_process.py
@@ -18,9 +18,3 @@ def process(client, edit, invitation):
         reader=m.replace('{number}', str(forum.number))
         if reader not in note.readers:
             raise openreview.OpenReviewException(f'{openreview.tools.pretty_id(reader)} must be readers of the comment')
-
-    replyto = note.replyto
-    parent_note = client.get_note(replyto)
-    volunteer_comment = 'Volunteer_to_Review' in parent_note.invitations[0]
-    if volunteer_comment:
-        raise openreview.OpenReviewException('To reply to a volunteer, use the "Volunteer to Review Comment" button')
