0e90640846474f266e090c5ee7466495c09e670a
Akshat Chhabra <akshat@openreview.net>
2025-02-03 11:13:50 -0800

    fix: only add modified fields to the invitation

--- openreview/venue_request/process/ithenticate_eula_process.py
+++ openreview/venue_request/process/ithenticate_eula_process.py
@@ -14,16 +14,29 @@ def process(client, invitation):
     submission_invitation = client.get_invitation(domain_group.content["submission_id"]["value"])
 
     content = submission_invitation.edit["note"]["content"]
-    modified_invitation = openreview.api.Invitation(id=submission_invitation.id, edit=submission_invitation.edit)
+    modified_invitation = openreview.api.Invitation(id=submission_invitation.id)
+    modified_invitation.edit = {"note": {"content": {}}}
     modified_invitation_content = modified_invitation.edit["note"]["content"]
     if "iThenticate_agreement" in content:
         iThenticate_agreement_object = content["iThenticate_agreement"]
         iThenticate_agreement_value = iThenticate_agreement_object["value"]["param"]["enum"][0]
         current_iThenticate_eula_version = iThenticate_agreement_value.split(":")[-1].strip()
         if current_iThenticate_eula_version != eula_version:
-            modified_iThenticate_agreement_object = modified_invitation_content["iThenticate_agreement"]
-            modified_iThenticate_agreement_object["description"] = f"The venue is using iThenticate for plagiarism detection. By submitting your paper, you agree to share your PDF with iThenticate and accept iThenticate's End User License Agreement. Read the full terms here: {eula_link}"
-            modified_iThenticate_agreement_object["value"]["param"]["enum"] = [f"Yes, I agree to iThenticate's EULA agreement version: {eula_version}"]
+            modified_invitation_content["iThenticate_agreement"] = {
+                "description": f"The venue is using iThenticate for plagiarism detection. By submitting your paper, you agree to share your PDF with iThenticate and accept iThenticate's End User License Agreement. Read the full terms here: {eula_link}",
+                "value": {
+                    "param": {
+                        "fieldName": "iThenticate Agreement",
+                        "type": "string",
+                        "optional": False,
+                        "input": "checkbox",
+                        "enum": [
+                            f"Yes, I agree to iThenticate's EULA agreement version: {eula_version}"
+                        ],
+                    }
+                },
+            }
+            
             client.post_invitation_edit(
                 invitations=domain_group.content["meta_invitation_id"]["value"],
                 readers=[domain_group.domain],
