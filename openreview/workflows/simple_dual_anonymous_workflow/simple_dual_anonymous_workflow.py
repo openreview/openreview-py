90498b9c0a0df09683a92d387cb3da15bfc5d1c0
Celeste Martinez <celeste@openreview.net>
2025-02-25 08:58:00 -0500

    add reviewers invited description

--- openreview/workflows/simple_dual_anonymous_workflow/simple_dual_anonymous_workflow.py
+++ openreview/workflows/simple_dual_anonymous_workflow/simple_dual_anonymous_workflow.py
@@ -3739,6 +3739,7 @@ To view your submission, click here: https://openreview.net/forum?id={{note_foru
                 'writers': ['~Super_User1'],
                 'group': {
                     'id': '${2/content/venue_id/value}/${2/content/reviewers_name/value}_Invited',
+                    'description': '<span class="text-muted">Group that contains the users who have been invited to act as reviewers for the venue.</span>',
                     'readers': ['${3/content/venue_id/value}'],
                     'writers': ['${3/content/venue_id/value}'],
                     'signatures': ['${3/content/venue_id/value}'],
