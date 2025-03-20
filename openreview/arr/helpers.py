3bc62e3f6ed1db5d63ceb90ace1ca36d3dee6982
Harold Rubio <harrywill.rubio@gmail.com>
2025-03-20 16:12:54 -0400

    Review readers to readers

--- openreview/arr/helpers.py
+++ openreview/arr/helpers.py
@@ -369,7 +369,7 @@ class ARRWorkflow(object):
                         venue.id + "/Program_Chairs",
                         venue.id + "/Submission{number}/Senior_Area_Chairs",
                         venue.id + "/Submission{number}/Area_Chairs",
-                        venue.id + "/Submission{number}/Reviewers/Submitted"
+                        venue.id + "/Submission{number}/Reviewers"
                     ]
                 }
             },
@@ -427,7 +427,7 @@ class ARRWorkflow(object):
                         venue.id + "/Program_Chairs",
                         venue.id + "/Submission{number}/Senior_Area_Chairs",
                         venue.id + "/Submission{number}/Area_Chairs",
-                        venue.id + "/Submission{number}/Reviewers/Submitted"
+                        venue.id + "/Submission{number}/Reviewers"
                     ]
                 }
             },
