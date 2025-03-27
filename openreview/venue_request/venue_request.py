690781e0ee267abdece1fb73a5ceca7afa92c947
Celeste Martinez <celeste@openreview.net>
2025-03-27 09:51:26 -0400

    update order of new iThenticate fields

--- openreview/venue_request/venue_request.py
+++ openreview/venue_request/venue_request.py
@@ -1674,7 +1674,7 @@ class VenueRequest():
             'iThenticate_plagiarism_check': {
                 'description': 'Indicate whether you would like to use iThenticate with OpenReview for plagiarism report generation.',
                 'value-radio': ['Yes', 'No'],
-                'order': 49,
+                'order': 51,
                 'required': False,
                 'hidden': True
             },
@@ -1703,96 +1703,96 @@ class VenueRequest():
             'iThenticate_plagiarism_check_add_to_index': {
                 'description': 'Your iThenticate account has a repository. Your account repository is private and no other iThenticate account can search against your indexed documents. The add to index option controls whether or not submissions are added to your iThenticate account\'s repository. If set to Yes, the submissions will be indexed and can be matched with future submissions made to the venue.',
                 'value-radio': ['Yes', 'No'],
-                'order': 53,
+                'order': 55,
                 'required': False,
                 'hidden': True
             },
             'iThenticate_plagiarism_check_exclude_quotes': {
                 'description': 'If set to true, text in quotes will not count as similar content.',
                 'value-radio': ['Yes', 'No'],
-                'order': 54,
+                'order': 56,
                 'required': False,
                 'hidden': True
             },
             'iThenticate_plagiarism_check_exclude_bibliography': {
                 'description': 'If set to true, text in a bibliography section will not count as similar content.',
                 'value-radio': ['Yes', 'No'],
-                'order': 55,
+                'order': 57,
                 'required': False,
                 'hidden': True
             },
             'iThenticate_plagiarism_check_exclude_abstract': {
                 'description': 'If set to true, text in the abstract section of the submission will not count as similar content.',
                 'value-radio': ['Yes', 'No'],
-                'order': 56,
+                'order': 58,
                 'required': False,
                 'hidden': True
             },
             'iThenticate_plagiarism_check_exclude_methods': {
                 'description': 'If set to true, text in the method section of the submission will not count as similar content',
                 'value-radio': ['Yes', 'No'],
-                'order': 57,
+                'order': 59,
                 'required': False,
                 'hidden': True
             },
             'iThenticate_plagiarism_check_exclude_internet': {
                 'description': 'If set to true, text matched to the Internet Collection will not count as similar content. The Internet Collection includes publicly accessible web pages, articles, blogs, and other online content used for plagiarism detection.',
                 'value-radio': ['Yes', 'No'],
-                'order': 58,
+                'order': 60,
                 'required': False,
                 'hidden': True
             },
             'iThenticate_plagiarism_check_exclude_publications': {
                 'description': ' If set to true, text matched to the Publications Collection will not count as similar content. The Publications Collection consists of published academic papers, journals, books, and other scholarly content used to detect plagiarism from external sources.',
                 'value-radio': ['Yes', 'No'],
-                'order': 59,
+                'order': 61,
                 'required': False,
                 'hidden': True
             },
             'iThenticate_plagiarism_check_exclude_submitted_works': {
                 'description': 'If set to true, text matched to the Submitted Works Collection will not count as similar content. The Submitted Works Collection consists of the works that have been previously submitted to the iThenticate account and can be used as comparison sources.',
                 'value-radio': ['Yes', 'No'],
-                'order': 60,
+                'order': 62,
                 'required': False,
                 'hidden': True
             },
              'iThenticate_plagiarism_check_exclude_citations': {
                 'description': 'If set to true, it will exclude citations. Using machine learning techniques we identify and exclude inline citations in the APA, MLA, and Turabian style formats.',
                 'value-radio': ['Yes', 'No'],
-                'order': 61,
+                'order': 63,
                 'required': False,
                 'hidden': True
             },
             'iThenticate_plagiarism_check_exclude_preprints': {
                 'description': 'If set to true, it will exclude a predefined set of pre-print sources. A pre-print is a version of a scholarly or scientific paper that precedes formal peer review and publication in a peer-reviewed scholarly or scientific journal. If you have a custom source you would like to exclude, you can add it to the list of custom websites in the admin console. This setting can be overridden by our admin settings page and please check that page to ensure the pre-prints setting is configured and enabled.',
                 'value-radio': ['Yes', 'No'],
-                'order': 62,
+                'order': 64,
                 'required': False,
                 'hidden': True
             },
              'iThenticate_plagiarism_check_exclude_custom_sections': {
                 'description': 'If set to true, text matched to the custom sections defined in the admin settings will not count as similar content.',
                 'value-radio': ['Yes', 'No'],
-                'order': 63,
+                'order': 65,
                 'required': False,
                 'hidden': True
             },
             'iThenticate_plagiarism_check_exclude_small_matches': {
                 'description': 'If set, similarity matches that match less than the specified amount of words will not count as similar content.',
                 'value-regex': '[0-9]*',
-                'order': 64,
+                'order': 66,
                 'required': False,
                 'hidden': True
             },
             'submission_assignment_max_reviewers': {
                 'value-regex': '[0-9]*',
-                'order': 65,
+                'order': 67,
                 'required': False,
                 'hidden': True
             },
             'comment_notification_threshold': {
                 'value-regex': '.*',
-                'order': 66,
+                'order': 68,
                 'required': False,
                 'hidden': True
             }
