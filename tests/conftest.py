c948c06ba707bbac820d911e885a51ac42fb45f9
celestemartinez <32438984+celestemartinez@users.noreply.github.com>
2025-06-11 15:18:46 -0400

    Feat/ Venue Config: refactor + AC workflow (#2579)
    * move venue request to separate file
    
    * rename templates file
    
    * rename all template invitations
    
    * move the templates and processes to another folder
    
    * rename reviewwers only test
    
    * start with AC and reviewers workflow
    
    * remove creating of workflow groups
    
    * test for parent_invitations
    
    * specify reviewers and ACs name in request form
    
    * change invitation prefix
    
    * fix template prefix
    
    * fix test
    
    * use one deployment invitation for reviewers only workflow
    
    * use one deployment invitation for acs  workflow
    
    * add additional readers in order to resuse templates for other workflows
    
    * do not use super user when it is not needed
    
    * test venue class with templates
    
    * use template to create PC group
    
    * test add PCs
    
    * use a single template to create committee groups
    
    * parametrize webfields
    
    * use templates in group.py
    
    * rollback group.py changes;do not use templaetes for PC and author groups
    
    * refactor committee group template
    
    * use request form invitation to decide when to create a template or not
    
    * create templates or edit invitations when the venue is a template_related_workflow
    
    * fix test for non template venues
    
    * fix signature
    
    * fix extra tabs
    
    * fix process index
    
    * fix another test
    
    * do not use Submission Group template; create edit invitations
    
    * fix deanonymizers edit invitation
    
    * do not use withdrawal templates; create edit invitations
    
    * fix submission invitation
    
    * fix test
    
    * do not use desk-rejection templates; create edit invitations
    
    * do not use Review template; create edit invitations
    
    * fix how to get reviews
    
    * get venue instance and use it to setup registration tasks
    
    * test paper submission
    
    * do not use Comment template; create edit invitations
    
    * do not use Rebuttal template; create edit invitations
    
    * fix withdraw and desk reject exp dates
    
    * fix submission change before bidding for area chairs only
    
    * fix test
    
    * do not use Decision template; create edit invitations
    
    * fix test
    
    * edit submission reader in the changes before bidding, create AC/SAC submission groups, update PC_Revision
    
    * read old rebuttal propoerty as well
    
    * fix pc revision
    
    * fix test
    
    * use venue code to upload decisions
    
    * fix test
    
    * read submission name from submission stage object
    
    * use matching code to compute affinity scores and conflicts
    
    * set a default value for role names
    
    * do not use Bid template; create edit invitations
    
    * do not use Revision template; create edit invitations
    
    * fix tests
    
    * read email_authors from decision stage
    
    * fix typo
    
    ---------
    
    Co-authored-by: Melisa Bok <545506+melisabok@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -90,7 +90,7 @@ class Helpers:
         assert not [l for l in super_client.get_process_logs(status='error') if l['executedOn'] == 'openreview-api-1']
 
     @staticmethod
-    def await_queue_edit(super_client, edit_id=None, invitation=None, count=1, error=False):
+    def await_queue_edit(super_client, edit_id=None, invitation=None, count=1, error=False, process_index=0):
         super_client = Helpers.get_user('openreview.net')
         expected_status = 'error' if error else 'ok'
         finished_status = ['error', 'ok']
@@ -98,7 +98,7 @@ class Helpers:
         wait_time = 0.5
         cycles = 60 * 1 / wait_time # print every 1 minutes
         while True:
-            process_logs = super_client.get_process_logs(id=edit_id, invitation=invitation)[:count]
+            process_logs = [l for l in super_client.get_process_logs(id=edit_id, invitation=invitation) if l.get('processIndex', 0) == process_index][:count]
             if len(process_logs) == count and all(process_log['status'] in finished_status for process_log in process_logs):
                 for process_log in process_logs:
                     assert process_log['status'] == (expected_status), process_log.get('log', 'No log available')
