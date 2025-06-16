b01b5a1d20838e8743d9179357f0df4228e24dc3
Carlos Mondragon <carlos@openreview.net>
2025-06-16 11:22:40 -0400

    Chore: Remove commented code

--- openreview/venue/invitation.py
+++ openreview/venue/invitation.py
@@ -4723,64 +4723,6 @@ class InvitationBuilder(object):
         invitation_id = f'{reviewers_id}/-/{metric_name}'
         readers = f'{reviewers_id}/{metric_name}/Readers'
         nonreaders = f'{reviewers_id}/{metric_name}/NonReaders'
-        # invitation = openreview.api.Invitation(
-        #     id=invitation_id,
-        #     invitees=[],
-        #     readers=[venue_id],
-        #     writers=[],
-        #     signatures=['~Super_User1'],
-        #     minReplies=1,
-        #     maxReplies=1,
-        #     type='Edge',            
-        #     edit={
-        #         'id': {
-        #             'param': {
-        #                 'withInvitation': invitation_id,
-        #                 'optional': True
-        #             }
-        #         },
-        #         'ddate': {
-        #             'param': {
-        #                 'range': [ 0, 9999999999999 ],
-        #                 'optional': True,
-        #                 'deletable': True
-        #             }
-        #         },
-        #         'cdate': {
-        #             'param': {
-        #                 'range': [ 0, 9999999999999 ],
-        #                 'optional': True,
-        #                 'deletable': True
-        #             }
-        #         },
-        #         'readers': [
-        #             venue_id,
-        #             readers,
-        #             '${2/tail}'
-        #         ],
-        #         'nonreaders': [nonreaders],
-        #         'writers': [venue_id],
-        #         'signatures': [venue_id],
-        #         'head': {
-        #             'param': {
-        #                 'type': 'group',
-        #                 'const': reviewers_id
-        #             }
-        #         },
-        #         'tail': {
-        #             'param': {
-        #                 'type': 'profile',
-        #                 'inGroup' : reviewers_id
-        #             }
-        #         },
-        #         'weight': {
-        #             'param': {
-        #                 'minimum': 0
-        #             }
-        #         }
-        #     }
-        # )  
-        # 
         invitation = openreview.api.Invitation(
             id=invitation_id,
             invitees=[],
