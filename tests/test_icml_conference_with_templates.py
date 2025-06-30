03cd0beb9dbdef076b3277ff71a6a20f7d0141cf
Celeste Martinez <celeste@openreview.net>
2025-06-30 12:11:06 -0400

    fix icml test

--- tests/test_icml_conference_with_templates.py
+++ tests/test_icml_conference_with_templates.py
@@ -102,7 +102,8 @@ class TestICMLConference():
                 'authors_name': { 'value': venue.authors_name },
                 'additional_readers': { 'value': [
                     'ICML.cc/2025/Conference/Senior_Area_Chairs',
-                    'ICML.cc/2025/Conference/Area_Chairs'
+                    'ICML.cc/2025/Conference/Area_Chairs',
+                    'ICML.cc/2025/Conference/Reviewers'
                 ] }                
             }
         )
@@ -884,36 +885,27 @@ reviewer6@yahoo.com, Reviewer ICMLSix
         assert desk_reject_inv.expdate == expdate
         assert pc_client.get_invitation('ICML.cc/2025/Conference/-/PC_Revision')
 
-        ## make submissions visible to SACs, ACs only
+        # ## make submissions visible to SACs, ACs only
         
-        ac_client = openreview.api.OpenReviewClient(username = 'ac1@icml.cc', password=helpers.strong_password)
-        submissions = ac_client.get_notes(invitation='ICML.cc/2025/Conference/-/Submission', sort='number:asc')
-        assert len(submissions) == 101
-        assert ['ICML.cc/2025/Conference',
-        'ICML.cc/2025/Conference/Senior_Area_Chairs',
-        'ICML.cc/2025/Conference/Area_Chairs',
-        'ICML.cc/2025/Conference/Submission1/Authors'] == submissions[0].readers
-        assert ['ICML.cc/2025/Conference',
-        'ICML.cc/2025/Conference/Submission1/Authors'] == submissions[0].writers
-        assert ['ICML.cc/2025/Conference/Submission1/Authors'] == submissions[0].signatures
-        assert 'authorids' not in submissions[0].content
-        assert 'authors' not in submissions[0].content
-        assert 'pdf' not in submissions[0].content
-        assert 'financial_aid' in submissions[0].content
-        assert not submissions[0].odate
+        # ac_client = openreview.api.OpenReviewClient(username = 'ac1@icml.cc', password=helpers.strong_password)
+        # submissions = ac_client.get_notes(invitation='ICML.cc/2025/Conference/-/Submission', sort='number:asc')
+        # assert len(submissions) == 101
+        # assert ['ICML.cc/2025/Conference',
+        # 'ICML.cc/2025/Conference/Senior_Area_Chairs',
+        # 'ICML.cc/2025/Conference/Area_Chairs',
+        # 'ICML.cc/2025/Conference/Submission1/Authors'] == submissions[0].readers
+        # assert ['ICML.cc/2025/Conference',
+        # 'ICML.cc/2025/Conference/Submission1/Authors'] == submissions[0].writers
+        # assert ['ICML.cc/2025/Conference/Submission1/Authors'] == submissions[0].signatures
+        # assert 'authorids' not in submissions[0].content
+        # assert 'authors' not in submissions[0].content
+        # assert 'pdf' not in submissions[0].content
+        # assert 'financial_aid' in submissions[0].content
+        # assert not submissions[0].odate
 
         pc_client.post_invitation_edit(
             invitations='ICML.cc/2025/Conference/-/Submission_Change_Before_Bidding/Restrict_Field_Visibility',
             content={
-                'submission_readers': {
-                    'value': [
-                        'ICML.cc/2025/Conference',
-                        'ICML.cc/2025/Conference/Senior_Area_Chairs',
-                        'ICML.cc/2025/Conference/Area_Chairs',
-                        'ICML.cc/2025/Conference/Reviewers',
-                        'ICML.cc/2025/Conference/Submission${{2/id}/number}/Authors'
-                    ]
-                },
                 'content_readers': { 
                     'value': {
                         "authors": {
@@ -947,6 +939,7 @@ reviewer6@yahoo.com, Reviewer ICMLSix
 
         helpers.await_queue_edit(openreview_client, 'ICML.cc/2025/Conference/-/Submission_Change_Before_Bidding-0-1', count=3)
 
+        ac_client = openreview.api.OpenReviewClient(username = 'ac1@icml.cc', password=helpers.strong_password)
         submissions = ac_client.get_notes(invitation='ICML.cc/2025/Conference/-/Submission', sort='number:asc')
         assert len(submissions) == 101
         assert ['ICML.cc/2025/Conference',
