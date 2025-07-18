c994a7d6995a13549ca228803078a997eaefd032
Yoshitomo Matsubara <dr.yoshitomo.matsubara@gmail.com>
2025-07-17 23:37:13 -0700

    Add a new option

--- openreview/venue/matching.py
+++ openreview/venue/matching.py
@@ -1022,7 +1022,8 @@ class Matching(object):
             )
 
         if compute_conflicts:
-            self._build_conflicts(submissions, user_profiles, openreview.tools.get_neurips_profile_info if compute_conflicts == 'NeurIPS' else openreview.tools.get_profile_info, compute_conflicts_n_years)
+            func_to_get_profile = openreview.tools.get_neurips_profile_info if compute_conflicts == 'NeurIPS' else openreview.tools.get_comprehensive_profile_info if compute_conflicts == 'Comprehensive' else openreview.tools.get_profile_info
+            self._build_conflicts(submissions, user_profiles, func_to_get_profile, compute_conflicts_n_years)
 
         if venue.automatic_reviewer_assignment:
             invitation = self._create_edge_invitation(venue.get_assignment_id(self.match_group.id))
