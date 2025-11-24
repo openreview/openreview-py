32597e0ecb3b431fe4ccd9f5f8268ab3e0ee1852
celestemartinez <32438984+celestemartinez@users.noreply.github.com>
2025-11-24 16:10:23 -0500

    Fix/tmlr fix (#2772)
    * allow archived rev to accept and remove from archived group
    
    * allow revs and aes to be reinvited by EICs
    
    * add fieldName for submission_length
    
    * revert changes to test
    
    * always allow users to be reinvited
    
    * add submission start date and deadline to settings
    
    * remove reinvite keyword
    
    ---------
    
    Co-authored-by: Melisa Bok <545506+melisabok@users.noreply.github.com>

--- openreview/journal/recruitment.py
+++ openreview/journal/recruitment.py
@@ -29,8 +29,6 @@ class Recruitment(object):
             memberships = [g.id for g in self.client.get_groups(member=invitee, prefix=action_editors_id)] if tools.get_group(self.client, invitee) else []
             if action_editors_id in memberships:
                 recruitment_status['already_member'].append(invitee)
-            elif action_editors_invited_id in memberships:
-                recruitment_status['already_invited'].append(invitee)
             else:
                 profile=openreview.tools.get_profile(self.client, invitee)
                 invitee = profile.id if profile else invitee
@@ -61,7 +59,7 @@ class Recruitment(object):
 
         return recruitment_status
 
-    def invite_reviewers(self, message, subject, invitees, invitee_names=None, replyTo=None, reinvite=False):
+    def invite_reviewers(self, message, subject, invitees, invitee_names=None, replyTo=None):
 
         reviewers_id = self.journal.get_reviewers_id()
         reviewers_declined_id = reviewers_id + '/Declined'
@@ -82,8 +80,6 @@ class Recruitment(object):
             memberships = [g.id for g in self.client.get_groups(member=invitee, prefix=reviewers_id)] if tools.get_group(self.client, invitee) else []
             if reviewers_id in memberships:
                 recruitment_status['already_member'].append(invitee)
-            elif not reinvite and reviewers_invited_id in memberships:
-                recruitment_status['already_invited'].append(invitee)
             else:
                 profile=openreview.tools.get_profile(self.client, invitee)
                 invitee = profile.id if profile else invitee
