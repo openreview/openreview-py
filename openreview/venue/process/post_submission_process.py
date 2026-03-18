7838b2fc28ed0567a594efa37cb0d117ac932164
Celeste Martinez <celeste@openreview.net>
2026-03-18 16:16:53 -0400

    get submissions by inv and filter before calling concurrent requests

--- openreview/venue/process/post_submission_process.py
+++ openreview/venue/process/post_submission_process.py
@@ -3,6 +3,7 @@ def process(client, invitation):
     domain = client.get_group(invitation.domain)
     venue_id = domain.id
     submission_venue_id = domain.content['submission_venue_id']['value']
+    submission_name = domain.content['submission_name']['value']
     venue_name = domain.content['title']['value']
 
     now = openreview.tools.datetime_millis(datetime.datetime.now())
@@ -15,46 +16,42 @@ def process(client, invitation):
 
     def post_submission_edit(submission):
 
-        # if submission matches invitation source, update submission
-        if openreview.tools.should_match_invitation_source(client, invitation, submission):
-            print(f'posting edit for submission {submission.id}, number {submission.number}')
+        updated_note = openreview.api.Note(
+            id=submission.id
+        )
 
-            updated_note = openreview.api.Note(
-                id=submission.id
-            )
-
-            if invitation.edit['note']['readers'] == ['everyone'] and submission.odate is None:
-                updated_note.odate = now
-                updated_note.content = {
-                    '_bibtex': {
-                        'value': openreview.tools.generate_bibtex(
-                            note=submission,
-                            venue_fullname=venue_name,
-                            year=str(datetime.datetime.now().year),
-                            url_forum=submission.forum,
-                            paper_status='under review',
-                            anonymous='readers' in submission.content['authors'] or 'readers' in invitation.edit.get('note', {}).get('content', {}).get('authors', {})
-                        )
-                    }
+        if invitation.edit['note']['readers'] == ['everyone'] and submission.odate is None:
+            updated_note.odate = now
+            updated_note.content = {
+                '_bibtex': {
+                    'value': openreview.tools.generate_bibtex(
+                        note=submission,
+                        venue_fullname=venue_name,
+                        year=str(datetime.datetime.now().year),
+                        url_forum=submission.forum,
+                        paper_status='under review',
+                        anonymous='readers' in submission.content['authors'] or 'readers' in invitation.edit.get('note', {}).get('content', {}).get('authors', {})
+                    )
                 }
+            }
 
-            client.post_note_edit(
-                invitation=invitation.id,
-                note=updated_note,
-                signatures=[venue_id]
-            )
+        client.post_note_edit(
+            invitation=invitation.id,
+            note=updated_note,
+            signatures=[venue_id]
+        )
     
     ## Release the submissions to specified readers if venueid is still submission
     source = openreview.tools.get_invitation_source(invitation, domain)
-    print('source', source)
-    venue_ids = source.get('venueid', [submission_venue_id])
-    submissions = client.get_all_notes(content= { 'venueid': ','.join([venue_ids] if isinstance(venue_ids, str) else venue_ids)}, sort='number:asc', domain=venue_id)
+    all_submissions = client.get_all_notes(invitation=f'{venue_id}/-/{submission_name}', sort='number:asc', domain=venue_id)
 
-    if not submissions:
+    filtered_submissions = [paper for paper in all_submissions if openreview.tools.should_match_invitation_source(client, invitation, paper)]
+
+    if not filtered_submissions:
         print('No submissions were updated since there are no active submissions')
         return
 
-    print(f'update {len(submissions)} submissions')
-    openreview.tools.concurrent_requests(post_submission_edit, submissions, desc='post_submission_edit')
+    print(f'update {len(filtered_submissions)} submissions')
+    openreview.tools.concurrent_requests(post_submission_edit, filtered_submissions, desc='post_submission_edit')
 
-    print('Submissions updated successfully')
+    print(f'{len(filtered_submissions)} submissions updated successfully')
