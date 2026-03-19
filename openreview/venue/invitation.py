93b19e711374b4519c06266dbcda6892298ba5b9
Harold Rubio <harrywill.rubio@gmail.com>
2026-03-19 10:25:45 -0400

    Rollback invitation changes

--- openreview/venue/invitation.py
+++ openreview/venue/invitation.py
@@ -1009,7 +1009,7 @@ To view your submission, click here: https://openreview.net/forum?id={{{{note_fo
                     }
                 },
                 'recommendation_field_name': {
-                    'value': meta_review_stage.recommendation_field_name
+                    'value': 'recommendation'
                 }
             },
             edit={
@@ -1133,7 +1133,7 @@ To view your submission, click here: https://openreview.net/forum?id={{{{note_fo
                         'param': {
                             'type': 'string',
                             'regex': '.*',
-                            'default': meta_review_stage.recommendation_field_name
+                            'default': 'recommendation'
                         }
                     }
                 }
