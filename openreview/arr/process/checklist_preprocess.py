8658cee963105efd8df27ccfb34e2f52392882f2
Harold Rubio <harrywill.rubio@gmail.com>
2026-04-07 19:21:46 +0200

    Remove extra unused code

--- openreview/arr/process/checklist_preprocess.py
+++ openreview/arr/process/checklist_preprocess.py
@@ -3,16 +3,6 @@ def process(client, edit, invitation):
   note = edit.note
   forum = client.get_note(note.forum)
 
-  violation_fields = ['appropriateness', 'formatting', 'length', 'anonymity', 'responsible_checklist', 'limitations']
-  format_field = {
-    'appropriateness': 'Appropriateness',
-    'formatting': 'Formatting',
-    'length': 'Length',
-    'anonymity': 'Anonymity',
-    'responsible_checklist': 'Responsible Checklist',
-    'limitations': 'Limitations'
-  }
-    
   needs_ethics_review = edit.note.content.get('need_ethics_review', {}).get('value', 'No') == 'Yes'
   if needs_ethics_review and 'N/A' in edit.note.content.get('ethics_review_justification', {}).get('value', 'N/A'):
     raise openreview.OpenReviewException("You have indicated that this submission needs an ethics review. Please enter a brief justification for your flagging.")
