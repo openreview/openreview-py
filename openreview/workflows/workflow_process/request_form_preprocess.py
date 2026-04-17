d4b1fe0081fe7468ad622350bdbc2dec064a0961
Celeste Martinez <celeste@openreview.net>
2026-04-17 15:27:05 -0400

    fix preprocess

--- openreview/workflows/workflow_process/request_form_preprocess.py
+++ openreview/workflows/workflow_process/request_form_preprocess.py
@@ -18,7 +18,8 @@ def process(client, edit, invitation):
     if full_submission_deadline and full_submission_deadline < submission_deadline:
         raise openreview.OpenReviewException('The full submission deadline must be after the submission deadline.')
 
-    reviewer_role_name = request_note.content.get('reviewer_role_name', {}).get('value', 'Reviewers')
-    area_chair_role_name = request_note.content.get('area_chair_role_name', {}).get('value', 'Area_Chairs')
-    if reviewer_role_name == area_chair_role_name:
+    reviewer_role_name = request_note.content.get('reviewer_role_name', request_note.content.get('reviewers_name', {})).get('value', 'Reviewers')
+    area_chair_role_name = request_note.content.get('area_chair_role_name', request_note.content.get('area_chairs_name', {})).get('value', 'Area_Chairs')
+    has_area_chairs = request_note.content.get('area_chairs_support', {}).get('value', False)
+    if has_area_chairs and reviewer_role_name == area_chair_role_name:
         raise openreview.OpenReviewException('The reviewer role name and area chair role name must be different.')
