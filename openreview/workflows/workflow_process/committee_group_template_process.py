a204a09dbb132b1381db912fa866d072b42662c7
Melisa Bok <545506+melisabok@users.noreply.github.com>
2026-04-27 12:35:51 -0400

    increase await timeout (#3032)
    Co-authored-by: celestemartinez <32438984+celestemartinez@users.noreply.github.com>

--- openreview/workflows/workflow_process/committee_group_template_process.py
+++ openreview/workflows/workflow_process/committee_group_template_process.py
@@ -10,6 +10,9 @@ def process(client, edit, invitation):
     committee_anon_name = edit.content.get('committee_anon_name', {}).get('value', False)
     committee_submitted_name = edit.content.get('committee_submitted_name', {}).get('value', False)
 
+    venue_short_name = domain.content['subtitle']['value']
+    venue_from_email = f"{venue_short_name.replace(' ', '').replace(':', '-').replace('@', '').replace('(', '').replace(')', '').replace(',', '-').lower()}-notifications@openreview.net"
+
     content = {
         f'{committee_role}_id': { 'value': edit.group.id },
         f'{committee_role}_name': { 'value': committee_name },
@@ -36,11 +39,10 @@ def process(client, edit, invitation):
         content={
             'venue_id': { 'value': venue_id },
             'group_id': { 'value': edit.group.id },
-            'venue_short_name': { 'value': domain.content['subtitle']['value'] },
-            'venue_from_email': { 'value': f"{domain.content['subtitle']['value'].replace(' ', '').replace(':', '-').replace('@', '').replace('(', '').replace(')', '').replace(',', '-').lower()}-notifications@openreview.net" }
+            'venue_short_name': { 'value': venue_short_name },
+            'venue_from_email': { 'value': venue_from_email }
         },
-        invitation=openreview.api.Invitation(),
-        await_process=True
+        invitation=openreview.api.Invitation()
     )
 
     client.post_invitation_edit(
@@ -50,21 +52,33 @@ def process(client, edit, invitation):
             'venue_id': { 'value': venue_id },
             'group_id': { 'value': edit.group.id },
         },
-        invitation=openreview.api.Invitation(),
-        await_process=True
+        invitation=openreview.api.Invitation()
     )
 
     ## Enable recruitment?
 
-    invited_group_id = client.post_group_edit(
+    invited_group_edit = client.post_group_edit(
         invitation=f'{invitation.domain}/-/Committee_Invited_Group',
         signatures=[invitation.domain],
         content={
             'venue_id': { 'value': venue_id },
             'committee_id': { 'value': edit.group.id },
             'committee_pretty_name': { 'value': committee_pretty_name }
+        }
+    )
+
+    invited_group_id = invited_group_edit['group']['id']
+
+    invited_message_invitation_edit = client.post_invitation_edit(
+        invitations=f'{invitation.domain}/-/Group_Message',
+        signatures=[invitation.domain],
+        content={
+            'venue_id': { 'value': venue_id },
+            'group_id': { 'value': invited_group_id },
+            'venue_short_name': { 'value': venue_short_name },
+            'venue_from_email': { 'value': venue_from_email }
         },
-        await_process=True
+        invitation=openreview.api.Invitation()
     )
 
     declined_group_edit = client.post_group_edit(
@@ -74,12 +88,12 @@ def process(client, edit, invitation):
             'venue_id': { 'value': venue_id },
             'committee_id': { 'value': edit.group.id },
             'committee_pretty_name': { 'value': committee_pretty_name }
-        },
-        await_process=True
-    )     
+        }
+    )
 
     content[f'{committee_role}_declined_id'] = { 'value': declined_group_edit['group']['id'] }
-    content[f'{committee_role}_invited_id'] = { 'value': invited_group_id['group']['id'] }
+    content[f'{committee_role}_invited_id'] = { 'value': invited_group_id }
+    content[f'{committee_role}_invited_message_id'] = { 'value': invited_message_invitation_edit['invitation']['id'] }
 
     invitation_edit = client.post_invitation_edit(
         invitations=f'{invitation.domain}/-/Committee_Recruitment_Request',
@@ -88,11 +102,10 @@ def process(client, edit, invitation):
             'venue_id': { 'value': venue_id },
             'committee_id': { 'value': edit.group.id },
             'committee_pretty_name': { 'value': committee_pretty_name },
-            'venue_short_name': { 'value': domain.content['subtitle']['value'] },
+            'venue_short_name': { 'value': venue_short_name },
             'reminder_delay': { 'value': 3000 if (invitation.domain.startswith('openreview.net')) else (1000 * 60 * 60 * 24 * 7)  }
         },
-        invitation=openreview.api.Invitation(),
-        await_process=True
+        invitation=openreview.api.Invitation()
     )
 
     edit_invitations_builder = openreview.workflows.EditInvitationsBuilder(client, domain.id)
@@ -106,24 +119,22 @@ def process(client, edit, invitation):
             'venue_id': { 'value': venue_id },
             'committee_id': { 'value': edit.group.id },
             'committee_pretty_name': { 'value': committee_pretty_name },
-            'venue_short_name': { 'value': domain.content['subtitle']['value'] }          
+            'venue_short_name': { 'value': venue_short_name }
         },
-        invitation=openreview.api.Invitation(),
-        await_process=True
-    )    
+        invitation=openreview.api.Invitation()
+    )
 
     invitation_edit = client.post_invitation_edit(
         invitations=f'{invitation.domain}/-/Committee_Recruitment_Response',
         signatures=[invitation.domain],
         content={
             'venue_id': { 'value': venue_id },
-            'venue_short_name': { 'value': domain.content['subtitle']['value'] },
+            'venue_short_name': { 'value': venue_short_name },
             'committee_id': { 'value': edit.group.id },
             'committee_pretty_name': { 'value': committee_pretty_name },
             'due_date': { 'value': openreview.tools.datetime_millis(datetime.datetime.now() + datetime.timedelta(weeks=12)) }
         },
-        invitation=openreview.api.Invitation(),
-        await_process=True
+        invitation=openreview.api.Invitation()
     )
 
     edit_invitations_builder.set_edit_dates_one_level_invitation(invitation_edit['invitation']['id'], include_due_date=True, include_exp_date=True)
@@ -138,4 +149,4 @@ def process(client, edit, invitation):
             id=venue_id,
             content=content
         )
-    )        
+    )
