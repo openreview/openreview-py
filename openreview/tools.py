1ed1a24cc5d8fcac4d35fc618bcb8da8eba9feed
Celeste Martinez <celeste@openreview.net>
2026-07-30 14:26:04 -0700

    have decision options have precedence over with decision accept in tools

--- openreview/tools.py
+++ openreview/tools.py
@@ -2159,16 +2159,15 @@ def should_match_invitation_source(client, invitation, submission, note=None, do
 
         decision_value = decision_notes[0].content[domain.content.get('decision_field_name', {}).get('value', 'decision')]['value']
 
-        if 'with_decision_accept' in source:
+        if 'decision_options' in source:
+            decision_options = source.get('decision_options')
+            if decision_value not in decision_options:
+                return False
+        elif 'with_decision_accept' in source:
             with_decision_accept = source.get('with_decision_accept')
             print('checking decision accept for submission', submission.id, 'with_decision_accept', with_decision_accept)
             accept_options = domain.content.get('accept_decision_options', {}).get('value')
             if is_accept_decision(decision_value, accept_options) != with_decision_accept:
-                return False
-
-        if 'decision_options' in source:
-            decision_options = source.get('decision_options')
-            if decision_value not in decision_options:
                 return False
 
     content_keys = invitation.edit.get('content', {}).keys()
