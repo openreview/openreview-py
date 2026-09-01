098c43a8ea285619bcc69567ecf5a97ab14bde5f
Melisa Bok <melisabok@users.noreply.github.com>
2026-09-01 15:52:33 -0400

    fix tests

--- openreview/venue/process/desk_rejection_reversion_submission_process.py
+++ openreview/venue/process/desk_rejection_reversion_submission_process.py
@@ -49,7 +49,7 @@ def process(client, edit, invitation):
             print(f'Remove expiration invitation {expired_invitation.id}')
             invitation_edits = client.get_invitation_edits(invitation_id=expired_invitation.id, invitation=desk_reject_expiration_id)
             for invitation_edit in invitation_edits:
-                print(f'remove edit {edit.id}')
+                print(f'remove edit {invitation_edit.id}')
                 invitation_edit.ddate = now
                 invitation_edit.invitation.expdate = None
                 invitation_edit.invitation.cdate = None
