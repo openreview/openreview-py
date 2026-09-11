b5efd9bfaf683a5f9b4cc8d02e0866fc02525a40
Anna Rogers <arogrs@proton.me>
2026-09-11 17:06:17 +0200

    clarifying reviewer feedback form

--- openreview/stages/arr_content.py
+++ openreview/stages/arr_content.py
@@ -1522,11 +1522,11 @@ arr_official_review_content = {
                 "input": "radio",
                 "enum": [
                     "This work aligns well with both my expertise and interests",
-                    "The expertise match is reasonable, but not my top choice if there was bidding",
+                    "This work is within my expertise, but not my top choice if there was bidding",
                     "This work is too far out of my technical expertise",
                     "This work is too far from my application/domain expertise or language(s)",
                     "The core goals/premises of this work aren't aligned with mine",
-                    "This match does not match my current research interests",
+                    "This work does not match my current research interests",
                     "Other"
                 ],
                 "optional": True,
